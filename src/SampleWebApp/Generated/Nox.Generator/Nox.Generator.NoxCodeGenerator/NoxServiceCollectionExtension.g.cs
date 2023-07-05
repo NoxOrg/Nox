fd8738efa2396785f69856b9d56d378374cd22c1
Jan Schutte <JanSchutte.Lands@gmail.com>
2023-07-05 08:38:37 +0200

    - new version of generated files

---
+++ src/SampleWebApp/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/NoxServiceCollectionExtension.g.cs
@@ -0,0 +1,27 @@
+﻿// Generated
+
+#nullable enable
+
+using Microsoft.EntityFrameworkCore;
+using Nox;
+using Nox.DatabaseProvider;
+using Nox.DatabaseProvider.SqlServer;
+using Nox.Types.EntityFramework.SqlServer;
+using Nox.Types.EntityFramework.vNext;
+using SampleWebApp.Infrastructure.Persistence;
+
+public static class NoxServiceCollectionExtension
+{
+    public static IServiceCollection AddNox(this IServiceCollection services)
+    {
+        services.AddNoxLib();
+        services.AddSingleton<DbContextOptions<SampleWebAppDbContext>>();
+        services.AddSingleton<INoxDatabaseConfigurator, SqlServerDatabaseConfigurator>();
+        services.AddSingleton<INoxDatabaseProvider, SqlServerDatabaseProvider>();
+        services.AddDbContext<SampleWebAppDbContext>();
+        var tmpProvider = services.BuildServiceProvider();
+        var dbContext = tmpProvider.GetRequiredService<SampleWebAppDbContext>();
+        dbContext.Database.EnsureCreated();
+        return services;
+    }
+}
