d41d060bf1245376e903ac4d946ac9e06bdfacdb
Denis Vieira dos Anjos <denis.vieira@authoritypartners.com>
2023-07-05 10:58:57 -0300

    CurrencyCode Implementation

---
+++ src/Nox.Types/Types/CurrencyCode3/CurrencyCode3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Nox.Types;
+
+/// <summary>
+/// Class for three-letters currency code (ISO 4217).
+/// </summary>
+public sealed class CurrencyCode3 : ValueObject<string, CurrencyCode3>
+{
+    private readonly string[] CurrencyCodes = Enum.GetNames(typeof(CurrencyCode));
+
+    /// <summary>
+    /// Validates the <see cref="CurrencyCode3"/> object.
+    /// </summary>
+    /// <returns>A validation result indicating whether the <see cref="CurrencyCode3"/> object is valid or not.</returns>
+    internal override ValidationResult Validate()
+    {
+        var result = base.Validate();
+
+        if (!CurrencyCodes.Contains(Value))
+        {
+            result.Errors.Add(new ValidationFailure(nameof(Value), $"Could not create a Nox CurrencyCode3 type with unsupported value '{Value}'."));
+        }
+
+        return result;
+    }
+}
---
+++ tests/Nox.Types.Tests/Types/CurrencyCode3/CurrencyCode3Tests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+
+namespace Nox.Types.Tests.Types.CountryCode3;
+public class CurrencyCode3Tests
+{
+    [Theory]
+    [ClassData(typeof(CurrencyCodeData))]
+    public void CurrencyCode3_Constructor_ReturnsSameValue_AllCurrencies(string currencyCode3String)
+    {
+        var currencyCode3 = CurrencyCode3.From(currencyCode3String);
+
+        currencyCode3.Value.Should().Be(currencyCode3String);
+    }
+
+    [Fact]
+    public void CurrencyCode3_Constructor_WithUnsupportedCurrencyCode3_ThrowsValidationException()
+    {
+        var action = () => CurrencyCode3.From("ABC");
+
+        action.Should().Throw<TypeValidationException>()
+            .And.Errors.Should().BeEquivalentTo(new[] { new ValidationFailure("Value", "Could not create a Nox CurrencyCode3 type with unsupported value 'ABC'.") });
+    }
+
+    [Fact]
+    public void CurrencyCode3_Equality_Tests()
+    {
+        var currencyCode3_1 = CurrencyCode3.From("USD");
+
+        var currencyCode3_2 = CurrencyCode3.From("USD");
+
+        currencyCode3_1.Should().Be(currencyCode3_2);
+    }
+
+    [Fact]
+    public void CurrencyCode3_NotEqual_Tests()
+    {
+        var currencyCode3_1 = CurrencyCode3.From("RWF");
+
+        var currencyCode3_2 = CurrencyCode3.From("SHP");
+
+        currencyCode3_1.Should().NotBe(currencyCode3_2);
+    }
+
+    [Fact]
+    public void CurrencyCode3_ToString_ReturnsString()
+    {
+        var currencyCode3 = CurrencyCode3.From("USD");
+
+        currencyCode3.ToString().Should().Be("USD");
+    }
+}
