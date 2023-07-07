2c41b7c79466a1da881519122185ab91df8ff80f
Ajla Becic <ajla.becic@regus.com>
2023-07-07 10:01:13 +0200

    add summaries

--- src/Nox.Types/Types/Temperature/Temperature.cs
+++ src/Nox.Types/Types/Temperature/Temperature.cs
@@ -23,12 +23,36 @@ public sealed class Temperature : ValueObject<(float TemperatureValue, Temperatu
 
     public Temperature() { Value = (0, TemperatureTypeUnit.Celsius); }
 
+    /// <summary>
+    /// Creates a new instance of <see cref="Temperature"/> object in <see cref="TemperatureTypeUnit.Celsius"/>.
+    /// </summary>
+    /// <param name="value">The value to create the <see cref="Temperature"/> with</param>
+    /// <returns></returns>
+    /// <exception cref="TypeValidationException"></exception>
     public static Temperature FromCelsius(float temperature) => From(temperature, TemperatureTypeUnit.Celsius);
 
+    /// <summary>
+    /// Creates a new instance of <see cref="Temperature"/> object in <see cref="TemperatureTypeUnit.Fahrenheit"/>.
+    /// </summary>
+    /// <param name="value">The value to create the <see cref="Temperature"/> with</param>
+    /// <returns></returns>
+    /// <exception cref="TypeValidationException"></exception>
     public static Temperature FromFahrenheit(float temperature) => From(temperature, TemperatureTypeUnit.Fahrenheit);
 
+    /// <summary>
+    /// Creates a new instance of <see cref="Temperature"/> object in <see cref="TemperatureTypeUnit.Celsius"/>.
+    /// </summary>
+    /// <param name="value">The value to create the <see cref="Temperature"/> with</param>
+    /// <returns></returns>
+    /// <exception cref="TypeValidationException"></exception>
     public static Temperature From(float temperature) => From(temperature, TemperatureTypeUnit.Celsius);
 
+    /// <summary>
+    /// Creates a new instance of <see cref="Temperature"/> object in sent TemperatureTypeUnit/>.
+    /// </summary>
+    /// <param name="value">The value to create the <see cref="Temperature"/> with</param>
+    /// <returns></returns>
+    /// <exception cref="TypeValidationException"></exception>
     public static Temperature From(float temperature, TemperatureTypeUnit temperatureTypeUnit)
     {
         var newObject = new Temperature
@@ -64,9 +88,17 @@ public sealed class Temperature : ValueObject<(float TemperatureValue, Temperatu
         return result;
     }
 
+    /// <summary>
+    /// Returns a string representation of the <see cref="Temperature"/> object./>.
+    /// </summary>
+    /// <returns>A string representation of the <see cref="Temperature"/> object.</returns>
     public override string ToString()
         => $"{TemperatureValue.ToString($"0.{new string('#', 2)}", CultureInfo.InvariantCulture)} {Unit.ToSymbol()}";
 
+    /// <summary>
+    /// Converts <see cref="Temperature.TemperatureValue"/> to value in <see cref="TemperatureTypeUnit.Celsius"/>.
+    /// </summary>
+    /// <returns>TemperatureValue in <see cref="TemperatureTypeUnit.Celsius"/></returns>
     public float TemperatureValueToCelsius()
     {
         if (Unit == TemperatureTypeUnit.Celsius) return TemperatureValue;
@@ -74,6 +106,10 @@ public sealed class Temperature : ValueObject<(float TemperatureValue, Temperatu
         return Round((TemperatureValue - 32) * 5 / 9);
     }
 
+    /// <summary>
+    /// Converts <see cref="Temperature.TemperatureValue"/> to value in <see cref="TemperatureTypeUnit.Fahrenheit"/>.
+    /// </summary>
+    /// <returns>TemperatureValue in <see cref="TemperatureTypeUnit.Fahrenheit"/></returns>
     public float TemperatureValueToFahrenheit()
     {
         if (Unit == TemperatureTypeUnit.Fahrenheit) return TemperatureValue;
