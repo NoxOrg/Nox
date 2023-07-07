829db8ac23430f4d00a351ee6fc0485eee489a3e
Denis Vieira dos Anjos <denis.vieira@authoritypartners.com>
2023-07-07 06:31:03 -0300

    Currency number implementation

--- src/Nox.Types/Types/CurrencyNumber/CurrencyNumber.cs
+++ src/Nox.Types/Types/CurrencyNumber/CurrencyNumber.cs
@@ -1,12 +1,72 @@
+using System;
+using System.Globalization;
+using System.Threading;
+
 namespace Nox.Types;
 
-    /// <summary>
-    /// Represents a Nox <see cref="CurrencyNumber"/> type and value object.
-    /// Supports ETL Process's and allows to get the proper <see cref="CurrencyCode"/>
-    /// </summary>
-    /// <remarks>Placeholder, needs to be implemented</remarks>
-    // TODO Find Correct Iso for number
-    // TODO Create Map / Relation with CurrencyCode, so its easy to navigate from/to
-    public sealed class CurrencyNumber : ValueObject<uint, CurrencyNumber>
+/// <summary>
+/// Represents a Nox <see cref="CurrencyNumber"/> type and value object.
+/// Supports ETL Process's and allows to get the proper <see cref="CurrencyCode"/>
+/// </summary>    
+[Serializable]
+public sealed class CurrencyNumber : ValueObject<(uint Amount, CurrencyCode CurrencyCode), CurrencyNumber>
+{
+    public uint Amount
     {
+        get => Value.Amount;
+        private set => Value = (value, Value.CurrencyCode);
     }
+
+    public string CurrencyCode
+    {
+        get => Value.CurrencyCode.ToString();
+        private set => Value = (Value.Amount, (CurrencyCode)Enum.Parse(typeof(CurrencyCode), value));   
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CurrencyNumber"/> class with default values.
+    /// </summary>
+    public CurrencyNumber() { Value = (0, Types.CurrencyCode.USD); }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="CurrencyNumber"/> class with the specified values.
+    /// </summary>
+    /// <param name="value">The currency value.</param>
+    /// <param name="currencyCode">The currency code enum.</param>
+    /// <returns>A new instance of the <see cref="CurrencyNumber"/> class.</returns>
+    public static CurrencyNumber From(uint value, CurrencyCode currencyCode) =>
+            From((value, currencyCode));
+
+    public override string ToString()
+    {
+        return $"{Value.CurrencyCode} {Value.Amount.ToString(CultureInfo.InvariantCulture)}";
+    }
+
+    /// <summary>
+    /// Returns a string representation of the <see cref="CurrencyNumber"/> object using the specified <paramref name="format"/>.
+    /// </summary>
+    /// <param name="format">The format specifier for the amount value.</param>
+    /// <returns>A string representation of the <see cref="CurrencyNumber"/> object with the amount formatted using the specified format and Invariant culture .</returns>
+    public string ToString(string format)
+    {
+        return ToString(format, Thread.CurrentThread.CurrentCulture);
+    }
+
+    /// <summary>
+    /// Returns a string representation of the <see cref="CurrencyNumber"/> object using the specified <paramref name="cultureInfo"/> and <paramref name="amountFormat"/>.
+    /// </summary>
+    /// <param name="format">The format specifier for the amount value.</param>
+    /// <param name="cultureInfo">The culture-specific information used to format the amount.</param>
+    /// <returns>A string representation of the <see cref="CurrencyNumber"/> object with the amount formatted using the specified culture and format.</returns>
+    public string ToString(string format, CultureInfo cultureInfo)
+    {
+        if (cultureInfo == null) throw new ArgumentNullException(nameof(cultureInfo));
+
+        if (format == null) throw new ArgumentNullException(nameof(format));
+
+        if (format == "C")
+            return CurrencySymbol.GetCurrencySymbol(Value.CurrencyCode) + Value.Amount.ToString("N");
+
+        return Value.CurrencyCode + " " + Value.Amount.ToString(format, cultureInfo);
+    }
+}
--- tests/Nox.Types.Tests/Types/CurrencyNumber/CurrencyNumberTests.cs
+++ tests/Nox.Types.Tests/Types/CurrencyNumber/CurrencyNumberTests.cs
@@ -1,11 +1,108 @@
-﻿// ReSharper disable once CheckNamespace
+﻿using FluentAssertions;
+using System.Globalization;
+
 namespace Nox.Types.Tests.Types;
 
 public class CurrencyNumberTests
 {
     [Fact]
-    public void When_Create_Should()
+    public void CurrencyNumber_DefaultConstructor_InitializedWithDefaultValues()
     {
+        var currencyNumber = new CurrencyNumber();
 
+        currencyNumber.Amount.Should().Be(0);
+        currencyNumber.CurrencyCode.Should().Be("USD");
+        currencyNumber.Value.CurrencyCode.Should().Be(CurrencyCode.USD);
+    }
+
+    [Fact]
+    public void CurrencyNumber_ParameterizedConstructor_InitializedWithProvidedValues()
+    {
+        // Arrange
+        uint value = 1000;
+        var currency = CurrencyCode.EUR;
+
+        // Act
+        var currencyNumber = CurrencyNumber.From(value, currency);
+
+        // Assert
+        currencyNumber.Amount.Should().Be(value);
+        currencyNumber.CurrencyCode.Should().Be("EUR");
+        currencyNumber.Value.CurrencyCode.Should().Be(CurrencyCode.EUR);
+    }
+
+    [Fact]
+    public void CurrencyNumber_FromMethod_CreatesMoneyObject()
+    {
+        // Arrange
+        uint value = 25000U;
+        var currency = CurrencyCode.GBP;
+
+        // Act
+        var currencyNumber = CurrencyNumber.From(value, currency);
+
+        // Assert
+        currencyNumber.Amount.Should().Be(value);
+        currencyNumber.CurrencyCode.Should().Be("GBP");
+        currencyNumber.Value.CurrencyCode.Should().Be(CurrencyCode.GBP);
+    }
+
+    [Fact]
+    public void CurrencyNumber_Should_Equal_When_Their_Value_And_Currency_Same()
+    {
+        // Arrange
+        var currencyNumber = CurrencyNumber.From(1550U, CurrencyCode.USD);
+        var currencyNumber2 = CurrencyNumber.From(1550U, CurrencyCode.USD);
+
+        // Act
+        var result = currencyNumber.Equals(currencyNumber2);
+
+        // Assert
+        currencyNumber.Amount.Should().Be(currencyNumber2.Value.Amount);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void CurrencyNumber_Should_Not_Equal_When_Their_Value_And_Currency_Different()
+    {
+        // Arrange
+        var currencyNumber = CurrencyNumber.From(13200U, CurrencyCode.USD);
+        var currencyNumber2 = CurrencyNumber.From(13200U, CurrencyCode.TRY);
+
+        var currencyNumber3 = CurrencyNumber.From(13200U, CurrencyCode.USD);
+        var currencyNumber4 = CurrencyNumber.From(13201U, CurrencyCode.USD);
+
+        // Act
+        var result = currencyNumber.Equals(currencyNumber2);
+        var result2 = currencyNumber3.Equals(currencyNumber4);
+
+        // Assert
+        result.Should().BeFalse();
+        result2.Should().BeFalse();
+        currencyNumber.Should().NotBe(currencyNumber2);
+        currencyNumber3.Should().NotBe(currencyNumber4);
+    }
+
+    [Fact]
+    public void CurrencyNumber_ToString_ReturnsCurrencyAndAmount()
+    {
+        var currencyNumber = CurrencyNumber.From(20000u, CurrencyCode.BRL);
+
+        Assert.Equal("BRL 20000", currencyNumber.ToString());
+    }
+
+    [Fact]
+    public void CurrencyNumber_ToString_With_C_Format_ReturnsCurrencySymbolAndAmount()
+    {
+        void Test()
+        {
+            var currencyNumber = CurrencyNumber.From(1455U, CurrencyCode.ZAR);
+            var currencyNumber2 = CurrencyNumber.From(1455U, CurrencyCode.GBP);
+
+            Assert.Equal("R1,455.00", currencyNumber.ToString("C", new CultureInfo("en-ZA")));
+            Assert.Equal("£1,455.00", currencyNumber2.ToString("C", new CultureInfo("en-GB")));
+        }
+
+        TestUtility.RunInInvariantCulture(Test);
     }
 }
