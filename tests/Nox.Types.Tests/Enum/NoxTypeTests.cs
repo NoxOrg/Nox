ca74fb6c074d90ef3a115cbd4b49efade3310b64
Ricardo Rocha <rochar@users.noreply.github.com>
2023-07-08 08:09:23 +0100

    set noxtype value to be immutable

---
+++ tests/Nox.Types.Tests/Enum/NoxTypeTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace Nox.Types.Tests.Enum
+{
+    public class NoxTypeTests
+    {
+        [Fact]
+        public void NoxType_Value_Is_Nuid()
+        {
+            const string noxTypeFormatNuid = "Nox.Types.{0}";
+
+            using (new AssertionScope())
+            {
+                foreach (var noxType in System.Enum.GetValues<NoxType>())
+                {
+                    var id = Utilities.Identifier.Nuid.ToInt32(string.Format(noxTypeFormatNuid, noxType.ToString()));
+
+                   ((int)noxType).Should().Be(System.Math.Abs(id));
+                   // Console.WriteLine($"{noxType.ToString()} = {System.Math.Abs(id)},");
+                }
+            }
+        }
+    }
+}
