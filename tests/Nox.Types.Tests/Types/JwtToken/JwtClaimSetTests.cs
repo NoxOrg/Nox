89aab88a336d84fe81fc9177edcd31ff1b0765fd
Dejan Bratic <dejan.bratic@igwplc.com>
2023-07-10 11:30:26 +0200

    updated test cases;

--- tests/Nox.Types.Tests/Types/JwtToken/JwtClaimSetTests.cs
+++ tests/Nox.Types.Tests/Types/JwtToken/JwtClaimSetTests.cs
@@ -8,7 +8,7 @@ public class JwtClaimSetTests
     [Fact]
     public void JwtClaimSet_Constructor_ReturnsValue()
     {
-        var encodedPayload = "eyJzdWIiOiIxMjM0NTY3ODkxIiwibmFtZSI6IkpvaG4gRG9lIiwicm9sZXMiOlt7Im5hbWUiOiJPd25lciIsImFzQXQiOiIyMDIzLTA3LTA1VDAwOjAwOjAwIn0seyJuYW1lIjoiQ29udHJpYnV0b3IiLCJhc0F0IjoiMjAyMy0wNy0wMVQwMDowMDowMCJ9XSwiaWF0IjoxNTE2MjM5MDIyfQ";
+        var encodedPayload = "eyJzdWIiOiIxMjM0NTY3ODkxIiwibmFtZSI6IkpvaG4gRG9lIiwicm9sZXMiOlt7Im5hbWUiOiJPd25lciIsImFzQXQiOiIyMDIzLTA3LTA1VDAwOjAwOjAwIn0seyJuYW1lIjoiQ29udHJpYnV0b3IiLCJhc0F0IjoiMjAyMy0wNy0wMVQwMDowMDowMCJ9XSwiaXNPd25lciI6dHJ1ZSwiaXNHdWVzdCI6ZmFsc2UsInRpbWVzdGFtcCI6IjIwMjMtMDctMTBUMTI6MDA6MDAiLCJpYXQiOjE1MTYyMzkwMjJ9";
 
         var claims = new JwtClaimSet(encodedPayload);
 
@@ -18,6 +18,9 @@ public class JwtClaimSetTests
             new Claim("name", "John Doe", "http://www.w3.org/2001/XMLSchema#string"),
             new Claim("roles", "{\"name\":\"Owner\",\"asAt\":\"2023-07-05T00:00:00\"}", "JSON"),
             new Claim("roles", "{\"name\":\"Contributor\",\"asAt\":\"2023-07-01T00:00:00\"}", "JSON"),
+            new Claim("isOwner", "true", "http://www.w3.org/2001/XMLSchema#boolean"),
+            new Claim("isGuest", "false", "http://www.w3.org/2001/XMLSchema#boolean"),
+            new Claim("timestamp", "2023-07-10T10:00:00.0000000Z", "http://www.w3.org/2001/XMLSchema#dateTime"),
             new Claim("iat", "1516239022", "http://www.w3.org/2001/XMLSchema#integer"),
         });
     }
