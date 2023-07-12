05076f6013ed7fcf1e021f1e843a29e2206c5ded
Denis Vieira <denis.vieira@authorityparterns.com>
2023-07-12 10:00:53 -0300

    Move branch

---
+++ src/Nox.Types/Types/Color/Color.cs
@@ -0,0 +1,8 @@
+﻿namespace Nox.Types;
+
+/// <summary>
+/// Represents a Nox <see cref="Color"/> type and value object. 
+/// </summary>
+public sealed class Color : ValueObject<(byte Alpha, byte Red, byte Green, byte Blue), Color>
+{
+}
