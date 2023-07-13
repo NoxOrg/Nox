60a740d7813ed52a0f9b87a907546b80d3d227e8
Jan Schutte <JanSchutte.Lands@gmail.com>
2023-07-13 13:55:25 +0200

    - added DataFlowSource in CsvIntegrationSource.cs

--- src/Nox.IntegrationSource.Csv/CsvIntegrationSource.cs
+++ src/Nox.IntegrationSource.Csv/CsvIntegrationSource.cs
@@ -1,10 +1,13 @@
 using System.Dynamic;
+using System.Globalization;
+using CsvHelper.Configuration;
 using ETLBox.Connection;
 using ETLBox.DataFlow;
 using ETLBox.DataFlow.Connectors;
 using Nox.Integration;
 using Nox.Integration.Abstractions;
 using Nox.Integration.Constants;
+using Nox.Integration.Exceptions;
 using Nox.Solution;
 using SqlKata.Compilers;
 
@@ -14,24 +17,58 @@ public class CsvIntegrationSource: ISource
 {
     private readonly string _name;
     private readonly ResourceType _resourceType;
+    private readonly DataConnection _dataConnection;
+    private readonly IntegrationSourceFileOptions _fileOptions;
     
-    protected CsvSource<ExpandoObject> _dataFlowExecutableSource = null!;
-
     public string Name => _name;
     
     public string Type => SourceType.Csv;
-    
-    public Compiler SqlCompiler { get; }
+
+    public Compiler SqlCompiler { get; } = null!;
 
     public CsvIntegrationSource(Solution.IntegrationSource sourceConfig, DataConnection dataConnection)
     {
         _name = sourceConfig.Name;
-        
+        _dataConnection = dataConnection;
+        _fileOptions = sourceConfig.FileOptions!;
+        var sourceUri = new Uri(dataConnection.ServerUri);
+        _resourceType = sourceUri.Scheme.ToLower() switch
+        {
+            "http" => ResourceType.Http,
+            "https" => ResourceType.Http,
+            "blob" => ResourceType.AzureBlob,
+            "file" => ResourceType.File,
+            _ => throw new NoxIntegrationException("Invalid Csv source specified in uri scheme. Available options are http, https, blob, file")
+        };
     }
     
     public IDataFlowExecutableSource<ExpandoObject> DataFlowSource()
     {
-        throw new NotImplementedException();
+        var dataFlowExecutableSource = new CsvSource<ExpandoObject>
+        {
+            Configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        };
+        
+        dataFlowExecutableSource.ResourceType = _resourceType;
+        
+        switch (_resourceType)
+        {
+            case ResourceType.File:
+                var uri = new Uri(_dataConnection.ServerUri);
+                
+                var filePath = Path.Combine(_folderPath, _sourceConfig.FileOptions!.Filename);
+                
+                break;
+            
+            case ResourceType.Http:
+                dataFlowExecutableSource.Uri = _dataConnection.ServerUri; 
+                break;
+            
+            case ResourceType.AzureBlob:
+                throw new NotImplementedException();
+        }
+
+        return dataFlowExecutableSource;
     }
 
     public void ApplyMergeInfo(IntegrationMergeStates lastMergeInfo, string[] dateTimeStampColumns, string[] targetColumns)
