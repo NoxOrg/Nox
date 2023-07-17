1c980ac214555b2f1cc97cd67be9097f0d4dcc6c
Daniel Dutra <137419123+danield-ap@users.noreply.github.com>
2023-07-17 20:01:28 +0200

    create autonumber nox type

---
+++ tests/Nox.Types.Tests/Types/AutoNumber/AutoNumberTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+
+namespace Nox.Types.Tests.Types;
+
+public class AutoNumberTests
+{
+    private static readonly Random RandomGenerator = new();
+
+    [Fact]
+    public void AutoNumber_Constructor_ReturnsInitialValue()
+    {
+        var autoNumber = new AutoNumber();
+
+        autoNumber.Value.Should().Be(0U);
+    }
+
+    [Fact]
+    public void AutoNumber_GetNext_ReturnNextNumber()
+    {
+        var initialValue = (uint)RandomGenerator.Next();
+
+        var autoNumber = AutoNumber.From(initialValue);
+
+        autoNumber.GetNext().Should().Be(initialValue + 1U);
+        autoNumber.GetNext().Should().Be(initialValue + 2U);
+    }
+
+    [Fact]
+    public void AutoNumber_Equals_ReturnTrue()
+    {
+        var initialValue = (uint)RandomGenerator.Next();
+
+        var autoNumber1 = AutoNumber.From(initialValue);
+        var autoNumber2 = AutoNumber.From(initialValue);
+
+        autoNumber1.Should().Be(autoNumber2);
+    }
+}
