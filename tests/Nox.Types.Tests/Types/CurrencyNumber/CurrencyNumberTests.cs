aa389b45bcb18fb25ab58ccf75855d301164aa65
Denis Vieira <denis.vieira@authorityparterns.com>
2023-07-18 19:08:57 -0300

    Tests

--- tests/Nox.Types.Tests/Types/CurrencyNumber/CurrencyNumberTests.cs
+++ tests/Nox.Types.Tests/Types/CurrencyNumber/CurrencyNumberTests.cs
@@ -1,125 +1,52 @@
 ﻿using FluentAssertions;
-using System.Globalization;
 
 namespace Nox.Types.Tests.Types;
 
 public class CurrencyNumberTests
 {
-    [Fact]
-    public void CurrencyNumber_DefaultConstructor_InitializedWithDefaultValues()
+    [Theory]
+    [ClassData(typeof(CurrencyNumberData))]
+    public void CurrencyNumber_Constructor_ReturnsSameValue_AllCurrencies(short currencyNumberCode)
     {
-        var currencyNumber = new CurrencyNumber();
+        var currencyNumber = CurrencyNumber.From(currencyNumberCode);
 
-        currencyNumber.Amount.Should().Be(0);
-        currencyNumber.CurrencyCode.Should().Be("USD");
-        currencyNumber.Value.CurrencyCode.Should().Be(CurrencyCode.USD);
+        currencyNumber.Value.Should().Be(currencyNumberCode);
     }
 
     [Fact]
-    public void CurrencyNumber_ParameterizedConstructor_InitializedWithProvidedValues()
+    public void CurrencyNumber_Constructor_WithUnsupportedCurrencyNumber_ThrowsValidationException()
     {
-        // Arrange
-        uint value = 1000;
-        var currency = CurrencyCode.EUR;
+        var action = () => CurrencyNumber.From(999);
 
-        // Act
-        var currencyNumber = CurrencyNumber.From(value, currency);
-
-        // Assert
-        currencyNumber.Amount.Should().Be(value);
-        currencyNumber.CurrencyCode.Should().Be("EUR");
-        currencyNumber.Value.CurrencyCode.Should().Be(CurrencyCode.EUR);
+        action.Should().Throw<TypeValidationException>()
+            .And.Errors.Should().BeEquivalentTo(new[] { new ValidationFailure("Value", "Could not create a Nox CurrencyNumber type with unsupported value '999'.") });
     }
 
     [Fact]
-    public void CurrencyNumber_FromMethod_CreatesMoneyObject()
+    public void CurrencyNumber_Equality_Tests()
     {
-        // Arrange
-        uint value = 25000U;
-        var currency = CurrencyCode.GBP;
+        var currencyNumber_1 = CurrencyNumber.From(784);
 
-        // Act
-        var currencyNumber = CurrencyNumber.From(value, currency);
+        var currencyNumber_2 = CurrencyNumber.From(784);
 
-        // Assert
-        currencyNumber.Amount.Should().Be(value);
-        currencyNumber.CurrencyCode.Should().Be("GBP");
-        currencyNumber.Value.CurrencyCode.Should().Be(CurrencyCode.GBP);
+        currencyNumber_1.Should().Be(currencyNumber_2);
     }
 
     [Fact]
-    public void CurrencyNumber_Should_Equal_When_Their_Value_And_Currency_Same()
+    public void CurrencyNumber_NotEqual_Tests()
     {
-        // Arrange
-        var currencyNumber = CurrencyNumber.From(1550U, CurrencyCode.USD);
-        var currencyNumber2 = CurrencyNumber.From(1550U, CurrencyCode.USD);
+        var currencyNumber_1 = CurrencyNumber.From(784);
 
-        // Act
-        var result = currencyNumber.Equals(currencyNumber2);
+        var currencyNumber_2 = CurrencyNumber.From(694);
 
-        // Assert
-        currencyNumber.Amount.Should().Be(currencyNumber2.Value.Amount);
-        result.Should().BeTrue();
+        currencyNumber_1.Should().NotBe(currencyNumber_2);
     }
 
     [Fact]
-    public void CurrencyNumber_Should_Not_Equal_When_Their_Value_And_Currency_Different()
+    public void CurrencyNumber_ToString_ReturnsString()
     {
-        // Arrange
-        var currencyNumber = CurrencyNumber.From(13200U, CurrencyCode.USD);
-        var currencyNumber2 = CurrencyNumber.From(13200U, CurrencyCode.TRY);
+        var currencyNumber = CurrencyNumber.From(986);
 
-        var currencyNumber3 = CurrencyNumber.From(13200U, CurrencyCode.USD);
-        var currencyNumber4 = CurrencyNumber.From(13201U, CurrencyCode.USD);
-
-        // Act
-        var result = currencyNumber.Equals(currencyNumber2);
-        var result2 = currencyNumber3.Equals(currencyNumber4);
-
-        // Assert
-        result.Should().BeFalse();
-        result2.Should().BeFalse();
-        currencyNumber.Should().NotBe(currencyNumber2);
-        currencyNumber3.Should().NotBe(currencyNumber4);
-    }
-
-    [Fact]
-    public void CurrencyNumber_ToString_ReturnsCurrencyAndAmount()
-    {
-        var currencyNumber = CurrencyNumber.From(20000u, CurrencyCode.BRL);
-
-        currencyNumber.ToString().Should().Be("BRL 20000");
-    }
-
-    [Fact]
-    public void CurrencyNumber_ToString_With_C_Format_ReturnsCurrencySymbolAndAmount()
-    {
-        void Test()
-        {
-            var currencyNumber = CurrencyNumber.From(1455U, CurrencyCode.ZAR);
-            var currencyNumber2 = CurrencyNumber.From(1455U, CurrencyCode.GBP);
-
-            currencyNumber.ToString("C", new CultureInfo("en-ZA")).Should().Be("R1,455.00");
-            currencyNumber2.ToString("C", new CultureInfo("en-GB")).Should().Be("£1,455.00");
-        }
-
-        TestUtility.RunInInvariantCulture(Test);
-    }
-
-    [Fact]
-    public void CurrencyNumber_ToString_SameCurrency_DifferentCulture_ReturnsSameCurrencySymbol()
-    {
-        void Test()
-        {
-            var currencyNumber = CurrencyNumber.From(1455U, CurrencyCode.BRL);
-
-            var resultWithUSCulture = currencyNumber.ToString("C", new CultureInfo("en-US"));
-            var resultWithBRCulture = currencyNumber.ToString("C", new CultureInfo("pt-BR"));
-
-            resultWithUSCulture.Should().Be("R$1,455.00");
-            resultWithBRCulture.Should().Be("R$1.455,00");
-        }
-
-        TestUtility.RunInInvariantCulture(Test);
+        currencyNumber.ToString().Should().Be("BRL");
     }
 }
