741c2fdb25a50eac8cde3723300c7db080989deb
Ajla Becic <ajla.becic@regus.com>
2023-07-19 10:52:56 +0200

    move options

--- src/Nox.Types.Abstractions/TypeDefinitions/NoxSimpleTypeDefinition.cs
+++ src/Nox.Types.Abstractions/TypeDefinitions/NoxSimpleTypeDefinition.cs
@@ -61,6 +61,9 @@ public class NoxSimpleTypeDefinition
     [IfEquals("Type", NoxType.DateTime)]
     public DateTimeTypeOptions? DateTimeTypeOptions { get; set; }
 
+    [IfEquals("Type", NoxType.Password)]
+    public PasswordTypeOptions? PasswordTypeOptions { get; set; }
+
     #endregion TypeOptions
 
     [Title("Is the attribute required? Boolean value.")]
