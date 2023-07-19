b156f8d29528c7be2422b16ff56592b73c1c93ad
Ricardo Rocha <rochar@users.noreply.github.com>
2023-07-19 16:20:35 +0100

    comment Lazy Proxies to have a better solution

--- src/Nox.EntityFramework.Postgres/PostgresDatabaseProvider.cs
+++ src/Nox.EntityFramework.Postgres/PostgresDatabaseProvider.cs
@@ -33,7 +33,7 @@ public class PostgresDatabaseProvider: NoxDatabaseConfigurator, INoxDatabaseProv
         SetConnectionString(csb.ConnectionString);
 
         return optionsBuilder
-            .UseLazyLoadingProxies()
+            //.UseLazyLoadingProxies()
             .UseNpgsql(_connectionString, opts => { opts.MigrationsHistoryTable("MigrationsHistory", "migrations"); });
     }
 
