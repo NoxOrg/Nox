1f834a3dad7171dd9c47ecf25416cbf9b1d8850b
Jan Schutte <JanSchutte.Lands@gmail.com>
2023-07-25 14:33:06 +0200

    - used skip on some secrets tests

--- tests/Nox.Secrets.Tests/ResolverTests.cs
+++ tests/Nox.Secrets.Tests/ResolverTests.cs
@@ -3,7 +3,6 @@ using Nox.Secrets.Abstractions;
 
 namespace Nox.Secrets.Tests;
 
-////These tests can only be run if you have started the hashicorp vault docker container
 public class ResolverTests: IClassFixture<SecretsFixture>
 {
     private readonly SecretsFixture _fixture;
@@ -13,7 +12,11 @@ public class ResolverTests: IClassFixture<SecretsFixture>
         _fixture = fixture;
     }
     
+#if DEBUG
     [Fact]
+#else
+    [Fact (Skip = "Only available if you have started the hashicorp vault docker container")]
+#endif  
     public void Result_Must_be_empty_if_secret_does_not_exist()
     {
         var resolver = _fixture.ServiceProvider.GetRequiredService<INoxSecretsResolver>();
@@ -28,13 +31,16 @@ public class ResolverTests: IClassFixture<SecretsFixture>
         Assert.Empty(result);
     }
     
-    //Secrets are resolved in this order: organization, solution, user. 
+#if DEBUG
     [Theory]
     [InlineData("org-only-secret", "This is an organization only secret")]
     [InlineData("sln-only-secret", "This is a solution only secret")]
     [InlineData("user-secret", "This secret only exists in user secrets")]
     [InlineData("org-sln-secret", "This is a secret that exists in org and sln and this is the solution value")]
     [InlineData("org-sln-user-secret", "This is a secret that exists in org, sln and user and this is the user value")]
+#else
+    [Theory (Skip = "Only available if you have started the hashicorp vault docker container")]
+#endif      
     public void Must_honor_the_order_or_precedence(string key, string expectedValue)
     {
         var resolver = _fixture.ServiceProvider.GetRequiredService<INoxSecretsResolver>();
