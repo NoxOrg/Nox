28a184307048b5cbbee34c7b51f9dc5486ceef28
Ajla Becic <ajla.becic@regus.com>
2023-07-27 11:11:37 +0200

    generate models

--- tests/Nox.Tests/DatabaseIntegrationTests/Models/TestEntityForTypes.cs
+++ tests/Nox.Tests/DatabaseIntegrationTests/Models/TestEntityForTypes.cs
@@ -82,6 +82,11 @@ public partial class TestEntityForTypes : AuditableEntityBase
     /// <summary>
     ///  (Optional).
     /// </summary>
+    public Temperature? TempratureTestField { get; set; } = null!;
+
+    /// <summary>
+    ///  (Optional).
+    /// </summary>
     public TranslatedText? TranslatedTextTestField { get; set; } = null!;
 
     /// <summary>
@@ -92,5 +97,5 @@ public partial class TestEntityForTypes : AuditableEntityBase
     /// <summary>
     ///  (Optional).
     /// </summary>
-    public Temperature? TempratureTestField { get; set; } = null!;
+    public LanguageCode? LanguageCodeTestField { get; set; } = null!;
 }
