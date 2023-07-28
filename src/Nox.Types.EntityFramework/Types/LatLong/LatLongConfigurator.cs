5ec6726b251129545ed8d20bb11ec8fcd8a10a2c
m.sadik.kose <sadik.kose@authoritypartners.com>
2023-07-28 12:45:00 +0300

    Database configuration for "LatLong"

---
+++ src/Nox.Types.EntityFramework/Types/LatLong/LatLongConfigurator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nox.Solution;
+using Nox.Types.EntityFramework.Abstractions;
+
+namespace Nox.Types.EntityFramework.Types;
+
+public class LatLongConfigurator : INoxTypeDatabaseConfigurator
+{
+    public NoxType ForNoxType => NoxType.LatLong;
+    public virtual bool IsDefault => true;
+
+    public void ConfigureEntityProperty(
+        NoxSolutionCodeGeneratorState noxSolutionCodeGeneratorState,
+        EntityTypeBuilder builder,
+        NoxSimpleTypeDefinition property,
+        Entity entity,
+        bool isKey)
+    {
+
+        builder
+            .OwnsOne(typeof(LatLong), property.Name)
+            .Ignore(nameof(LatLong.Value))
+            .Property(nameof(LatLong.Latitude));
+    }
+
+    public string GetKeyPropertyName(NoxSimpleTypeDefinition key) => key.Name;
+}
