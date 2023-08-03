ea91d11658795e12f9c030c4d4204c041da1789d
Daniel Dutra <137419123+danield-ap@users.noreply.github.com>
2023-08-03 19:36:04 +0200

    update tests

--- tests/Nox.Types.Tests/Types/Markdown/MarkdownTests.cs
+++ tests/Nox.Types.Tests/Types/Markdown/MarkdownTests.cs
@@ -44,18 +44,20 @@ public class MarkdownTests
     [Fact]
     public void MarkdownOptions_Constructor_ReturnsDefaults()
     {
-        var textOptions = new MarkdownTypeOptions();
+        var markdownOptions = new MarkdownTypeOptions();
 
-        textOptions.IsUnicode.Should().BeTrue();
-        textOptions.MinLength.Should().Be(0u);
-        textOptions.MaxLength.Should().Be(255u);
+        markdownOptions.IsUnicode.Should().BeTrue();
+        markdownOptions.MinLength.Should().Be(0u);
+        markdownOptions.MaxLength.Should().Be(255u);
     }
 
     [Fact]
     public void Markdown_Constructor_SpecifyingMaxLength_WithLongerLengthInput_ThrowsValidationException()
     {
-        var fromAct = () => Text.From("long text", new TextTypeOptions { MaxLength = 3 });
+        var maxLength = 3u;
+        var fromAct = () => Markdown.From("long text", new MarkdownTypeOptions { MaxLength = maxLength });
 
-        fromAct.Should().Throw<TypeValidationException>();
+        fromAct.Should().Throw<TypeValidationException>().And.Errors.Should().BeEquivalentTo(new[] { new ValidationFailure("Value",
+            $"Could not create a Markdown type that is 9 characters long and longer than the maximum specified length of {maxLength}") });
     }
 }
