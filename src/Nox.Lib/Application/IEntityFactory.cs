6926bf6ea178c0e7f044a16b4b8c3d03b8d59ce3
Ricardo Rocha <rochar@users.noreply.github.com>
2023-08-03 10:48:42 +0100

    register entity factories in the container

--- src/Nox.Lib/Application/IEntityFactory.cs
+++ src/Nox.Lib/Application/IEntityFactory.cs
@@ -2,6 +2,9 @@
 
 namespace Nox.Application
 {
+    /// <summary>
+    /// Factory for Entity created by using a dto
+    /// </summary>    
     public interface IEntityFactory<T,E> where T : class where E : IEntity
     {
         E CreateEntity(T dto);
