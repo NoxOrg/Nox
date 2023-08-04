8f073d3bd559c575e2328b635b2a82734345e6a4
Andrii Dehtiarov <Andrii.Dehtiarov@iwgplc.com>
2023-08-04 10:18:01 +0300

    [Nox.Generators] [Odata] Refactor ODataModelGenerator extract OEntitiy model to its own Generator #407 [Nox.Generators] [Odata] Refactor ODataModelGenerator extract EntitiyDto model to its own Generator #408

--- tests/Nox.Generator.Tests/Presentation/ApiControllerTest.cs
+++ tests/Nox.Generator.Tests/Presentation/ApiControllerTest.cs
@@ -42,19 +42,19 @@ public class ApiControllerTest : IClassFixture<GeneratorFixture>
         Assert.Single(allOutputs);
 
         var generatedSources = result.GeneratedSources;
-        Assert.Equal(16, generatedSources.Length);
+        Assert.Equal(17, generatedSources.Length);
         Assert.True(generatedSources.Any(s => s.HintName == "NoxWebApplicationExtensions.g.cs"), "NoxWebApplicationExtensions.g.cs not generated");
-        
+
         // Check base files
         Assert.True(generatedSources.Any(s => s.HintName == "Generator.g.cs"), "Generator.g.cs not generated");
-        
+
         // check entities/queries/commands
         Assert.True(generatedSources.Any(s => s.HintName == "Entities.Country.g.cs"), "Country.g.cs not generated");
         Assert.True(generatedSources.Any(s => s.HintName == "UpdatePopulationStatistics.g.cs"), "UpdatePopulationStatistics.g.cs not generated");
         Assert.True(generatedSources.Any(s => s.HintName == "UpdatePopulationStatisticsCommandHandlerBase.g.cs"), "UpdatePopulationStatisticsCommandHandlerBase.g.cs not generated");
         Assert.True(generatedSources.Any(s => s.HintName == "CountryInfo.g.cs"), "CountryInfo.g.cs not generated");
         Assert.True(generatedSources.Any(s => s.HintName == "GetCountriesByContinentQueryBase.g.cs"), "GetCountriesByContinentQuery.g.cs not generated");
-        
+
         // check controllers
         var controllerFileName = "Controllers.CountriesController.g.cs";
         Assert.True(generatedSources.Any(s => s.HintName == controllerFileName), $"{controllerFileName} not generated");
