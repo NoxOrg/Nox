6c27bca2ac3ed83b23c19a1ecfd19ba85011fd87
Ricardo Rocha <rochar@users.noreply.github.com>
2023-08-07 10:20:32 +0100

    add factories for vatnumber and file noxtypes

--- src/Nox.Generator/Application/EntityFactory.template.cs
+++ src/Nox.Generator/Application/EntityFactory.template.cs
@@ -12,6 +12,7 @@ using Nox.Factories;
 using Nox.Types;
 using Nox.Application;
 using Nox.Extensions;
+using File = Nox.Types.File;
 using {{codeGeneratorState.ApplicationNameSpace}}.Dto;
 using {{codeGeneratorState.DomainNameSpace}};
 
@@ -29,7 +30,7 @@ public class {{className}}: EntityFactoryBase<{{entity.Name}}CreateDto, {{entity
     #pragma warning restore CS0168 // Variable is declared but never used
     {{ for attribute in entity.Attributes }}
     {{- #to be removed when we support all types -}}
-    {{- if attribute.Type == "StreetAddress" || attribute.Type == "Money" || attribute.Type == "Text" || attribute.Type == "Number" || attribute.Type == "Area" }}
+    {{- if attribute.Type == "File" ||  attribute.Type == "VatNumber" || attribute.Type == "StreetAddress" || attribute.Type == "Money" || attribute.Type == "Text" || attribute.Type == "Number" || attribute.Type == "Area" }}
         noxTypeValue =  CreateNoxType<{{attribute.Type}}>(entityDefinition,"{{attribute.Name}}",dto.{{attribute.Name}});
         if(noxTypeValue != null)
         {        
