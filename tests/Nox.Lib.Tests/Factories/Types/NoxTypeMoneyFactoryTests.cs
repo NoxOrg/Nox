477632dc7c250a4392e3f7b1f9d2ca28379760ba
Ricardo Rocha <rochar@users.noreply.github.com>
2023-08-08 09:23:21 +0100

    add tests to noxtypes factories

--- tests/Nox.Lib.Tests/Factories/Types/NoxTypeMoneyFactoryTests.cs
+++ tests/Nox.Lib.Tests/Factories/Types/NoxTypeMoneyFactoryTests.cs
@@ -1,28 +1,33 @@
-﻿using AutoFixture.Xunit2;
+﻿using FluentAssertions;
+using Moq;
 using Nox.Factories.Types;
 using Nox.Lib.Tests.FixtureConfig;
 using Nox.Solution;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Nox.Types;
 
 namespace Nox.Lib.Tests.Factories.Types
 {
+    public record MoneyDto(decimal Amount, CurrencyCode CurrencyCode);
     public class NoxTypeMoneyFactoryTests
     {
-        [Theory, AutoMoqData]
-        public void CreateNoxType_FromDto_IsValid(NoxSolution noxSolution)
+                [Theory, AutoMoqData]
+        public void CreateNoxType_FromDto_IsValid(NoxSolution noxSolution, EntityDefinitionFixture fixture)
         {
             // Arrange            
             NoxTypeMoneyFactory sut = new NoxTypeMoneyFactory(noxSolution);
+            var amount = 100M;
+            var currencyCode = CurrencyCode.AFN;
+            var dto = new MoneyDto(amount, currencyCode);
 
             // Act
-
+            var money = sut.CreateNoxType(fixture.EntityDefinition, fixture.PropertyName, dto);
 
             // Assert
-
-        }        
+            money.Should().NotBeNull();
+            money!.Amount.Should().Be(amount);
+            money!.CurrencyCode.Should().Be(currencyCode);
+        }
     }
+
+    
 }
