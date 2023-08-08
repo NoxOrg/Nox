d48f182d702d154bd105c0fed3ef6ffd149db0e0
Ricardo Rocha <rochar@users.noreply.github.com>
2023-08-08 08:19:15 +0100

    add Nox.Lib.Tests

---
+++ tests/Nox.Lib.Tests/Factories/Types/NoxTypeMoneyFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture.Xunit2;
+using Nox.Factories.Types;
+using Nox.Lib.Tests.FixtureConfig;
+using Nox.Solution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nox.Lib.Tests.Factories.Types
+{
+    public class NoxTypeMoneyFactoryTests
+    {
+        [Theory, AutoMoqData]
+        public void CreateNoxType_FromDto_IsValid(NoxSolution noxSolution)
+        {
+            // Arrange            
+            NoxTypeMoneyFactory sut = new NoxTypeMoneyFactory(noxSolution);
+
+            // Act
+
+
+            // Assert
+
+        }        
+    }
+}
