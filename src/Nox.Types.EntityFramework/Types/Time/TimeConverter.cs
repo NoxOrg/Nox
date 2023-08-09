c2a9d31f1a1ceea8ebf81f3fb7587a5f815e9e51
emir.sehovic <emir.sehovic@regus.com>
2023-08-09 14:09:16 +0200

    Fixing db number and time types

--- src/Nox.Types.EntityFramework/Types/Time/TimeConverter.cs
+++ src/Nox.Types.EntityFramework/Types/Time/TimeConverter.cs
@@ -11,5 +11,5 @@ public class TimeConverter : ValueConverter<Time, System.DateTime>
     /// <summary>
     /// Initializes a new instance of the <see cref="TimeConverter"/> class.
     /// </summary>
-    public TimeConverter() : base(time => time.Value.ToDateTime(), convertFromProviderExpression: timeValue => Time.FromDatabase(timeValue)) { }
+    public TimeConverter() : base(time => System.DateTime.SpecifyKind(time.Value.ToDateTime(), DateTimeKind.Utc), convertFromProviderExpression: timeValue => Time.FromDatabase(timeValue)) { }
 }
