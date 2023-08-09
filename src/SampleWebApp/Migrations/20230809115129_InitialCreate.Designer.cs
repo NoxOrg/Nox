a35f74646c9d750e098e05cc05540cd847a4b0f3
Dmytro Dorodnykh <Dmytro.Dorodnykh@iwgplc.com>
2023-08-09 15:05:59 +0300

    fix test run

--- src/SampleWebApp/Migrations/20230809115129_InitialCreate.Designer.cs
+++ src/SampleWebApp/Migrations/20230809115129_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using SampleWebApp.Infrastructure.Persistence;
 namespace SampleWebApp.Migrations
 {
     [DbContext(typeof(SampleWebAppDbContext))]
-    [Migration("20230809112808_InitialCreate")]
+    [Migration("20230809115129_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
