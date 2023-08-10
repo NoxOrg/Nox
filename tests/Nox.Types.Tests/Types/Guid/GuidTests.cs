f8e492939c96e3cec2f65c271640e4ce048e255f
Daniel Dutra <137419123+danield-ap@users.noreply.github.com>
2023-08-10 12:27:14 +0200

    MP-8380: create Nox Type Guid

---
+++ tests/Nox.Types.Tests/Types/Guid/GuidTests.cs
@@ -0,0 +1,48 @@
+﻿// ReSharper disable once CheckNamespace
+using FluentAssertions;
+
+namespace Nox.Types.Tests.Types;
+
+public class GuidTests
+{
+    [Fact]
+    public void FromString_Construct_ReturnNewGuid()
+    {
+        var textGuid = System.Guid.NewGuid().ToString();
+
+        var guid = Guid.From(textGuid);
+
+        guid.Should().NotBeNull();
+        guid.Value.Should().NotBeEmpty();
+        guid.ToString().Should().Be(textGuid);
+    }
+
+    [Fact]
+    public void Guid_ToString_ReturnSameGuid()
+    {
+        var textGuid = System.Guid.NewGuid().ToString();
+
+        var guid = Guid.From(textGuid);
+
+        guid.ToString().Should().Be(textGuid);
+    }
+
+    [Fact]
+    public void Guid_Equals_SameGuid_ReturnTrue()
+    {
+        var systemGuid = System.Guid.NewGuid();
+
+        var guid1 = Guid.From(systemGuid);
+        var guid2 = Guid.From(systemGuid);
+
+        guid1.Should().Be(guid2);
+    }
+
+    [Fact]
+    public void Guid_NewGuid_ReturnsValidGuid()
+    {
+        var guid = Guid.NewGuid().ToString();
+
+        System.Guid.TryParse(guid, out _).Should().BeTrue();
+    }
+}
