28de48773c995a2d902ad9f8fb0ab5f979554951
Daniel Dutra <137419123+danield-ap@users.noreply.github.com>
2023-08-14 22:24:43 +0200

    MP-8465: create color type factory

---
+++ src/Nox.Lib/Factories/Types/NoxTypeColorFactory.cs
@@ -0,0 +1,25 @@
+﻿using Nox.Solution;
+using Nox.Types;
+
+namespace Nox.Factories.Types
+{
+    public class NoxTypeColorFactory : NoxTypeFactoryBase<Color>
+    {
+        public NoxTypeColorFactory(NoxSolution solution) : base(solution)
+        {
+        }
+
+        public override Color? CreateNoxType(NoxSimpleTypeDefinition simpleTypeDefinition, dynamic? value)
+        {
+            switch (value)
+            {
+                case byte[] valueBytes:
+                    return Color.From(valueBytes);
+                case System.Drawing.Color valueColor:
+                    return Color.From(valueColor);
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ tests/Nox.Lib.Tests/Factories/Types/NoxTypeColorFactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Nox.Factories.Types;
+using Nox.Lib.Tests.FixtureConfig;
+using Nox.Solution;
+
+namespace Nox.Lib.Tests.Factories.Types;
+
+public class NoxTypeColorFactoryTests
+{
+    [Theory, AutoMoqData]
+    public void CreateNoxType_FromDto_WithArray_IsValid(NoxSolution noxSolution, EntityDefinitionFixture fixture)
+    {
+        // Arrange
+        NoxTypeColorFactory sut = new NoxTypeColorFactory(noxSolution);
+        var value = new byte[] { 1, 2, 3, 4 };
+
+        // Act
+        var entity = sut.CreateNoxType(fixture.EntityDefinition, fixture.PropertyName, value);
+
+        // Assert
+        entity.Should().NotBeNull();
+        entity!.Value.Should().BeEquivalentTo(value);
+    }
+
+    [Theory, AutoMoqData]
+    public void CreateNoxType_FromDto_WithColor_IsValid(NoxSolution noxSolution, EntityDefinitionFixture fixture)
+    {
+        // Arrange
+        NoxTypeColorFactory sut = new NoxTypeColorFactory(noxSolution);
+        var value = System.Drawing.Color.Lavender;
+
+        // Act
+        var entity = sut.CreateNoxType(fixture.EntityDefinition, fixture.PropertyName, value);
+
+        // Assert
+        entity.Should().NotBeNull();
+        entity!.Value.Should().BeEquivalentTo(new byte[] { value.A, value.R, value.G, value.B });
+    }
+}
