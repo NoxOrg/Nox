6f4a0e28fee8dfefbfca27fe5c7a94211884c72b
Dejan Bratic <dejan.bratic@igwplc.com>
2023-08-14 09:31:53 +0200

    added factory;

--- src/Nox.Lib/Factories/NoxTypeFactoryBase.cs
+++ src/Nox.Lib/Factories/NoxTypeFactoryBase.cs
@@ -5,7 +5,7 @@ namespace Nox.Factories
 {
     public abstract class NoxTypeFactoryBase<T> : INoxTypeFactory<T> where T : INoxType
     {
-        public NoxTypeFactoryBase(NoxSolution solution)
+        protected NoxTypeFactoryBase(NoxSolution solution)
         {
             Solution = solution;
         }
@@ -18,6 +18,7 @@ namespace Nox.Factories
 
             return CreateNoxType(attributeDefinition, value);
         }
+
         public abstract T? CreateNoxType(NoxSimpleTypeDefinition simpleTypeDefinition, dynamic? value);
     }
 }
