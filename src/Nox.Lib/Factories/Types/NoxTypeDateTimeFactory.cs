4ff73d73179ca09c00f0bff7027dcf142629d1f4
Khurram Sultan <khurram.sultan@authoritypartners.com>
2023-08-15 16:24:32 +0500

    MP-8514: NOX - Factory for converting Primitive types into Nox types for "DateTime"

---
+++ src/Nox.Lib/Factories/Types/NoxTypeDateTimeFactory.cs
@@ -0,0 +1,27 @@
+﻿using Nox.Solution;
+using Nox.Types;
+
+namespace Nox.Factories.Types;
+
+/// <summary>
+/// The nox type date time factory.
+/// </summary>
+public class NoxTypeDateTimeFactory : NoxTypeFactoryBase<Nox.Types.DateTime>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NoxTypeDateTimeFactory"/> class.
+    /// </summary>
+    /// <param name="solution">The solution.</param>
+    public NoxTypeDateTimeFactory(NoxSolution solution) : base(solution)
+    {
+    }
+
+    /// <summary>
+    /// Creates the nox type.
+    /// </summary>
+    /// <param name="simpleTypeDefinition">The simple type definition.</param>
+    /// <param name="value">The value.</param>
+    /// <returns>A Nox.Types.DateTime? .</returns>
+    public override Nox.Types.DateTime? CreateNoxType(NoxSimpleTypeDefinition simpleTypeDefinition, dynamic? value)
+        => value is null ? null : Nox.Types.DateTime.From(value!, simpleTypeDefinition.DateTimeTypeOptions ?? new DateTimeTypeOptions());
+}
