b54a9fc196c4c2d69fb6b9c8f77f9779fe947fba
Ajla Becic <ajla.becic@regus.com>
2023-08-15 10:11:15 +0200

    add converter

---
+++ src/Nox.Types.EntityFramework/Types/DateTime/DateTimeConverter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Nox.Types.EntityFramework.Types;
+
+public class DateTimeConverter : ValueConverter<DateTimeOffset, System.DateTime>
+{
+    public DateTimeConverter()
+        : base(
+            d => d.UtcDateTime,
+            d => new DateTimeOffset(d, TimeSpan.Zero))
+    {
+    }
+}
--- src/Nox.Types.EntityFramework/Types/DateTime/DateTimeDatabaseConfigurator.cs
+++ src/Nox.Types.EntityFramework/Types/DateTime/DateTimeDatabaseConfigurator.cs
@@ -16,7 +16,7 @@ public class DateTimeDatabaseConfigurator : INoxTypeDatabaseConfigurator
             .OwnsOne(typeof(DateTime), property.Name, dtr =>
             {
                 dtr.Property(nameof(DateTime.DateTimeValue))
-                    .HasConversion<DateTimeRangeConverter>();
+                    .HasConversion<DateTimeConverter>();
 
                 dtr.Property(nameof(DateTime.TimeZoneOffset))
                     .UsePropertyAccessMode(PropertyAccessMode.Property);
