6605bf1840421ec5d592c321cbbe2a34774ed87e
Khurram Sultan <khurram.sultan@authoritypartners.com>
2023-08-15 17:25:40 +0500

    MP-8521: NOX - Factory for converting Primitive types into Nox types for "DateTimeDuration"

---
+++ src/Nox.Lib/Factories/Types/NoxTypeDateTimeDurationFactory .cs
@@ -0,0 +1,27 @@
+﻿using Nox.Solution;
+using Nox.Types;
+
+namespace Nox.Factories.Types;
+
+/// <summary>
+/// The nox type date time duration factory.
+/// </summary>
+public class NoxTypeDateTimeDurationFactory : NoxTypeFactoryBase<Nox.Types.DateTimeDuration>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NoxTypeDateTimeDurationFactory"/> class.
+    /// </summary>
+    /// <param name="solution">The solution.</param>
+    public NoxTypeDateTimeDurationFactory(NoxSolution solution) : base(solution)
+    {
+    }
+
+    /// <summary>
+    /// Creates the nox type.
+    /// </summary>
+    /// <param name="simpleTypeDefinition">The simple type definition.</param>
+    /// <param name="value">The value.</param>
+    /// <returns>A DateTimeDuration? .</returns>
+    public override DateTimeDuration? CreateNoxType(NoxSimpleTypeDefinition simpleTypeDefinition, dynamic? value)
+        => value is null ? null : DateTimeDuration.From(value, simpleTypeDefinition.DateTimeDurationTypeOptions ?? new DateTimeDurationTypeOptions());
+}
