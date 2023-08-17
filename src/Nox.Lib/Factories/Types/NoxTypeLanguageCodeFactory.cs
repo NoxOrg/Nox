fb37edc3dc6f6a58bdc073bf7fafb273940de0f1
Khurram Sultan <khurram.sultan@authoritypartners.com>
2023-08-17 12:21:46 +0500

    MP-8605: Implemented Language Code Factory

---
+++ src/Nox.Lib/Factories/Types/NoxTypeLanguageCodeFactory.cs
@@ -0,0 +1,22 @@
+﻿using Nox.Solution;
+using Nox.Types;
+
+namespace Nox.Factories.Types;
+
+/// <summary>
+/// The nox type language code factory.
+/// </summary>
+public class NoxTypeLanguageCodeFactory : NoxTypeFactoryBase<LanguageCode>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NoxTypeLanguageCodeFactory"/> class.
+    /// </summary>
+    /// <param name="solution">The solution.</param>
+    public NoxTypeLanguageCodeFactory(NoxSolution solution) : base(solution)
+    {
+    }
+
+    /// <inheritdoc />
+    public override LanguageCode? CreateNoxType(NoxSimpleTypeDefinition simpleTypeDefinition, dynamic? value)
+        => value is null ? null : LanguageCode.From(value);
+}
