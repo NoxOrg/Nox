917e9f9e9a9addefff015f29d9f022b496428144
DanielHodgeGH <github@danielhodge.co.uk>
2023-08-17 17:18:50 +0100

    feature/482 initial skeleton Cryptocash.Api phase1

---
+++ src/Cryptocash.Api/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Dto.CustomerUpdateDto.g.cs
@@ -0,0 +1,37 @@
+﻿// Generated
+
+#nullable enable
+
+using Nox.Abstractions;
+using Nox.Types;
+using System.Collections.Generic;
+
+namespace CryptocashApi.Application.Dto; 
+
+/// <summary>
+/// Customer definition and related data.
+/// </summary>
+public partial class CustomerUpdateDto
+{
+    //TODO Add owned Entities and update odata endpoints
+    /// <summary>
+    /// The customer's first name (Required).
+    /// </summary>
+    public System.String FirstName { get; set; } = default!;
+    /// <summary>
+    /// The customer's last name (Required).
+    /// </summary>
+    public System.String LastName { get; set; } = default!;
+    /// <summary>
+    /// The customer's email (Required).
+    /// </summary>
+    public System.String Email { get; set; } = default!;
+    /// <summary>
+    /// The customer's address (Required).
+    /// </summary>
+    public StreetAddressDto Address { get; set; } = default!;
+    /// <summary>
+    /// The customer's mobile number (Optional).
+    /// </summary>
+    public System.String? MobileNumber { get; set; } 
+}
---
+++ src/Cryptocash.Api/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Factories.CustomerMapper.g.cs
@@ -0,0 +1,54 @@
+﻿// Generated
+
+#nullable enable
+
+using MediatR;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Nox.Solution;
+using Nox.Domain;
+using Nox.Factories;
+using Nox.Types;
+using Nox.Application;
+using Nox.Extensions;
+using CryptocashApi.Application.Dto;
+using CryptocashApi.Domain;
+
+
+namespace CryptocashApi.Application;
+
+public class CustomerMapper: EntityMapperBase<Customer>
+{
+    public  CustomerMapper(NoxSolution noxSolution, IServiceProvider serviceProvider): base(noxSolution, serviceProvider) { }
+
+    public override void MapToEntity(Customer entity, Entity entityDefinition, dynamic dto)
+    {
+    #pragma warning disable CS0168 // Variable is declared but never used        
+        dynamic? noxTypeValue;
+    #pragma warning restore CS0168 // Variable is declared but never used
+    
+        noxTypeValue = CreateNoxType<Nox.Types.Text>(entityDefinition,"FirstName",dto.FirstName);
+        if(noxTypeValue != null)
+        {        
+            entity.FirstName = noxTypeValue;
+        }
+        noxTypeValue = CreateNoxType<Nox.Types.Text>(entityDefinition,"LastName",dto.LastName);
+        if(noxTypeValue != null)
+        {        
+            entity.LastName = noxTypeValue;
+        }
+
+        // TODO map Email Email remaining types and remove if else
+        noxTypeValue = CreateNoxType<Nox.Types.StreetAddress>(entityDefinition,"Address",dto.Address);
+        if(noxTypeValue != null)
+        {        
+            entity.Address = noxTypeValue;
+        }
+        noxTypeValue = CreateNoxType<Nox.Types.PhoneNumber>(entityDefinition,"MobileNumber",dto.MobileNumber);
+        if(noxTypeValue != null)
+        {        
+            entity.MobileNumber = noxTypeValue;
+        }
+    }
+}
