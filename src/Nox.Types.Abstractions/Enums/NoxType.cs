6e5f0cc835b2ac8451c0b32128734acf7f3f3253
Khurram Sultan <khurram.sultan@authoritypartners.com>
2023-08-18 16:05:40 +0500

    MP-8218: Reorder Nox types based on compound types and alphabetical order.

--- src/Nox.Types.Abstractions/Enums/NoxType.cs
+++ src/Nox.Types.Abstractions/Enums/NoxType.cs
@@ -1,6 +1,9 @@
 using System;
 
 namespace Nox.Types;
+/// <summary>
+/// The nox type.
+/// </summary>
 
 public enum NoxType : uint
 {
@@ -10,6 +13,12 @@ public enum NoxType : uint
     Object = 2524865811,
 
     // Compound Types - requires multiple fields to persist
+
+    [CompoundType]
+    [CompoundComponent("Start", typeof(DateTime))]
+    [CompoundComponent("End", typeof(DateTime))]
+    DateTimeRange = 3837929056,
+
     [CompoundType]
     // Entity depends on the concrete instance and if it supports multiple keys or not!
     [CompoundComponent("Type", typeof(string))]
@@ -17,8 +26,25 @@ public enum NoxType : uint
     Entity = 2276495181,
 
     [CompoundType]
-    [CompoundComponent("Latitude",typeof(double))]
-    [CompoundComponent("Longitude",typeof(double))]
+    [CompoundComponent("Url", typeof(string))]
+    [CompoundComponent("PrettyName", typeof(string))]
+    [CompoundComponent("SizeInBytes", typeof(uint))]
+    File = 612041382,
+
+    [CompoundType(Read = false, Update = false)]
+    [CompoundComponent("HashText", typeof(string))]
+    [CompoundComponent("Salt", typeof(string))]
+    HashedText = 3656553818,
+
+    [CompoundType]
+    [CompoundComponent("Url", typeof(string))]
+    [CompoundComponent("PrettyName", typeof(string))]
+    [CompoundComponent("SizeInBytes", typeof(uint))]
+    Image = 3650429592,
+
+    [CompoundType]
+    [CompoundComponent("Latitude", typeof(decimal))]
+    [CompoundComponent("Longitude", typeof(decimal))]
     LatLong = 4061881939,
 
     [CompoundType]
@@ -26,8 +52,13 @@ public enum NoxType : uint
     [CompoundComponent("CurrencyCode", typeof(CurrencyCode))]
     Money = 3500951620,
 
+    [CompoundType(Read = false, Update = false)]
+    [CompoundComponent("HashedPassword", typeof(string))]
+    [CompoundComponent("Salt", typeof(string))]
+    Password = 1755902638,
+
     [CompoundType]
-    [CompoundComponent("StreetNumber",typeof(int))]
+    [CompoundComponent("StreetNumber", typeof(int))]
     [CompoundComponent("AddressLine1", typeof(string))]
     [CompoundComponent("AddressLine2", typeof(string))]
     [CompoundComponent("Route", typeof(string))]
@@ -45,20 +76,9 @@ public enum NoxType : uint
     TranslatedText = 967269030,
 
     [CompoundType]
-    [CompoundComponent("From", typeof(DateTimeOffset))]
-    [CompoundComponent("To", typeof(DateTimeOffset))]
-    DateTimeRange = 3837929056,
-
-    [CompoundType]
     [CompoundComponent("Number", typeof(string))]
     [CompoundComponent("CountryCode2", typeof(string))]
     VatNumber = 1055627262,
-
-    [CompoundType]
-    [CompoundComponent("Url", typeof(string))]
-    [CompoundComponent("PrettyName", typeof(string))]
-    [CompoundComponent("SizeInBytes", typeof(ulong))]
-    File = 612041382,
 
     // Simple Types
 
@@ -71,7 +91,7 @@ public enum NoxType : uint
     [SimpleType(typeof(bool))]
     Boolean = 2157507194,
 
-    [SimpleType(typeof(string))]
+    [SimpleType(typeof(byte[]))]
     Color = 1567592592,
 
     [SimpleType(typeof(string))]
@@ -95,16 +115,10 @@ public enum NoxType : uint
     [SimpleType(typeof(DateTime))]
     Date = 463099971,
 
-    [SimpleType(typeof(byte))]
-    Month = 4186740261,
-
-    [SimpleType(typeof(short))]
-    Year = 3709785124,
-
-    [SimpleType(typeof(DateTimeOffset))]
+    [SimpleType(typeof(DateTime))]
     DateTime = 2998644573,
 
-    [SimpleType(typeof(TimeSpan))]
+    [SimpleType(typeof(long))]
     DateTimeDuration = 2070327063,
 
     // Propose to store DateTimeSchedule as iCal
@@ -114,16 +128,16 @@ public enum NoxType : uint
     [SimpleType(typeof(string))]
     DateTimeSchedule = 3271762546,
 
-    [SimpleType(typeof(byte))]
+    [SimpleType(typeof(int))]
     DayOfWeek = 491349974,
 
-    [SimpleType(typeof(double))]
+    [SimpleType(typeof(decimal))]
     Distance = 1460893529,
 
     [SimpleType(typeof(string))]
     Email = 3393987164,
 
-    [SimpleType(typeof(string), Read = false, Update = false)]
+    [SimpleType(typeof(byte[]), Read = false, Update = false)]
     EncryptedText = 1841598137,
 
     [SimpleType(typeof(string))]
@@ -132,22 +146,9 @@ public enum NoxType : uint
     [SimpleType(typeof(Guid))]
     Guid = 1043908053,
 
-    [SimpleType(typeof(Guid))]
-    DatabaseGuid = 2076277156,
-
-    [CompoundType(Read = false, Update = false)]
-    [CompoundComponent("HashText", typeof(string))]
-    [CompoundComponent("Salt", typeof(string))]
-    HashedText = 3656553818,
 
     [SimpleType(typeof(string))]
     Html = 2477180992,
-
-    [CompoundType]
-    [CompoundComponent("Url", typeof(string))]
-    [CompoundComponent("PrettyName", typeof(string))]
-    [CompoundComponent("SizeInBytes", typeof(int))]
-    Image = 3650429592,
 
     [SimpleType(typeof(string))]
     InternetDomain = 1821438912,
@@ -164,7 +165,7 @@ public enum NoxType : uint
     [SimpleType(typeof(string))]
     LanguageCode = 3654380314,
 
-    [SimpleType(typeof(double))]
+    [SimpleType(typeof(decimal))]
     Length = 1024984906,
 
     [SimpleType(typeof(string))]
@@ -173,16 +174,16 @@ public enum NoxType : uint
     [SimpleType(typeof(string))]
     Markdown = 2745254000,
 
+    [SimpleType(typeof(byte))]
+    Month = 4186740261,
+
     [SimpleType(typeof(uint))]
     Nuid = 3304944825,
 
     [SimpleType(typeof(NumberTypeComponentsDiscover))]
     Number = 4223714796,
 
-    [CompoundType(Read = false, Update = false)]
-    [CompoundComponent("HashedPassword", typeof(string))]
-    [CompoundComponent("Salt", typeof(string))]
-    Password = 1755902638,
+
 
     [SimpleType(typeof(float))]
     Percentage = 3986447473,
@@ -190,13 +191,13 @@ public enum NoxType : uint
     [SimpleType(typeof(string))]
     PhoneNumber = 3655711066,
 
-    [SimpleType(typeof(float))]
+    [SimpleType(typeof(decimal))]
     Temperature = 1744108624,
 
     [SimpleType(typeof(string))]
     Text = 1432028016,
 
-    [SimpleType(typeof(DateTimeOffset))]
+    [SimpleType(typeof(TimeSpan))]
     Time = 2288042805,
 
     [SimpleType(typeof(string))]
@@ -211,12 +212,15 @@ public enum NoxType : uint
     [SimpleType(typeof(string))]
     User = 2972321043,
 
-    [SimpleType(typeof(double))]
+    [SimpleType(typeof(decimal))]
     Volume = 1729151620,
 
-    [SimpleType(typeof(double))]
+    [SimpleType(typeof(decimal))]
     Weight = 760317285,
 
     [SimpleType(typeof(string))]
-    Yaml = 3788751714
+    Yaml = 3788751714,
+
+    [SimpleType(typeof(ushort))]
+    Year = 3709785124
 }
