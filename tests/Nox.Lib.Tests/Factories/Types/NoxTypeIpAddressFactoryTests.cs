04c89cbd20437a3873569b1e9f07ff13b26dd7e2
danield-ap <137419123+danield-ap@users.noreply.github.com>
2023-08-18 11:33:39 +0300

    MP-8584 - Factory for converting Primitive types into "IpAddress" (#586)

---
+++ tests/Nox.Lib.Tests/Factories/Types/NoxTypeIpAddressFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Nox.Factories.Types;
+using Nox.Lib.Tests.FixtureConfig;
+using Nox.Solution;
+
+namespace Nox.Lib.Tests.Factories.Types;
+
+public class NoxTypeIpAddressFactoryTests
+{
+    [Theory, AutoMoqData]
+    public void CreateNoxType_FromDto_IsValid(NoxSolution noxSolution, EntityDefinitionFixture fixture)
+    {
+        // Arrange
+        NoxTypeIpAddressFactory sut = new NoxTypeIpAddressFactory(noxSolution);
+        var value = "127.0.0.1";
+
+        // Act
+        var entity = sut.CreateNoxType(fixture.EntityDefinition, fixture.PropertyName, value);
+
+        // Assert
+        entity.Should().NotBeNull();
+        entity!.Value.Should().Be(value);
+    }
+}
