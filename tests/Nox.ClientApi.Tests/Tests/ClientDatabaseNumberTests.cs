a0e9e0fd11dd697c0c2e305aa29cbf764edcfedf
Ricardo Rocha <rochar@users.noreply.github.com>
2023-08-19 15:20:58 +0100

    add test to required field model state validator

--- tests/Nox.ClientApi.Tests/Tests/ClientDatabaseNumberTests.cs
+++ tests/Nox.ClientApi.Tests/Tests/ClientDatabaseNumberTests.cs
@@ -4,6 +4,7 @@ using ClientApi.Application.Dto;
 using ClientApi.Presentation.Api.OData;
 using Microsoft.AspNetCore.OData.Results;
 using AutoFixture;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Nox.ClientApi.Tests.Tests
 {
@@ -19,12 +20,33 @@ namespace Nox.ClientApi.Tests.Tests
                 new ClientDatabaseNumberCreateDto
                 {
                     Name = apiFixture.Fixture.Create<string>()
-                }); 
+                });
             //Assert
             result.Should().NotBeNull();
             result.Should()
                 .BeOfType<CreatedODataResult<ClientDatabaseNumberKeyDto>>()
                 .Which.Entity.keyId.Should().BeGreaterThan(0);
+        }
+
+        [Theory, AutoMoqData]
+        public async void Post_IfNoRequireField_ThrowsException(ApiFixture apiFixture)
+        {
+            
+            // Arrange  
+            Func<Task> action = () =>
+            {
+                return apiFixture.ClientDatabaseNumbersController!.Post(
+                new ClientDatabaseNumberCreateDto
+                {
+                    //Name = null
+                });
+            };
+
+            // Act 
+            // Assert
+            //await action.Should().ThrowAsync<ModelValidationException?>();
+            //This is incorrect is getting to the insert command should fail model validation
+            await action.Should().ThrowAsync<Microsoft.EntityFrameworkCore.DbUpdateException>();            
         }
     }
 }
