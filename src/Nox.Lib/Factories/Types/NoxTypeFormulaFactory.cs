de177dc578ac469542d071328293ab336d246808
Daniel Dutra <137419123+danield-ap@users.noreply.github.com>
2023-08-22 11:25:28 +0300

    MP-8556 - Factory for converting Primitive types into "Formula"

---
+++ src/Nox.Lib/Factories/Types/NoxTypeFormulaFactory.cs
@@ -0,0 +1,24 @@
+﻿using Nox.Solution;
+using Nox.Types;
+
+namespace Nox.Factories.Types;
+
+public class NoxTypeFormulaFactory : NoxTypeFactoryBase<Formula>
+{
+    public NoxTypeFormulaFactory(NoxSolution solution) : base(solution)
+    {
+    }
+
+    public override Formula? CreateNoxType(NoxSimpleTypeDefinition simpleTypeDefinition, dynamic? value)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+
+        var formulaValue = simpleTypeDefinition.FormulaTypeOptions ?? new FormulaTypeOptions();
+        formulaValue.Expression = value;
+
+        return Formula.From(formulaValue);
+    }
+}
