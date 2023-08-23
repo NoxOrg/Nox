c72df2dcc314735b14d2edca4b2804c4f72ee864
Ricardo Rocha <rochar@users.noreply.github.com>
2023-08-23 08:29:34 +0100

    add database attribute configuration for VatNumber
    for now is hardcoded until we need more then one configuration

--- src/Nox.Types.EntityFramework/Abstractions/NoxDtoDatabaseConfigurator.cs
+++ src/Nox.Types.EntityFramework/Abstractions/NoxDtoDatabaseConfigurator.cs
@@ -2,6 +2,7 @@
 using Nox.Types.EntityFramework.EntityBuilderAdapter;
 using Nox.Solution.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Nox.Types.EntityFramework.Abstractions;
 
@@ -9,17 +10,35 @@ public class NoxDtoDatabaseConfigurator : INoxDtoDatabaseConfigurator
 {
     public void ConfigureDto(NoxSolutionCodeGeneratorState codeGeneratorState, IEntityBuilder builder, Entity entity)
     {
-        foreach (var key in entity.Keys!)
+        ConfigureKeys(builder, entity);
+
+        ConfigureAttributes(codeGeneratorState, builder, entity);
+
+        ConfigureOwnedRelations(codeGeneratorState, builder, entity);
+    }
+
+    private static void ConfigureAttributes(NoxSolutionCodeGeneratorState codeGeneratorState, IEntityBuilder builder, Entity entity)
+    {
+        foreach (var attribute in entity.Attributes!)
         {
-            builder.HasKey(key.Name);
+            // TODO inject configuration per attribute or use NoxType metadata to it
+
+            if (attribute.Type == NoxType.VatNumber)
+            {
+                var compoundDtoType = codeGeneratorState.GetEntityDtoType("VatNumberDto")!;
+
+                builder.OwnsOne(compoundDtoType, attribute.Name)
+                    .Property(nameof(VatNumber.CountryCode))
+                    .HasConversion(new EnumToStringConverter<CountryCode>());
+            }
         }
-
-        var keyNames = entity.Keys.Select(x => x.Name);
-
-       
-
+    }
+    private static void ConfigureOwnedRelations(NoxSolutionCodeGeneratorState codeGeneratorState, IEntityBuilder builder, Entity entity)
+    {
         if (entity.OwnedRelationships != null)
         {
+            var keyNames = entity.Keys!.Select(x => x.Name);
+
             //#pragma warning disable S3267 // Loops should be simplified with "LINQ" expressions
             foreach (var ownedRelationship in entity.OwnedRelationships)
             //#pragma warning restore S3267 // Loops should be simplified with "LINQ" expressions
@@ -32,13 +51,21 @@ public class NoxDtoDatabaseConfigurator : INoxDtoDatabaseConfigurator
                 builder.OwnsMany(relatedEntityDtoType,
                     ownedRelationship.Related.Entity.PluralName,
                     owned =>
-                {
-                    owned.WithOwner().HasForeignKey($"{entity.Name}Id");
-                    owned.HasKey(string.Join(",", keyNames));
-                    owned.ToTable(ownedRelationship.Related.Entity.Name);
-                });
+                    {
+                        owned.WithOwner().HasForeignKey($"{entity.Name}Id");
+                        owned.HasKey(string.Join(",", keyNames));
+                        owned.ToTable(ownedRelationship.Related.Entity.Name);
+                    });
 
             }
+        }
+    }
+
+    private static void ConfigureKeys(IEntityBuilder builder, Entity entity)
+    {
+        foreach (var key in entity.Keys!)
+        {
+            builder.HasKey(key.Name);
         }
     }
 }
