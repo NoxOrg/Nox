ac98a0c8744b6c127f2b4d1b2d6d4973c0e3affb
Ricardo Rocha <rochar@users.noreply.github.com>
2023-08-23 14:46:55 +0100

    system test for add security and validation to commands and queries

---
+++ tests/Nox.ClientApi.Tests/Application/Queries/GetClientDatabaseNumberByIdQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MediatR;
+using ClientApi.Application.Queries;
+
+namespace Nox.ClientApi.Tests.Application.Queries
+{
+
+    /// <summary>
+    /// Example of a validator, that is executed before a Query or Command Handler for security checks    
+    /// </summary>
+    public class GetClientDatabaseNumberByIdQueryValidator : AbstractValidator<GetClientDatabaseNumberByIdQuery>
+    {
+        public GetClientDatabaseNumberByIdQueryValidator(ILogger<GetClientDatabaseNumberByIdQuery> logger)
+        {            
+            RuleFor(query => query.keyId).Must(key => key < 50).WithMessage("No permissions for keys greater then 50");
+        }
+    }
+}
---
+++ tests/Nox.ClientApi.Tests/Tests/Application/Query/GetClientDatabaseNumberByIdQueryValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Nox.ClientApp.Tests.FixtureConfig;
+using ClientApi.Application.Dto;
+using ClientApi.Presentation.Api.OData;
+using Microsoft.AspNetCore.OData.Results;
+using AutoFixture;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Nox.Types;
+using Nox.ClientApi.Tests.Tests;
+using FluentValidation.TestHelper;
+
+namespace Nox.ClientApi.Tests.Tests.Controllers
+{
+    [Collection("Sequential")]
+    public class GetClientDatabaseNumberByIdQueryValidatorTests
+    {
+        /// <summary>
+        /// Test a Query or Command Validation, that can be used for security checks
+        /// </summary>        
+        [Theory, AutoMoqData]        
+        public async void Get_NumberWithKeyGreaterThen50_ShouldFailSecurityValidation(ApiFixture apiFixture)
+        {
+            // Arrange            
+
+            // Act 
+            Func<Task> taskResult = async () => { await apiFixture.ClientDatabaseNumbersController!.Get(51); };
+
+            //Assert
+            await taskResult.Should().ThrowAsync<TypeValidationException>();
+        }
+    }
+}
