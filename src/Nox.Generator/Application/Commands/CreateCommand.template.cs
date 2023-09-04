fa7ca31f71472465669008d42c21fc24b28dc7cc
Ricardo Rocha <rochar@users.noreply.github.com>
2023-09-04 17:02:34 +0100

    remove entityfactory and replace by EntityCreateDto.ToEntity method

--- src/Nox.Generator/Application/Commands/CreateCommand.template.cs
+++ src/Nox.Generator/Application/Commands/CreateCommand.template.cs
@@ -24,16 +24,13 @@ public record Create{{entity.Name}}Command({{entity.Name}}CreateDto EntityDto) :
 public partial class Create{{entity.Name}}CommandHandler: CommandBase<Create{{entity.Name}}Command,{{entity.Name}}>, IRequestHandler <Create{{entity.Name}}Command, {{entity.Name}}KeyDto>
 {
 	public {{codeGeneratorState.Solution.Name}}DbContext DbContext { get; }
-	public IEntityFactory<{{entity.Name}}CreateDto,{{entity.Name}}> EntityFactory { get; }
 
 	public Create{{entity.Name}}CommandHandler(
 		{{codeGeneratorState.Solution.Name}}DbContext dbContext,
 		NoxSolution noxSolution,
-		IServiceProvider serviceProvider,
-		IEntityFactory<{{entity.Name}}CreateDto,{{entity.Name}}> entityFactory): base(noxSolution, serviceProvider)
+		IServiceProvider serviceProvider): base(noxSolution, serviceProvider)
 	{
 		DbContext = dbContext;
-		EntityFactory = entityFactory;
 	}
 
 	public async Task<{{entity.Name}}KeyDto> Handle(Create{{entity.Name}}Command request, CancellationToken cancellationToken)
@@ -41,7 +38,7 @@ public partial class Create{{entity.Name}}CommandHandler: CommandBase<Create{{en
 		cancellationToken.ThrowIfCancellationRequested();
 		OnExecuting(request);
 
-		var entityToCreate = EntityFactory.CreateEntity(request.EntityDto);
+		var entityToCreate = request.EntityDto.ToEntity();
 
 		{{- for key in entity.Keys ~}}
 		{{- if key.Type == "Nuid" }}
