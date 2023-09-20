e38aee337dd083612fb2101c5ccde6dff6e013d6
Daniel Dutra <137419123+danield-ap@users.noreply.github.com>
2023-09-20 11:11:20 +0300

    Refactor NoxWebApplicationExtensionGenerator to use TemplateCodeBuilder

---
+++ src/Nox.Generator/Application/NoxWebApplicationBuilderExtension.template.cs
@@ -0,0 +1,27 @@
+﻿// Generated
+
+#nullable enable
+{{~ for namespace in namespaces}}
+using {{ namespace ~}};
+{{- end }}
+
+public static class NoxWebApplicationBuilderExtension
+{
+    public static IServiceCollection AddNox(this IServiceCollection services)
+    {
+        return services.AddNox(null);
+    }
+
+    public static IServiceCollection AddNox(this IServiceCollection services, Action<ODataModelBuilder>? configureOData)
+    {
+        services.AddNoxLib(Assembly.GetExecutingAssembly());
+        services.AddNoxOdata(configureOData);
+        services.AddSingleton(typeof(INoxClientAssemblyProvider), s => new NoxClientAssemblyProvider(Assembly.GetExecutingAssembly()));
+        services.AddSingleton<DbContextOptions<{{ dbContext }}>>();
+        services.AddSingleton<INoxDatabaseConfigurator, {{ dbProvider }}>();
+        services.AddSingleton<INoxDatabaseProvider, {{ dbProvider }}>();
+        services.AddDbContext<{{ dbContext }}>();
+        services.AddDbContext<DtoDbContext>();
+        return services;
+    }
+}
--- src/Nox.Generator/Application/NoxWebApplicationExtensionGenerator.cs
+++ src/Nox.Generator/Application/NoxWebApplicationExtensionGenerator.cs
@@ -1,10 +1,7 @@
 using System.Collections.Generic;
-using System.Text;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 using Nox.Generator.Common;
 using Nox.Solution;
-using Scriban;
 
 namespace Nox.Generator.Application;
 
@@ -16,36 +13,7 @@ internal class NoxWebApplicationExtensionGenerator : INoxCodeGenerator
     {
         context.CancellationToken.ThrowIfCancellationRequested();
 
-        var scribanTemplate = Template.Parse(
-            @"// Generated
-
-#nullable enable
-{{~ for namespace in namespaces}}
-using {{ namespace ~}};
-{{- end }}
-
-public static class NoxWebApplicationBuilderExtension
-{
-    public static IServiceCollection AddNox(this IServiceCollection services)
-    {
-        return services.AddNox(null);
-    }
-
-    public static IServiceCollection AddNox(this IServiceCollection services, Action<ODataModelBuilder>? configureOData)
-    {
-        services.AddNoxLib(Assembly.GetExecutingAssembly());
-        services.AddNoxOdata(configureOData);
-        services.AddSingleton(typeof(INoxClientAssemblyProvider), s => new NoxClientAssemblyProvider(Assembly.GetExecutingAssembly()));
-        services.AddSingleton<DbContextOptions<{{ dbContext }}>>();
-        services.AddSingleton<INoxDatabaseConfigurator, {{ dbProvider }}>();
-        services.AddSingleton<INoxDatabaseProvider, {{ dbProvider }}>();
-        services.AddDbContext<{{ dbContext }}>();
-        services.AddDbContext<DtoDbContext>();
-        return services;
-    }
-}
-"
-        );
+        var namePrefix = "Application";
 
         var solution = codeGeneratorState.Solution;
 
@@ -86,14 +54,12 @@ public static class NoxWebApplicationBuilderExtension
 
         namespaces.Sort();
 
-        object model = new
-        {
-            namespaces,
-            dbProvider,
-            dbContext = $"{solution.Name}DbContext"
-        };
-
-        context.AddSource("Application.NoxWebApplicationExtensions.g.cs",
-            SourceText.From(scribanTemplate.Render(model, member => member.Name), Encoding.UTF8));
+        new TemplateCodeBuilder(context, codeGeneratorState)
+            .WithClassName("NoxWebApplicationExtensions")
+            .WithFileNamePrefix(namePrefix)
+            .WithObject("namespaces", namespaces)
+            .WithObject("dbProvider", dbProvider)
+            .WithObject("dbContext", $"{solution.Name}DbContext")
+            .GenerateSourceCodeFromResource($"{namePrefix}.NoxWebApplicationBuilderExtension");
     }
 }
