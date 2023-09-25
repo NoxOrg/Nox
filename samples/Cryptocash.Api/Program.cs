aea8f852596489e44d197b38f227dd5396685aca
emir.sehovic <emir.sehovic@regus.com>
2023-09-25 16:02:17 +0200

    Rmoved json options

--- samples/Cryptocash.Api/Program.cs
+++ samples/Cryptocash.Api/Program.cs
@@ -6,12 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-    });
+builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
