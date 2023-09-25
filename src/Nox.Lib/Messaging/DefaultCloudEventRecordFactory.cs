7aa72a57b37d3a9bc706d030f9d9a410f6472eba
Ricardo Rocha <rochar@users.noreply.github.com>
2023-09-25 15:59:14 +0100

    setup factory to generate cloud events records

---
+++ src/Nox.Lib/Messaging/DefaultCloudEventRecordFactory.cs
@@ -0,0 +1,18 @@
+﻿using Nox.Abstractions;
+using Nox.Application;
+
+namespace Nox.Messaging
+{
+    internal class DefaultCloudEventRecordFactory : ICloudEventRecordFactory
+    {
+
+        public DefaultCloudEventRecordFactory()
+        {
+        }
+
+        public CloudEventRecord<T> CreateRecordForIntegrationEvent<T>(T integrationEvent) where T : IIntegrationEvent
+        {
+            return new CloudEventRecord<T>(integrationEvent);
+        }
+    }
+}
