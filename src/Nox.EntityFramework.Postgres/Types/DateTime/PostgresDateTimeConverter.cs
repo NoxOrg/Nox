618d90bb784f528d5dde2161bb78bf3ded8a69ad
Andrii Dehtiarov <Andrii.Dehtiarov@iwgplc.com>
2023-09-27 13:03:33 +0300

    Merge remote-tracking branch 'remotes/origin/main' into issues/829-Postgre-Test-Provider
    # Conflicts:
    #	src/Nox.Types.EntityFramework/Configurations/NoxDatabaseConfigurator.cs

--- src/Nox.EntityFramework.Postgres/Types/DateTime/PostgresDateTimeConverter.cs
+++ src/Nox.EntityFramework.Postgres/Types/DateTime/PostgresDateTimeConverter.cs
@@ -7,12 +7,6 @@ public class PostgresDateTimeConverter : ValueConverter<Nox.Types.DateTime, Syst
     public PostgresDateTimeConverter()
         : base(
             dateTime => dateTime.Value.UtcDateTime,
-            dateTimeValue => Test(dateTimeValue))
+            dateTimeValue => Nox.Types.DateTime.FromDatabase(new DateTimeOffset(dateTimeValue, TimeSpan.Zero)))
     { }
-
-    private static Nox.Types.DateTime Test(System.DateTime dateTimeValue)
-    {
-        var pp = Nox.Types.DateTime.FromDatabase(new DateTimeOffset(dateTimeValue, TimeSpan.Zero));
-        return Nox.Types.DateTime.FromDatabase(new DateTimeOffset(dateTimeValue, TimeSpan.Zero));
-    }
 }
