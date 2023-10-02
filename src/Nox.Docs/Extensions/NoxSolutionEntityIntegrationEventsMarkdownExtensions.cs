7ae165d8cc2271c2438018caf4b5406fd50b343a
Dejan Bratic <dejan.bratic@igwplc.com>
2023-10-02 13:59:51 +0200

    initial commit;

---
+++ src/Nox.Docs/Extensions/NoxSolutionEntityIntegrationEventsMarkdownExtensions.cs
@@ -0,0 +1,79 @@
+﻿using Nox.Docs.Models;
+using Nox.Solution;
+using Scriban;
+
+namespace Nox.Docs.Extensions;
+
+public static class NoxSolutionEntityIntegrationEventsMarkdownExtensions
+{
+    private const string ResourceName = "Nox.Docs.Templates.IntegrationEvents.template.md";
+
+    public static MarkdownFile ToMarkdownIntegrationEvents(this NoxSolution noxSolution)
+    {
+        var template = ResourceName.ReadScribanTemplate();
+
+        return CreateIntegrationEvents(template, noxSolution);
+    }
+
+    private static MarkdownFile CreateIntegrationEvents(Template template, NoxSolution noxSolution)
+    {
+        var model = new Dictionary<string, object>
+        {
+            ["integrationEvents"] = ResolveAllIntegrationEvents(noxSolution)
+                .ToDictionary(e => e.Topic),
+        };
+
+        return new MarkdownFile
+        {
+            Name = $"./IntegrationEvents.md",
+            Content = template.RenderScribanTemplate(model)
+        };
+    }
+
+    private static IEnumerable<IntegrationEventMarkdownEntry> ResolveAllIntegrationEvents(NoxSolution noxSolution)
+    {
+        var defaultIntegrationEvents = noxSolution.Domain?.Entities?
+            .Where(ShouldCreateMarkdown)
+            .Select(ToIntegrationEvent)
+            ?? Array.Empty<IntegrationEventMarkdownEntry>();
+
+        var customIntegrationEvents = noxSolution.Application?.IntegrationEvents?
+            .Select(ToIntegrationEvent)
+            ?? Array.Empty<IntegrationEventMarkdownEntry>();
+
+        return defaultIntegrationEvents.Concat(customIntegrationEvents);
+    }
+
+    private static bool ShouldCreateMarkdown(Entity entity)
+        => entity.Persistence!.Create!.RaiseIntegrationEvents
+        || entity.Persistence!.Update!.RaiseIntegrationEvents
+        || entity.Persistence!.Delete!.RaiseIntegrationEvents;
+
+    private static IntegrationEventMarkdownEntry ToIntegrationEvent(Entity entity)
+    {
+        return new()
+        {
+            Topic = "Default",
+            Name = entity.Name,
+            Attributes = entity.Attributes
+        };
+    }
+
+    private static IntegrationEventMarkdownEntry ToIntegrationEvent(IntegrationEvent integrationEvent)
+    {
+        return new()
+        {
+            Topic = "Custom",
+            Name = integrationEvent.Name,
+            Attributes = integrationEvent.ObjectTypeOptions?.Attributes 
+                ?? Array.Empty<Types.NoxSimpleTypeDefinition>(),
+        };
+    }
+
+    class IntegrationEventMarkdownEntry
+    {
+        public string? Topic { get; init; }
+        public string? Name { get; init; }
+        public IEnumerable<Types.NoxSimpleTypeDefinition> Attributes { get; init; } = Array.Empty<Types.NoxSimpleTypeDefinition>();
+    }
+}
---
+++ src/Nox.Docs/Templates/IntegrationEvents.template.md
@@ -0,0 +1,51 @@
+﻿# Domain Events for the {{entity.Name}} entity
+
+This document provides information about the {{entity.Name}} Domain Events in our application.
+
+## Events
+{{ if entity.Persistence.Create.RaiseDomainEvents }}
+### `{{entity.Name}}Created`
+
+**Description:**
+This event is triggered when a new {{entity.Name}} is created.
+
+**Members (Keys, Attributes & Relationships):**
+Member|Type|Description
+------|----|-----------
+{{ for member in entityMembers -}}
+{{member.Name}}|{{member.Type}}|{{member.Description}}
+{{ end -}}
+{{- if entity.Persistence.IsAudited -}}
+*(AuditInfo)*||*Contains date/time, user and system info on state changes.*|*Created, Updated, Deleted*
+{{ end }}
+{{ end }}{{ if entity.Persistence.Update.RaiseDomainEvents }}
+### `{{entity.Name}}Updated`
+
+**Description:** 
+This event is triggered when an existing {{entity.Name}} is updated.
+
+**Members (Keys, Attributes & Relationships):**
+Member|Type|Description
+------|----|-----------
+{{ for member in entityMembers -}}
+{{member.Name}}|{{member.Type}}|{{member.Description}}
+{{ end -}}
+{{- if entity.Persistence.IsAudited -}}
+*(AuditInfo)*||*Contains date/time, user and system info on state changes.*|*Created, Updated, Deleted*
+{{ end }}
+{{ end }}{{ if entity.Persistence.Delete.RaiseDomainEvents }}
+### `{{entity.Name}}Deleted`
+
+**Description:**
+This event is triggered when an existing {{entity.Name}} is deleted.
+
+**Members (Keys, Attributes & Relationships):**
+Member|Type|Description
+------|----|-----------
+{{ for member in entityMembers -}}
+{{member.Name}}|{{member.Type}}|{{member.Description}}
+{{ end -}}
+{{- if entity.Persistence.IsAudited -}}
+*(AuditInfo)*||*Contains date/time, user and system info on state changes.*|*Created, Updated, Deleted*
+{{ end }}
+{{ end -}}
