86ce3f9db53b9a066d5261f314011de79664f398
sadik.kose <sadik.kose@profe.com.tr>
2023-10-03 00:32:57 +0300

    Compile tests were fixed for windows environment

--- tests/Nox.Generator.Tests/Common/CompileTests.cs
+++ tests/Nox.Generator.Tests/Common/CompileTests.cs
@@ -42,7 +42,7 @@ public class CompileTests
 
         var references = GetReferences().ToList();
         _testOutputHelper.WriteLine("References count: " + references.Count);
-        
+
         var compilation = CreateCompilation(result.GeneratedSources.Select(x => x.SourceText.ToString()), references);
 
         var diagnostics = compilation.GetDiagnostics().Where(x => x.Severity == DiagnosticSeverity.Error).ToList();
@@ -94,6 +94,12 @@ global using global::Microsoft.AspNetCore.Builder;";
         {
             if (referencePaths.Contains(Path.GetFileName(dllFile)))
                 continue;
+            if (!IsManagedAssembly(dllFile))
+            {
+                referencePaths.Add(Path.GetFileName(dllFile));
+                continue;
+            }
+
             referencePaths.Add(Path.GetFileName(dllFile));
             references.Add(MetadataReference.CreateFromFile(dllFile));
         }
@@ -104,6 +110,12 @@ global using global::Microsoft.AspNetCore.Builder;";
         {
             if (referencePaths.Contains(Path.GetFileName(dllFile)))
                 continue;
+            if (!IsManagedAssembly(dllFile))
+            {
+                referencePaths.Add(Path.GetFileName(dllFile));
+                continue;
+            }
+
             referencePaths.Add(Path.GetFileName(dllFile));
             references.Add(MetadataReference.CreateFromFile(dllFile));
         }
@@ -114,11 +126,79 @@ global using global::Microsoft.AspNetCore.Builder;";
         {
             if (referencePaths.Contains(Path.GetFileName(dllFile)))
                 continue;
+            if (!IsManagedAssembly(dllFile))
+            {
+                referencePaths.Add(Path.GetFileName(dllFile));
+                continue;
+            }
+
             referencePaths.Add(Path.GetFileName(dllFile));
             references.Add(MetadataReference.CreateFromFile(dllFile));
         }
-        
+
 
         return references;
+    }
+
+    private static bool IsManagedAssembly(string fileName)
+    {
+        using Stream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+        using var binaryReader = new BinaryReader(fileStream);
+        if (fileStream.Length < 64)
+        {
+            return false;
+        }
+
+        //PE Header starts @ 0x3C (60). Its a 4 byte header.
+        fileStream.Position = 0x3C;
+        uint peHeaderPointer = binaryReader.ReadUInt32();
+        if (peHeaderPointer == 0)
+        {
+            peHeaderPointer = 0x80;
+        }
+
+        // Ensure there is at least enough room for the following structures:
+        //     24 byte PE Signature & Header
+        //     28 byte Standard Fields         (24 bytes for PE32+)
+        //     68 byte NT Fields               (88 bytes for PE32+)
+        // >= 128 byte Data Dictionary Table
+        if (peHeaderPointer > fileStream.Length - 256)
+        {
+            return false;
+        }
+
+        // Check the PE signature.  Should equal 'PE\0\0'.
+        fileStream.Position = peHeaderPointer;
+        uint peHeaderSignature = binaryReader.ReadUInt32();
+        if (peHeaderSignature != 0x00004550)
+        {
+            return false;
+        }
+
+        // skip over the PEHeader fields
+        fileStream.Position += 20;
+
+        const ushort pe32 = 0x10b;
+        const ushort pe32Plus = 0x20b;
+
+        // Read PE magic number from Standard Fields to determine format.
+        var peFormat = binaryReader.ReadUInt16();
+        if (peFormat != pe32 && peFormat != pe32Plus)
+        {
+            return false;
+        }
+
+        // Read the 15th Data Dictionary RVA field which contains the CLI header RVA.
+        // When this is non-zero then the file contains CLI data otherwise not.
+        ushort dataDictionaryStart = (ushort)(peHeaderPointer + (peFormat == pe32 ? 232 : 248));
+        fileStream.Position = dataDictionaryStart;
+
+        uint cliHeaderRva = binaryReader.ReadUInt32();
+        if (cliHeaderRva == 0)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
