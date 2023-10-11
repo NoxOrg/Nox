d54b63d6eef53658808a879ec81eb843921e766f
Dmytro Dorodnykh <Dmytro.Dorodnykh@iwgplc.com>
2023-10-12 00:27:39 +0300

    Add test for staging

--- tests/Nox.Solution.Tests/IntegrationEventTests.cs
+++ tests/Nox.Solution.Tests/IntegrationEventTests.cs
@@ -7,7 +7,7 @@ public class IntegrationEventTests
 {
     [Theory]
     [InlineData("missing-trait-integration-event.solution.nox.yaml")]
-    public void When_miassing_trait_integration_event_should_throw_exception(string fileName)
+    public void When_missing_trait_integration_event_should_throw_exception(string fileName)
     {
         var solutionBuilder = new NoxSolutionBuilder().UseYamlFile($"./files/{fileName}");
 
