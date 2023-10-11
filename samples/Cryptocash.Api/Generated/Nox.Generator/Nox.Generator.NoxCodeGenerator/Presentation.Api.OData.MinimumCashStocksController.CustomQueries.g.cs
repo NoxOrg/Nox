407228d1b8bb8e39d9c468e1ca9ea79f5ea9e3bb
emir.sehovic <emir.sehovic@regus.com>
2023-10-11 19:08:41 +0200

    Regenerating files

--- samples/Cryptocash.Api/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Presentation.Api.OData.MinimumCashStocksController.CustomQueries.g.cs
+++ samples/Cryptocash.Api/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Presentation.Api.OData.MinimumCashStocksController.CustomQueries.g.cs
@@ -2,6 +2,15 @@
 
 #nullable enable
 
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+using Cryptocash.Application;
+using Cryptocash.Application.Queries;
+using Cryptocash.Application.Commands;
+
+using Nox.Types;
+
 namespace Cryptocash.Presentation.Api.OData;
 
 public abstract partial class MinimumCashStocksControllerBase
--- tests/Nox.Integration.Tests/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Presentation.Api.OData.ThirdTestEntityZeroOrManiesController.CustomQueries.g.cs
+++ tests/Nox.Integration.Tests/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Presentation.Api.OData.ThirdTestEntityZeroOrManiesController.CustomQueries.g.cs
@@ -2,6 +2,15 @@
 
 #nullable enable
 
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+using TestWebApp.Application;
+using TestWebApp.Application.Queries;
+using TestWebApp.Application.Commands;
+
+using Nox.Types;
+
 namespace TestWebApp.Presentation.Api.OData;
 
 public abstract partial class ThirdTestEntityZeroOrManiesControllerBase
