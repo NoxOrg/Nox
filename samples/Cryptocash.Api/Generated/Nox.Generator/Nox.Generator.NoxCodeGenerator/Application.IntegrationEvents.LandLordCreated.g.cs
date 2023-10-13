de3d69b8748d80c095770987ecfd876cf125072c
Ricardo Rocha <rochar@users.noreply.github.com>
2023-10-13 11:33:00 +0100

    fix issue when using ienumerable and iterating multiple times
    in this case the ienumerable result is changing while iterating

---
+++ samples/Cryptocash.Api/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Application.IntegrationEvents.LandLordCreated.g.cs
@@ -0,0 +1,17 @@
+﻿// Generated
+
+#nullable enable
+
+using Nox.Abstractions;
+using Nox.Application;
+using Nox.Infrastructure.Messaging;
+
+using Cryptocash.Application.Dto;
+
+namespace Cryptocash.Application.IntegrationEvents;
+
+/// <summary>
+/// LandLordCreated integration event.
+/// </summary>
+[IntegrationEventType("created", nameof(LandLord))]
+internal record LandLordCreated(LandLordDto LandLord) :  IIntegrationEvent;
