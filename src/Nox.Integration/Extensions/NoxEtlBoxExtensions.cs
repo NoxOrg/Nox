36ff82e9690b489d769d5a5f395cf231d987808b
Andrii Dehtiarov <Andrii.Dehtiarov@iwgplc.com>
2023-10-17 11:40:07 +0300

    [Nox.Integration] Setup ETL Box #978. Introduced services to get license key for EtlBox

---
+++ src/Nox.Integration/Extensions/NoxEtlBoxExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ETLBoxOffice.LicenseManager;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Nox.Integration.Abstractions;
+
+namespace Nox.Integration.Extensions;
+
+public static class NoxEtlBoxExtensions
+{
+    public static void UseEtlBox(this IApplicationBuilder applicationBuilder)
+    {
+        var etlLicenseProvider = applicationBuilder.ApplicationServices.GetRequiredService<IEtlBoxLicenseProvider>();
+
+        LicenseCheck.LicenseKey = etlLicenseProvider.GetLicenseKey();
+
+        LicenseCheck.CheckValidLicenseOrThrow(progressCount: 1);
+    }
+}
