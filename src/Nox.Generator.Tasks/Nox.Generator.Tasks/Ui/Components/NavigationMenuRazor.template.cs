89a0552aa3df3decc61ea3af9e09835a2afdccbd
Anna Naumova <anna.naumova@dataart.com>
2023-10-20 11:47:04 -0400

    1012 add Nox.Generator.Tasks project

---
+++ src/Nox.Generator.Tasks/Nox.Generator.Tasks/Ui/Components/NavigationMenuRazor.template.cs
@@ -0,0 +1,14 @@
+﻿@*Generated *@
+
+@namespace {{codeGeneratorState.UiNameSpace}}.Components;
+@inherits NavigationMenu 
+
+Navigation Menu Component
+@*
+<MudNavMenu Style="margin-top:20px;">    
+    {{- for entity in entities }}
+
+    <MudNavLink Match = "NavLinkMatch.Prefix" Icon = "@{{entity.PluralName}}Icon" IconColor = "Color.Inherit" Href = "@{{entity.PluralName}}PageLink" ActiveClass = "custom-linkHighlighted" >{{ entity.PluralName}}</MudNavLink>
+    {{- end }}
+</MudNavMenu >
+*@
