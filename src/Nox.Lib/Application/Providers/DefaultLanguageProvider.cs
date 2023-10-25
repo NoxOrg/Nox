f1984cfa9ffc2e22ffd0a1bac7a83002e2698ad6
Dejan Bratic <dejan.bratic@igwplc.com>
2023-10-25 09:06:07 +0200

    added support for different language headers;

--- src/Nox.Lib/Application/Providers/DefaultLanguageProvider.cs
+++ src/Nox.Lib/Application/Providers/DefaultLanguageProvider.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Nox.Abstractions;
 using Nox.Solution;
+using System.Text.RegularExpressions;
 
 namespace Nox.Application.Providers;
 
 public class DefaultLanguageProvider : HttpHeaderValueProvider, ILanguageProvider
 {
     private readonly string _defaultLanguage;
+    private readonly HashSet<string> _supportedLanguages;
+
+    private static readonly Regex AcceptLanguageHeaderRegex = new("^(?<lang>[a-z]{2}(?:\\-[A-Za-z]+){0,2})(?:\\,\\s*(?:\\*|[a-z]{2}(?:\\-[A-Za-z]+){0,2})\\;q\\=\\d+\\.\\d+)*$");
 
     public DefaultLanguageProvider(
         NoxSolution solution,
@@ -14,11 +18,20 @@ public class DefaultLanguageProvider : HttpHeaderValueProvider, ILanguageProvide
         : base(httpContextAccessor)
     {
         _defaultLanguage = solution.Application?.Localization?.DefaultCulture ?? "en-US"; // should we set the language or throw an exception?
+        _supportedLanguages = solution.Application?.Localization?.SupportedCultures?.ToHashSet() ?? new HashSet<string>();
     }
 
     public override string HeaderName => "Accept-Language";
 
     public override string DefaultHeaderValue => _defaultLanguage;
 
-    public string GetLanguage() => GetHeaderValue(); // should we validate the returned language?
+    public string GetLanguage()
+    {
+        var language = GetHeaderValue();
+
+        var match = AcceptLanguageHeaderRegex.Match(language);
+        if (match.Success) language = match.Groups["lang"].Value;
+
+        return _supportedLanguages.Contains(language) ? language : _defaultLanguage;
+    }
 }
--- tests/Nox.Lib.Tests/Application/Providers/DefaultLanguageProviderTests.cs
+++ tests/Nox.Lib.Tests/Application/Providers/DefaultLanguageProviderTests.cs
@@ -11,11 +11,23 @@ public class DefaultLanguageProviderTests
     private readonly DefaultLanguageProvider _provider;
 
     private readonly Mock<IHttpContextAccessor> _httpContextAccessorMock = new();
+    private readonly NoxSolution _noxSolution = new();
 
     public DefaultLanguageProviderTests()
     {
+        var yamlName = "solution.solution.nox.yaml";
+        var yamlContent = "{name: solution, application: {localization: {supportedCultures: [en-US, fr-FR], defaultCulture: en-US}}}";
+
+        _noxSolution = new NoxSolutionBuilder()
+            .UseYamlFilesAndContent(new Dictionary<string, Func<TextReader>>
+            {
+                [yamlName] = () => new StringReader(yamlContent)
+            })
+            .AllowMissingSolutionYaml()
+            .Build();
+
         _provider = new DefaultLanguageProvider(
-            new NoxSolution(),
+            _noxSolution,
             _httpContextAccessorMock.Object);
     }
 
@@ -45,16 +57,40 @@ public class DefaultLanguageProviderTests
         actual.Should().Be(expected);
     }
 
-    [Fact]
-    public void GetLanguage_WhenHeaderIsSet_ReturnsHeaderValue()
+    [Theory]
+    [InlineData("*")]
+    [InlineData("en")]
+    [InlineData("fr-CH")]
+    [InlineData("fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5")]
+    public void GetLanguage_WhenHeaderIsSetWithUnsupportedLanguage_ReturnsDefaultValue(string value)
+    {
+        // Arrange
+        var expected = "en-US";
+
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers.Add("Accept-Language", value);
+
+        _httpContextAccessorMock.SetupGet(x => x.HttpContext).Returns(httpContext);
+
+        // Act
+        var actual = _provider.GetLanguage();
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("fr-FR")]
+    [InlineData("fr-FR, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5")]
+    public void GetLanguage_WhenHeaderIsSet_ReturnsHeaderValue(string value)
     {
         // Arrange
         var expected = "fr-FR";
 
         var httpContext = new DefaultHttpContext();
-        httpContext.Request.Headers.Add("Accept-Language", expected);
+        httpContext.Request.Headers.Add("Accept-Language", value);
 
-        _httpContextAccessorMock.SetupGet(accessor => accessor.HttpContext).Returns(httpContext);
+        _httpContextAccessorMock.SetupGet(x => x.HttpContext).Returns(httpContext);
 
         // Act
         var actual = _provider.GetLanguage();
