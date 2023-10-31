683781cd002ea12ae141dbf54845abe978c7c041
Dejan Bratic <dejan.bratic@igwplc.com>
2023-10-31 09:31:09 +0100

    added missing generated files;

---
+++ samples/Cryptocash.Ui/Nox.Generated/Generator.g.cs
@@ -0,0 +1,75 @@
+﻿// Generated
+
+#nullable enable
+
+/* Found files ->
+- cryptocash.application.nox.yaml
+- CountrySync.integration.nox.yaml
+- cryptocash.solution.nox.yaml
+- cryptocash.domain.nox.yaml
+- BankNote.entity.nox.yaml
+- Booking.entity.nox.yaml
+- CashStockOrder.entity.nox.yaml
+- Commission.entity.nox.yaml
+- Country.entity.nox.yaml
+- CountryTimeZone.entity.nox.yaml
+- Currency.entity.nox.yaml
+- Customer.entity.nox.yaml
+- Employee.entity.nox.yaml
+- EmployeePhoneNumber.entity.nox.yaml
+- ExchangeRate.entity.nox.yaml
+- Holiday.entity.nox.yaml
+- Landlord.entity.nox.yaml
+- MinimumCashStock.entity.nox.yaml
+- PaymentDetail.entity.nox.yaml
+- PaymentProvider.entity.nox.yaml
+- Transaction.entity.nox.yaml
+- VendingMachine.entity.nox.yaml
+- cryptocash.infrastructure.nox.yaml
+- cryptocash.dependencies.nox.yaml
+- NoxReferenceData.dataConnection.nox.yaml
+- cryptocash.messaging.nox.yaml
+- cryptocash.persistence.nox.yaml
+- BankNoteDomainEvents.md
+- BookingDomainEvents.md
+- CashStockOrderDomainEvents.md
+- CommissionDomainEvents.md
+- CountryDomainEvents.md
+- CountryTimeZoneDomainEvents.md
+- CurrencyDomainEvents.md
+- CustomerDomainEvents.md
+- EmployeeDomainEvents.md
+- EmployeePhoneNumberDomainEvents.md
+- ExchangeRateDomainEvents.md
+- HolidayDomainEvents.md
+- LandLordDomainEvents.md
+- MinimumCashStockDomainEvents.md
+- PaymentDetailDomainEvents.md
+- PaymentProviderDomainEvents.md
+- TransactionDomainEvents.md
+- VendingMachineDomainEvents.md
+- BookingEndpoints.md
+- CashStockOrderEndpoints.md
+- CommissionEndpoints.md
+- CountryEndpoints.md
+- CurrencyEndpoints.md
+- CustomerEndpoints.md
+- EmployeeEndpoints.md
+- LandLordEndpoints.md
+- MinimumCashStockEndpoints.md
+- PaymentDetailEndpoints.md
+- PaymentProviderEndpoints.md
+- TransactionEndpoints.md
+- VendingMachineEndpoints.md
+- IntegrationEvents.md
+- README.md
+- generator.nox.yaml
+Errors ->
+- Could not find file 'C:\Repos\Nox.Generator\samples\Cryptocash.Ui\generator.nox.yaml'.   at System.IO.__Error.WinIOError(Int32 errorCode, String maybeFullPath)
+   at System.IO.FileStream.Init(String path, FileMode mode, FileAccess access, Int32 rights, Boolean useRights, FileShare share, Int32 bufferSize, FileOptions options, SECURITY_ATTRIBUTES secAttrs, String msgPath, Boolean bFromProxy, Boolean useLongPath, Boolean checkHost)
+   at System.IO.FileStream..ctor(String path, FileMode mode, FileAccess access, FileShare share, Int32 bufferSize, FileOptions options, String msgPath, Boolean bFromProxy, Boolean useLongPath, Boolean checkHost)
+   at System.IO.StreamReader..ctor(String path, Encoding encoding, Boolean detectEncodingFromByteOrderMarks, Int32 bufferSize, Boolean checkHost)
+   at System.IO.File.InternalReadAllText(String path, Encoding encoding, Boolean checkHost)
+   at Nox.Generator.Tasks.NoxFileGenerator.TryGetGeneratorConfig(GeneratorConfig& config) in /home/runner/work/Nox.Generator/Nox.Generator/src/Nox.Generator.Tasks/NoxFileGenerator.cs:line 138
+   at Nox.Generator.Tasks.NoxFileGenerator.GenerateFiles() in /home/runner/work/Nox.Generator/Nox.Generator/src/Nox.Generator.Tasks/NoxFileGenerator.cs:line 45
+*/
--- src/Nox.ClientApi.Tests/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Presentation.Api.OData/CountriesController/Entity.g.cs
+++ src/Nox.ClientApi.Tests/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Presentation.Api.OData/CountriesController/Entity.g.cs
@@ -42,9 +42,9 @@ public abstract partial class CountriesControllerBase : ODataController
     protected readonly IMediator _mediator;
 
     /// <symmary>
-    /// The HTTP language provider.
+    /// The Culture Code from the HTTP request.
     /// </symmary>
-    protected readonly Nox.Presentation.Api.IHttpLanguageProvider _httpLanguageProvider;
+    protected readonly Nox.Types.CultureCode _cultureCode;
 
     public CountriesControllerBase(
         IMediator mediator,
@@ -52,7 +52,7 @@ public abstract partial class CountriesControllerBase : ODataController
     )
     {
         _mediator = mediator;
-        _httpLanguageProvider = httpLanguageProvider;
+        _cultureCode = Nox.Types.CultureCode.From(httpLanguageProvider.GetLanguage());
     }
 
     [EnableQuery]
@@ -76,8 +76,7 @@ public abstract partial class CountriesControllerBase : ODataController
             return BadRequest(ModelState);
         }
 
-        var language = _httpLanguageProvider.GetLanguage();
-        var createdKey = await _mediator.Send(new CreateCountryCommand(country, Nox.Types.CultureCode.From(language)));
+        var createdKey = await _mediator.Send(new CreateCountryCommand(country, _cultureCode));
 
         var item = (await _mediator.Send(new GetCountryByIdQuery(createdKey.keyId))).SingleOrDefault();
 
