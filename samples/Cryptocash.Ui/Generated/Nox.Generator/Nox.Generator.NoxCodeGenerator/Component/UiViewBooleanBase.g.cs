1e1ef911b2b6cfaaf7add0d0003352b95f67a07a
DanielHodgeGH <github@danielhodge.co.uk>
2023-11-07 14:09:35 +0000

    feature/113, 117, 119 NoxComponents Percentage, Year, Boolean

---
+++ samples/Cryptocash.Ui/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Component/UiViewBooleanBase.g.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Cryptocash.Ui.Generated.Component
+{
+    public class UiViewBooleanBase : ComponentBase
+    {
+        #region Declarations
+
+        [Parameter]
+        public bool? Boolean { get; set; }
+
+        [Parameter]
+        public string TitleTrue { get; set; } = "True";
+
+        [Parameter]
+        public string TitleFalse { get; set; } = "False";
+
+        public string DisplayBoolean { 
+            
+            get
+            {
+                if (Boolean != null)
+                {
+                    if ((bool)Boolean)
+                    {
+                        return TitleTrue;
+                    }
+                    else
+                    {
+                        return TitleFalse;
+                    }
+                }
+
+                return String.Empty;
+            }
+        }
+
+        #endregion
+    }
+}
