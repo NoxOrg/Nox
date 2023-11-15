2940e5af6e4f03b0aa0eafdd4a61c4f7ba9edd30
DanielHodgeGH <65465917+DanielHodgeGH@users.noreply.github.com>
2023-11-15 14:35:03 +0000

    feature 221 Ui Components Temperature (#1115)
    * feature 221 Ui Components Temperature
    
    * feature/221 use config BaseApiUrl

---
+++ samples/Cryptocash.Ui/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Component/UiViewCountryBase.g.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Cryptocash.Application.Dto;
+using Nox.Types;
+using Microsoft.SqlServer.Server;
+
+namespace Cryptocash.Ui.Generated.Component
+{
+    public class UiViewCountryBase : ComponentBase
+    {
+        #region Declarations
+
+        [Parameter]
+        public CountryDto Country { get; set; }
+
+        public string DisplayCountryName
+        {
+
+            get
+            {
+                if (Country != null
+                    && !String.IsNullOrWhiteSpace(Country.Name))
+                {
+                    return Country.Name;
+                }
+
+                return String.Empty;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ samples/Cryptocash.Ui/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Component/UiViewMoneyBase.g.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Nox.Types;
+using Cryptocash.Application.Dto;
+using Microsoft.SqlServer.Server;
+
+namespace Cryptocash.Ui.Generated.Component
+{
+    public class UiViewMoneyBase : ComponentBase
+    {
+#nullable enable
+
+        #region Declarations
+
+        [Parameter]
+        public MoneyDto? Money { get; set; }
+
+        [Parameter]
+        public CurrencyDto? Currency { get; set; }
+
+        public string DisplayMoney { 
+            
+            get
+            {
+                if (Money != null
+                    && Currency != null)
+                {
+                    string CurrencyFormat = "{0:#.##}";
+
+                    if (!String.IsNullOrWhiteSpace(Currency.ThousandsSeparator)
+                        && !String.IsNullOrWhiteSpace(Currency.DecimalSeparator))
+                    {
+                        CurrencyFormat = "{" + $"0:#{Currency.ThousandsSeparator}##0{Currency.DecimalSeparator}"
+                            + String.Concat("#", Currency.DecimalDigits) 
+                            + "}";
+                    }
+
+                    string SymbolSeparator = "";
+                    if (Currency.SpaceBetweenAmountAndSymbol)
+                    {
+                        SymbolSeparator = " ";
+                    }
+
+                    return Currency.Symbol 
+                        + SymbolSeparator 
+                        + String.Format(CurrencyFormat, Money.Amount).Trim();                    
+                }
+
+                return String.Empty;
+            }
+        }
+
+        #endregion
+    }
+}
