0bd8bb510d186a461ff3ecec38a467bb1448b3cc
Morne van Zyl <64355398+Mornevanzyl@users.noreply.github.com>
2023-11-17 10:46:22 +0000

    Updated select classes from nested to file-scoped namespaces (#1133)

--- src/Nox.Solution/Models/Infrastructure/Security/Secrets/Secrets.cs
+++ src/Nox.Solution/Models/Infrastructure/Security/Secrets/Secrets.cs
@@ -1,21 +1,20 @@
 using Nox.Yaml.Attributes;
 
-namespace Nox.Solution
+namespace Nox.Solution;
+
+[Title("The definition namespace for secrets used in a Nox solution.")]
+[Description("Specify properties pertinent to secrets as configured within a Nox solution here. Examples include servers and validity period.")]
+[AdditionalProperties(false)]
+public class Secrets
 {
-    [Title("The definition namespace for secrets used in a Nox solution.")]
-    [Description("Specify properties pertinent to secrets as configured within a Nox solution here. Examples include servers and validity period.")]
+    
+    [Title("The organization secrets server.")]
+    [Description("Specify the provider and server attributes for secrets stored at an organizational level (org.secret.<secret_key>).")]
     [AdditionalProperties(false)]
-    public class Secrets
-    {
-        
-        [Title("The organization secrets server.")]
-        [Description("Specify the provider and server attributes for secrets stored at an organizational level (org.secret.<secret_key>).")]
-        [AdditionalProperties(false)]
-        public SecretsServer? OrganizationSecretsServer { get; internal set; }
-        
-        [Title("The solution secrets server.")]
-        [Description("Specify the provider and server attributes for secrets stored at a solution level (solution.secret.<secret_key>).")]
-        [AdditionalProperties(false)]
-        public SecretsServer? SolutionSecretsServer { get; internal set; }
-    }
+    public SecretsServer? OrganizationSecretsServer { get; internal set; }
+    
+    [Title("The solution secrets server.")]
+    [Description("Specify the provider and server attributes for secrets stored at a solution level (solution.secret.<secret_key>).")]
+    [AdditionalProperties(false)]
+    public SecretsServer? SolutionSecretsServer { get; internal set; }
 }
