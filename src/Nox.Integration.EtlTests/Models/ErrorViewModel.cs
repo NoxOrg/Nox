ebaee0b4071f5bbedc062b1c91857071d2ef1324
jan <janschutte.lands@gmail.com>
2023-11-17 16:07:17 +0200

    - removed invalid files

--- src/Nox.Integration.EtlTests/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Nox.Integration.EtlTests.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
