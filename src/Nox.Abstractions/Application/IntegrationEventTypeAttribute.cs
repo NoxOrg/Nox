3dca11d50f3f43c78c3868239197dc7e9aab6417
Ricardo Rocha <rochar@users.noreply.github.com>
2023-11-17 11:55:29 +0000

    rename IE Trait to DomainContext

--- src/Nox.Abstractions/Application/IntegrationEventTypeAttribute.cs
+++ src/Nox.Abstractions/Application/IntegrationEventTypeAttribute.cs
@@ -3,12 +3,15 @@
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
 public class IntegrationEventTypeAttribute : Attribute
 {
-    public IntegrationEventTypeAttribute(string eventName, string trait)
+    public IntegrationEventTypeAttribute(string eventName, string domainContext)
     {
         EventName = eventName;
-        Trait = trait;
+        DomainContext = domainContext;
     }
 
     public string EventName { get; private set; }
-    public string Trait { get; private set; }
+    /// <summary>
+    /// Domain context should have one of this value types: EntityName | BussinessProcessName | BoundedContext
+    /// </summary>
+    public string DomainContext { get; private set; }
 }
