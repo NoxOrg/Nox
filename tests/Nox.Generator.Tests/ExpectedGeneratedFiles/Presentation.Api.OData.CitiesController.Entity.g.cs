bf54e1fe04a6577e7bd4125c5562081d99a4f9ad
sadik.kose <sadik.kose@profe.com.tr>
2023-11-20 14:10:26 +0300

    Generator tests have been enriched with translations files

---
+++ tests/Nox.Generator.Tests/ExpectedGeneratedFiles/Presentation.Api.OData.CitiesController.Entity.g.cs
@@ -0,0 +1,134 @@
+// Generated
+
+#nullable enable
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Deltas;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Results;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Nox.Application;
+using Nox.Extensions;
+
+using System;
+using System.Net.Http.Headers;
+using SampleWebApp.Application;
+using SampleWebApp.Application.Dto;
+using SampleWebApp.Application.Queries;
+using SampleWebApp.Application.Commands;
+using SampleWebApp.Domain;
+using SampleWebApp.Infrastructure.Persistence;
+
+namespace SampleWebApp.Presentation.Api.OData;
+
+public partial class CitiesController : CitiesControllerBase
+{
+    public CitiesController(
+            IMediator mediator,
+            Nox.Presentation.Api.IHttpLanguageProvider httpLanguageProvider
+        ): base(mediator, httpLanguageProvider)
+    {}
+}
+
+public abstract partial class CitiesControllerBase : ODataController
+{
+    /// <summary>
+    /// The Mediator.
+    /// </summary>
+    protected readonly IMediator _mediator;
+
+    /// <symmary>
+    /// The Culture Code from the HTTP request.
+    /// </symmary>
+    protected readonly Nox.Types.CultureCode _cultureCode;
+
+    public CitiesControllerBase(
+        IMediator mediator,
+        Nox.Presentation.Api.IHttpLanguageProvider httpLanguageProvider
+    )
+    {
+        _mediator = mediator;
+        _cultureCode = httpLanguageProvider.GetLanguage();
+    }
+
+    [EnableQuery]
+    public virtual async Task<ActionResult<IQueryable<CityDto>>> Get()
+    {
+        var result = await _mediator.Send(new GetCitiesQuery(_cultureCode));
+        return Ok(result);
+    }
+
+    [EnableQuery]
+    public async Task<SingleResult<CityDto>> Get([FromRoute] System.String key)
+    {
+        var result = await _mediator.Send(new GetCityByIdQuery(_cultureCode, key));
+        return SingleResult.Create(result);
+    }
+
+    public virtual async Task<ActionResult<CityDto>> Post([FromBody] CityCreateDto city)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var createdKey = await _mediator.Send(new CreateCityCommand(city, _cultureCode));
+
+        var item = (await _mediator.Send(new GetCityByIdQuery(_cultureCode, createdKey.keyId))).SingleOrDefault();
+
+        return Created(item);
+    }
+
+    public virtual async Task<ActionResult<CityDto>> Put([FromRoute] System.String key, [FromBody] CityUpdateDto city)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var etag = Request.GetDecodedEtagHeader();
+        var updatedKey = await _mediator.Send(new UpdateCityCommand(key, city, _cultureCode, etag));
+
+        if (updatedKey is null)
+        {
+            return NotFound();
+        }
+
+        var item = (await _mediator.Send(new GetCityByIdQuery(_cultureCode, updatedKey.keyId))).SingleOrDefault();
+
+        return Ok(item);
+    }
+
+    public virtual async Task<ActionResult<CityDto>> Patch([FromRoute] System.String key, [FromBody] Delta<CityUpdateDto> city)
+    {
+        if (!ModelState.IsValid || city is null)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var updatedProperties = new Dictionary<string, dynamic>();
+
+        foreach (var propertyName in city.GetChangedPropertyNames())
+        {
+            if (city.TryGetPropertyValue(propertyName, out dynamic value))
+            {
+                updatedProperties[propertyName] = value;
+            }
+        }
+
+        var etag = Request.GetDecodedEtagHeader();
+        var updatedKey = await _mediator.Send(new PartialUpdateCityCommand(key, updatedProperties, _cultureCode, etag));
+
+        if (updatedKey is null)
+        {
+            return NotFound();
+        }
+
+        var item = (await _mediator.Send(new GetCityByIdQuery(_cultureCode, updatedKey.keyId))).SingleOrDefault();
+
+        return Ok(item);
+    }
+
+}
