6235f62fb164e204ceb461626d8918fdfafbb208
emir-sehovic1 <135145425+emir-sehovic1@users.noreply.github.com>
2023-11-21 15:14:07 +0100

    NOX-243 Allow relationship generated attributes to be used in UniqueAttributeConstraints configurationAdded support for foreign keys in unique constraints (#1140)
    * Added support for foreign keys in unique constraints
    
    * Updating implementation to follow new structure
    
    * Extended unique constraint validation
    
    * Added comments for unique constraint validations

---
+++ tests/Nox.Integration.Tests/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Application.Dto/EntityUniqueConstraintsWithForeignKeyCreateDto.g.cs
@@ -0,0 +1,54 @@
+﻿// Generated
+
+#nullable enable
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using Nox.Abstractions;
+using Nox.Application.Dto;
+using Nox.Domain;
+using Nox.Extensions;
+using Nox.Types;
+
+using DomainNamespace = TestWebApp.Domain;
+
+namespace TestWebApp.Application.Dto;
+
+/// <summary>
+/// Entity created for testing constraints with Foreign Key.
+/// </summary>
+public partial class EntityUniqueConstraintsWithForeignKeyCreateDto : EntityUniqueConstraintsWithForeignKeyCreateDtoBase
+{
+
+}
+
+/// <summary>
+/// Entity created for testing constraints with Foreign Key.
+/// </summary>
+public abstract class EntityUniqueConstraintsWithForeignKeyCreateDtoBase : IEntityDto<DomainNamespace.EntityUniqueConstraintsWithForeignKey>
+{/// <summary>
+    ///      
+    /// </summary>
+    /// <remarks>Optional.</remarks>
+    public System.Guid Id { get; set; } = default!;
+    /// <summary>
+    ///      
+    /// </summary>
+    /// <remarks>Optional</remarks>
+    public virtual System.String? TextField { get; set; }
+    /// <summary>
+    ///      
+    /// </summary>
+    /// <remarks>Required</remarks>
+    [Required(ErrorMessage = "SomeUniqueId is required")]
+    
+    public virtual System.Int32 SomeUniqueId { get; set; } = default!;
+
+    /// <summary>
+    /// EntityUniqueConstraintsWithForeignKey for ExactlyOne EntityUniqueConstraintsRelatedForeignKeys
+    /// </summary>
+    public System.Int32? EntityUniqueConstraintsRelatedForeignKeyId { get; set; } = default!;
+    
+    [System.Text.Json.Serialization.JsonIgnore]
+    public virtual EntityUniqueConstraintsRelatedForeignKeyCreateDto? EntityUniqueConstraintsRelatedForeignKey { get; set; } = default!;
+}
