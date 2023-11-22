7723eb8f85c39f586f5bdac65ee412726ea8552c
DanielHodgeGH <github@danielhodge.co.uk>
2023-11-22 16:00:25 +0000

    feature 122 Nox Ui Components Time

--- samples/Cryptocash.Ui/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Component/UiViewDateBase.g.cs
+++ samples/Cryptocash.Ui/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Component/UiViewDateBase.g.cs
@@ -3,15 +3,15 @@ using System.Globalization;
 
 namespace Cryptocash.Ui.Generated.Component
 {
-    public class UiViewDateTimeBase : ComponentBase
+    public class UiViewDateBase : ComponentBase
     {
         #region Declarations
 
         [Parameter]
-        public DateTime? DateTime { get; set; }
+        public DateTime? Date { get; set; }
 
         [Parameter]
-        public string Format { get; set; } = "dd/MM/yyyy HH:mm:ss";
+        public string Format { get; set; } = "dd/MM/yyyy";
 
         [Parameter]
         public CultureInfo CultureInfo { get; set; } = CultureInfo.CurrentCulture;
