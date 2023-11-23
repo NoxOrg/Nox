937b3167d3f7e9c2a81e2019444c7feeeaca7875
Ricardo Rocha <rochar@users.noreply.github.com>
2023-11-23 16:30:51 +0000

    Create new nox type ReferenceNumber

---
+++ src/Nox.Types/Types/ReferenceNumber/ReferenceNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nox.Types;
+
+/// <summary>
+/// Represents a Nox <see cref="ReferenceNumber"/> type and value object.
+/// </summary>
+public sealed class ReferenceNumber : ValueObject<string, ReferenceNumber>
+{
+    public ReferenceNumber()
+    {
+        Value = string.Empty;
+    }
+
+    /// <summary>
+    /// <see cref="ReferenceNumber"/> object can only be created with <see cref="ValueObject{T,TValueObject}.FromDatabase"/>.
+    /// </summary>
+    public new static ReferenceNumber From(string _) => throw new InvalidOperationException($"{nameof(ReferenceNumber)} can only be created with {nameof(FromDatabase)}.");
+    public static ReferenceNumber From(string _, ReferenceNumberTypeOptions __) => throw new InvalidOperationException($"{nameof(ReferenceNumber)} can only be created with {nameof(FromDatabase)}.");
+}
