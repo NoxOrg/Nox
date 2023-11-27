ea8c383e6a741b2b9bb22c3680ba4f94d7f3fc34
Morne van Zyl <catbeatsadrum@gmail.com>
2023-11-27 11:43:54 +0000

    Added Country data seeder

--- samples/Cryptocash.Api/DataSeed/Seeders/CryptocashCountryDataSeeder.cs
+++ samples/Cryptocash.Api/DataSeed/Seeders/CryptocashCountryDataSeeder.cs
@@ -23,6 +23,21 @@ internal class CryptocashCountryDataSeeder : DataSeederBase<CountryDto, Country>
     protected override Country TransformToEntity(CountryDto model)
     {
         return new() {
+            Id = CountryCode2.From(model.Id),
+            Name = Text.From(model.Name!),
+            OfficialName = Text.From(model.OfficialName!),
+            CountryIsoNumeric = CountryNumber.From((ushort)model.CountryIsoNumeric!),
+            CountryIsoAlpha3 = CountryCode3.From(model.CountryIsoAlpha3!),
+            GeoCoords = LatLong.From(model.GeoCoords!.Latitude, model.GeoCoords.Longitude),
+            FlagEmoji = Text.From(model?.FlagEmoji!),
+            FlagSvg = model?.FlagSvg == null ? null : Image.From(model?.FlagSvg!),
+            FlagPng = model?.FlagPng == null ? null : Image.From(model?.FlagPng!),
+            CoatOfArmsSvg = model?.CoatOfArmsSvg == null ? null : Image.From(model?.CoatOfArmsSvg!),
+            CoatOfArmsPng = model?.CoatOfArmsPng == null ? null : Image.From(model?.CoatOfArmsPng!),
+            GoogleMapsUrl = Url.From(model?.GoogleMapsUrl!),
+            OpenStreetMapsUrl = Url.From(model?.OpenStreetMapsUrl!),
+            StartOfWeek = Nox.Types.DayOfWeek.From((ushort)model?.StartOfWeek!),
+            CurrencyId = CurrencyCode3.From(model?.CurrencyId!)
         };
     }
 }
