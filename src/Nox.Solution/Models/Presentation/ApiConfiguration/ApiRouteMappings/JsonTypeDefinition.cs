c013c438a22374e85399a476d7c5c5b42ab36b94
Andre Sharpe <andre.sharpe@gmail.com>
2023-11-30 12:56:20 +0100

    Feature/dynamic API verb sensitivity 02 (#1189)
    * Allow different signatures for Http verbs on dynamic Api middleware
    
    * Changed dependancy test for Nox Abstractions to pass on Windows+Linux
    
    * Enhanced Api mapping features for swagger and query variables
    
    * Fixed merge bug
    
    * Added post documentation and example for ApiRoute

---
+++ src/Nox.Solution/Models/Presentation/ApiConfiguration/ApiRouteMappings/JsonTypeDefinition.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Nox.Yaml.Attributes;
+using Nox.Types;
+using YamlDotNet.Serialization;
+using Nox.Yaml;
+using System;
+
+namespace Nox.Solution;
+
+
+[AdditionalProperties(false)]
+[GenerateJsonSchema]
+public class JsonTypeDefinition : YamlConfigNode<NoxSolution,ApiRouteMapping>
+{
+    [Required]
+    public string Name { get; internal set; } = null!;
+    
+    public string? Description { get; internal set; }
+    
+    [Required]
+    public JsonType Type {get; internal set;}
+
+    public bool IsRequired {get; internal set;} = false;
+
+    public object? Default {get; internal set; }
+
+    [IfEquals(nameof(Type),JsonType.Array)]
+    [Required]
+    public JsonTypeDefinition Items {get; internal set;} = null!;
+ 
+    [IfEquals(nameof(Type),JsonType.Object)]
+    [Required]
+    public IReadOnlyList<JsonTypeDefinition> Attributes {get; internal set;} = null!;
+
+    [YamlIgnore]
+    public string JsonTypeString { get; internal set; } = null!;
+
+    public override void SetDefaults(NoxSolution topNode, ApiRouteMapping parentNode, string yamlPath)
+    {
+        JsonTypeString = ToJsonTypeString(Type);
+    }
+
+    private static string ToJsonTypeString(JsonType type) 
+    {
+        return type switch
+        {
+            JsonType.Number => "number",
+            JsonType.String => "string",
+            JsonType.DateString => "string",
+            JsonType.Boolean => "boolean",
+            JsonType.Array => "array",
+            JsonType.Object => "object",
+            JsonType.Null => "null",
+            _ => throw new NotImplementedException()
+        };
+    }
+
+}
