af658108766ab5ed8657cb812e5e388ac2954836
DanielHodgeGH <github@danielhodge.co.uk>
2023-12-01 10:17:19 +0000

    feature 294 Remove Try Catch and allow errors to be raised also removed AutoMapper

--- samples/Cryptocash.Api/DataSeed/Seeders/CryptocashEmployeeDataSeeder.cs
+++ samples/Cryptocash.Api/DataSeed/Seeders/CryptocashEmployeeDataSeeder.cs
@@ -3,7 +3,6 @@ using Cryptocash.DataSeed.Seeders;
 using Cryptocash.Domain;
 using Cryptocash.Infrastructure.Persistence;
 using Cryptocash.Application.Dto;
-using AutoMapper;
 using MassTransit.Transports;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Nox.Extensions;
