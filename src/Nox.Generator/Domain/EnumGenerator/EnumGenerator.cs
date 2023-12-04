191356459eee8d68c45f835dd972a681542d5d3e
emir-sehovic1 <135145425+emir-sehovic1@users.noreply.github.com>
2023-12-04 10:21:48 +0100

    NOX-257 Generate Enum Class for Enumeration Values (#1193)
    * Added generation of enums for enumeration type attributes
    
    * Updated expected number file count

---
+++ src/Nox.Generator/Domain/EnumGenerator/EnumGenerator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+
+using Nox.Generator.Common;
+using Nox.Solution;
+
+using System.Linq;
+
+namespace Nox.Generator.Domain.EnumGenerator;
+internal class EnumGenerator : INoxCodeGenerator
+{
+    public NoxGeneratorKind GeneratorKind => NoxGeneratorKind.Domain;
+
+    public void Generate(
+        SourceProductionContext context,
+        NoxCodeGenConventions codeGeneratorState,
+        GeneratorConfig config,
+        System.Action<string> log,
+        string? projectRootPath
+     )
+    {
+        context.CancellationToken.ThrowIfCancellationRequested();
+
+        if (codeGeneratorState.Solution.Domain == null)
+            return;
+
+        foreach (var entity in codeGeneratorState.Solution.Domain.Entities.Where(x => x.Attributes.Any(x => x.Type == Types.NoxType.Enumeration)))
+        {
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            var enumAttributes = entity.Attributes.Where(attribute => attribute.Type == Types.NoxType.Enumeration
+                    && attribute.EnumerationTypeOptions?.Values?.Any() == true)
+                .Select(attribute => new
+                {
+                    Description = attribute.Description,
+                    Name = attribute.Name,
+                    Values = attribute.EnumerationTypeOptions!.Values.Select(value => new
+                    {
+                        Name = value.Name,
+                        SanitizedName = codeGeneratorState.GetEnumPropertyName(value.Name),
+                        Id = value.Id
+                    })
+                });
+
+            new TemplateCodeBuilder(context, codeGeneratorState)
+                    .WithClassName($"{entity.Name}Enums")
+                    .WithFileNamePrefix($"Domain")
+                    .WithObject("entity", entity)
+                    .WithObject("enumAttributes", enumAttributes)
+                    .GenerateSourceCodeFromResource("Domain.EnumGenerator.Enum");
+        }
+    }
+}
---
+++ tests/Nox.Generator.Tests/ExpectedGeneratedFiles/Domain.CountryEnums.expected.g.cs
@@ -0,0 +1,62 @@
+﻿// Generated
+
+namespace SampleWebApp.Domain;
+
+/// <summary>
+/// The country's continent.
+/// </summary> 
+public enum CountryContinentEnum
+{
+    /// <summary>
+    /// Europe$%!
+    /// </summary>
+    Europe = 1,
+    
+    /// <summary>
+    /// Asia
+    /// </summary>
+    Asia = 2,
+    
+    /// <summary>
+    /// Africa
+    /// </summary>
+    Africa = 3,
+    
+    /// <summary>
+    /// North America
+    /// </summary>
+    North_America = 4,
+    
+    /// <summary>
+    /// South America
+    /// </summary>
+    South_America = 5,
+    
+    /// <summary>
+    /// Oceania
+    /// </summary>
+    Oceania = 6,
+    
+    /// <summary>
+    /// Antarctica
+    /// </summary>
+    Antarctica = 7,
+    
+}
+
+/// <summary>
+/// Dummy enum.
+/// </summary> 
+public enum CountryDummyEnum
+{
+    /// <summary>
+    /// Val1
+    /// </summary>
+    Val1 = 10,
+    
+    /// <summary>
+    /// Val2
+    /// </summary>
+    Val2 = 15,
+    
+}
