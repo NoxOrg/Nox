373b82a5d483b2530f90075d838bd83ffc9cbf8a
jan <janschutte.lands@gmail.com>
2023-12-04 16:40:30 +0200

    - added Generator.g.cs

---
+++ samples/Cryptocash.Ui/Nox.Generated/Generator.g.cs
@@ -0,0 +1,81 @@
+﻿// Generated
+
+#nullable enable
+
+/* Found files ->
+- cryptocash.application.nox.yaml
+- CountrySync.integration.nox.yaml
+- cryptocash.solution.nox.yaml
+- cryptocash.domain.nox.yaml
+- BankNote.entity.nox.yaml
+- Booking.entity.nox.yaml
+- CashStockOrder.entity.nox.yaml
+- Commission.entity.nox.yaml
+- Country.entity.nox.yaml
+- CountryTimeZone.entity.nox.yaml
+- Currency.entity.nox.yaml
+- Customer.entity.nox.yaml
+- Employee.entity.nox.yaml
+- EmployeePhoneNumber.entity.nox.yaml
+- ExchangeRate.entity.nox.yaml
+- Holiday.entity.nox.yaml
+- Landlord.entity.nox.yaml
+- MinimumCashStock.entity.nox.yaml
+- PaymentDetail.entity.nox.yaml
+- PaymentProvider.entity.nox.yaml
+- Transaction.entity.nox.yaml
+- VendingMachine.entity.nox.yaml
+- cryptocash.infrastructure.nox.yaml
+- cryptocash.dependencies.nox.yaml
+- NoxReferenceData.dataConnection.nox.yaml
+- cryptocash.endpoints.nox.yaml
+- cryptocash.messaging.nox.yaml
+- cryptocash.persistence.nox.yaml
+- countriesByName.apiRouteMapping.nox.yaml
+- cryptocash.apiConfiguration.nox.yaml
+- cryptocash.presentation.nox.yaml
+- BankNoteDomainEvents.md
+- BookingDomainEvents.md
+- CashStockOrderDomainEvents.md
+- CommissionDomainEvents.md
+- CountryDomainEvents.md
+- CountryTimeZoneDomainEvents.md
+- CurrencyDomainEvents.md
+- CustomerDomainEvents.md
+- EmployeeDomainEvents.md
+- EmployeePhoneNumberDomainEvents.md
+- ExchangeRateDomainEvents.md
+- HolidayDomainEvents.md
+- LandLordDomainEvents.md
+- MinimumCashStockDomainEvents.md
+- PaymentDetailDomainEvents.md
+- PaymentProviderDomainEvents.md
+- TransactionDomainEvents.md
+- VendingMachineDomainEvents.md
+- BookingEndpoints.md
+- CashStockOrderEndpoints.md
+- CommissionEndpoints.md
+- CountryEndpoints.md
+- CurrencyEndpoints.md
+- CustomerEndpoints.md
+- EmployeeEndpoints.md
+- LandLordEndpoints.md
+- MinimumCashStockEndpoints.md
+- PaymentDetailEndpoints.md
+- PaymentProviderEndpoints.md
+- TransactionEndpoints.md
+- VendingMachineEndpoints.md
+- IntegrationEvents.md
+- README.md
+- generator.nox.yaml
+Errors ->
+- The Nox type validation failed with 4 error(s). PropertyName: . Error: Disallowed property ["requestInput"]. (at line 13 in countriesByName.apiRouteMapping.nox.yaml)
+PropertyName: . Error: Disallowed property ["requestBodyType"]. (at line 19 in countriesByName.apiRouteMapping.nox.yaml)
+PropertyName: . Error: Disallowed property ["jsonBodyType"]. (at line 21 in countriesByName.apiRouteMapping.nox.yaml)
+PropertyName: . Error: Disallowed property ["items"]. (at line 32 in countriesByName.apiRouteMapping.nox.yaml)   at Nox.Solution.Schema.NoxSchemaValidator.Deserialize[T](YamlReferenceResolver yamlRefResolver)
+   at Nox.Yaml.YamlConfigurationReader`2.ResolveAndLoadConfiguration()
+   at Nox.Yaml.YamlConfigurationReader`2.Read()
+   at Nox.Solution.NoxSolutionBuilder.Build()
+   at Nox.Generator.Tasks.NoxFileGenerator.TryGetNoxSolution(NoxSolution& solution) in /home/runner/work/Nox.Generator/Nox.Generator/src/Nox.Generator.Tasks/NoxFileGenerator.cs:line 195
+   at Nox.Generator.Tasks.NoxFileGenerator.GenerateFiles() in /home/runner/work/Nox.Generator/Nox.Generator/src/Nox.Generator.Tasks/NoxFileGenerator.cs:line 45
+*/
