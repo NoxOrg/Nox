28e0d42207ace906deb907e195b37830b73ba8e9
DanielHodgeGH <github@danielhodge.co.uk>
2023-12-18 14:52:24 +0000

    add feature Nox.Ui.Blazor.Lib test page

--- src/Nox.Ui.Blazor.Lib/Components/NoxTypes/EditText.razor.cs
+++ src/Nox.Ui.Blazor.Lib/Components/NoxTypes/EditText.razor.cs
@@ -1,44 +1,41 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace Nox.Ui.Blazor.Lib.Components.NoxTypes
+namespace Nox.Ui.Blazor.Lib.Components.NoxTypes;
+
+public partial class EditText : ComponentBase
 {
-    public partial class EditText : ComponentBase
+
+    #region Declarations
+
+    [Parameter]
+    public string? Text { get; set; }
+
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter]
+    public EventCallback<string> TextChanged { get; set; }
+
+    [Parameter]
+    public int MaxLength { get; set; } = 63;
+
+    [Parameter]
+    public int MinLength { get; set; } = 0;
+
+    #endregion
+
+    protected async Task OnTextChanged(string newValue)
     {
+        Text = newValue;
 
-#nullable enable
+        await TextChanged.InvokeAsync(Text);
+    }
 
-        #region Declarations
-
-        [Parameter]
-        public string? Text { get; set; }
-
-        [Parameter]
-        public string? Title { get; set; }
-
-        [Parameter]
-        public EventCallback<string> TextChanged { get; set; }
-
-        [Parameter]
-        public int MaxLength { get; set; } = 63;
-
-        [Parameter]
-        public int MinLength { get; set; } = 0;
-
-        #endregion
-
-        protected async Task OnTextChanged(string newValue)
-        {
-            Text = newValue;
-
-            await TextChanged.InvokeAsync(Text);
-        }
-
-        public string? ValidateLength(string arg)
-        {
-            if (arg != null
-                && arg.Length < MinLength)
-                return String.Format("{0} is required and must be at least {1} characters long", Title, MinLength.ToString());
-            return null;
-        }
+    public string ValidateLength(string arg)
+    {
+        if (arg != null
+            && arg.Length < MinLength)
+            return String.Format("{0} is required and must be at least {1} characters long", Title, MinLength.ToString());
+        return String.Empty;
     }
 }
---
+++ tests/No.Ui.Blazor.Lib.Tests/FixtureConfig/AutoMoqDataAttribute.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using AutoFixture;
+
+namespace No.Ui.Blazor.Lib.Tests.FixtureConfig;
+
+public class AutoMoqDataAttribute : AutoDataAttribute
+{
+    public AutoMoqDataAttribute()
+        : base(CreateFixture)
+    {
+    }
+
+    private static IFixture CreateFixture()
+    {
+        var fixture = new Fixture();
+        fixture.Customize(new AutoMoqCustomization());
+        return fixture;
+    }
+}
