accc4c85336da7585b9a9d385a9f821550f5e2fa
Anna Naumova <anna.naumova@dataart.com>
2023-12-20 09:43:10 -0500

    rename and move RouteMatchers

--- src/Nox.Lib/Middlewares/NoxApiMiddleware.cs
+++ src/Nox.Lib/Middlewares/NoxApiMiddleware.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Nox.Solution;
-using System.Diagnostics;
 
 namespace Nox.Lib;
 
@@ -86,51 +85,4 @@ public class NoxApiMiddleware
         return;
     }
 
-}
-
-[DebuggerDisplay("{_apiRoute}")]
-internal class RouteMatcher
-{
-
-    private readonly ApiRouteMatcher _matcher;
-
-    private readonly ApiRouteMapping _apiRoute;
-
-    private IDictionary<string, string>? _values;
-
-    private const int _slash = 47;
-
-    public RouteMatcher(ApiRouteMapping apiRoute, string prefix)
-    {
-        var leadingChar = apiRoute.Route[0] == _slash ? string.Empty : "/";
-
-        var template = $"{prefix}{leadingChar}{apiRoute.Route}";
-
-        _apiRoute = apiRoute;
-
-        _matcher = new ApiRouteMatcher(template, 
-            apiRoute
-            .RequestInput
-            .Where(i => i.Default is not null)
-            .ToDictionary(i => i.Name, i => i.Default?.ToString() ?? string.Empty)
-        );
-    }
-
-    internal ApiRouteMapping ApiRoute => _apiRoute;
-
-    public bool Match(HttpRequest httpRequest)
-    {
-        var isMatch = _matcher.Match($"{httpRequest.Path}{httpRequest.QueryString}", out var values);
-
-        _values = values;
-
-        return isMatch;
-    }
-
-    public string TransformTo(string toPath)
-    {
-        if (_values is null) return toPath;
-
-        return _matcher.TransformTo(toPath, _values);
-    }
 }
