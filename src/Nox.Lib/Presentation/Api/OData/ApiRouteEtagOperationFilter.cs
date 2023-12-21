9480cb0c44405a8380d941bcaa6c7fc91c28de77
Muhammed Sadik Kose <136459287+msadikkoseaupa@users.noreply.github.com>
2023-12-21 10:05:44 +0300

    NOX-337 etag  header parameter added as optional for PUT, PATCH, DELETE actions. (#1265)
    * etag  header parameter added as optional for PUT, PATCH, DELETE actions.
    
    * Array variable converted to static readonly
    
    * Code quality improved
    
    * Code review comments have been resolved

---
+++ src/Nox.Lib/Presentation/Api/OData/ApiRouteEtagOperationFilter.cs
@@ -0,0 +1,30 @@
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Nox.Presentation.Api.OData;
+
+public class ApiRouteEtagOperationFilter:  IOperationFilter
+{
+    private  static readonly  HashSet<string> ETagRequiredHttpMethods = new() { HttpMethod.Delete.Method, HttpMethod.Patch.Method, HttpMethod.Put.Method };
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (ETagRequiredHttpMethods.Contains(context.ApiDescription.HttpMethod!))
+        {
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "If-Match",
+                Description = "Concurrency check id",
+                In = ParameterLocation.Header,
+                Style = ParameterStyle.Simple,
+                Schema = new OpenApiSchema
+                {
+                    Type = "string", 
+                    Format = "uuid",
+                    Pattern = "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$"
+                },
+                Required = false
+            });
+        }
+    }
+}
