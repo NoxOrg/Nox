07983456510ee268707bf1ef9a8c77a5c0b62d9a
m.sadik.kose <sadik.kose@authoritypartners.com>
2023-12-21 13:11:55 +0300

    Code review comments have been resolved

--- src/Nox.Lib/Presentation/Api/OData/ApiRouteQueryStringOperationFilter.cs
+++ src/Nox.Lib/Presentation/Api/OData/ApiRouteQueryStringOperationFilter.cs
@@ -6,29 +6,29 @@ namespace Nox.Presentation.Api.OData;
 
 public class ApiRouteQueryStringOperationFilter: IOperationFilter
 {
-    private readonly IEnumerable<string> _auditEntityPaths;
+    private readonly HashSet<string> _auditEntityPaths;
 
     public ApiRouteQueryStringOperationFilter(NoxSolution noxSolution)
     {
         _auditEntityPaths = 
             noxSolution.Domain!.Entities.Where(e => e.Persistence.IsAudited)
-                .Select(e=> string.Concat(noxSolution.Presentation.ApiConfiguration.ApiRoutePrefix.Trim('/'), "/", e.PluralName));
+                .Select(e=> string.Concat(noxSolution.Presentation.ApiConfiguration.ApiRoutePrefix, "/", e.PluralName)).ToHashSet();
     }
     
     public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
-        if (context.ApiDescription.HttpMethod == HttpMethod.Get.Method && _auditEntityPaths.Any(p => context.ApiDescription.RelativePath!.Contains(p)))
+        if (context.ApiDescription.HttpMethod == HttpMethod.Get.Method && _auditEntityPaths.Any(p => p.Contains(context.ApiDescription.RelativePath!)))
         {
             operation.Parameters.Add(new OpenApiParameter
             {
-                Name = "lang",
+                Name = QueryParams.Language,
                 Description = "language parameter",
                 In = ParameterLocation.Query,
                 Style = ParameterStyle.Simple,
                 Schema = new OpenApiSchema
                 {
                     Type = "string",
-                    Pattern = "^[a-z]{2}-[A-Z]{2}$"
+                    Pattern = @"^[a-z]{2}(?:-[A-Z]{2})?(?:-[A-Z][a-z]{3})?$"
                 },
                 Required = false
             });
