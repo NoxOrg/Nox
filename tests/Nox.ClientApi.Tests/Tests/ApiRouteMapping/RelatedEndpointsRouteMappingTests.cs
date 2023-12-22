f5b7be6c561feca7d859ceda08c6646843ca8863
Anna Naumova <anna.naumova@dataart.com>
2023-12-22 11:19:03 -0500

    NOX-517  Use EntityPluralName when redirect via NavigationName (#1284)
    * use EntityPluralName when redirecting via RelatedEndpointsMiddleware
    
    * build sln

--- tests/Nox.ClientApi.Tests/Tests/ApiRouteMapping/RelatedEndpointsRouteMappingTests.cs
+++ tests/Nox.ClientApi.Tests/Tests/ApiRouteMapping/RelatedEndpointsRouteMappingTests.cs
@@ -45,5 +45,57 @@ public partial class RelatedEndpointsRouteMappingTests : NoxWebApiTestBase
         getCountryResponse!.Workplaces.First().Name.Should().Be(expectedName);
     }
 
+    [Fact]
+    public async Task WhenPatchZeroToOneRelatedEntity_ShouldSucceed()
+    {
+        // Arrange
+        var ownerExpectedName = _fixture.Create<string>();
+        var storeResponse = await PostAsync<StoreCreateDto, StoreDto>(Endpoints.StoresUrl, new StoreCreateDto
+        {
+            Name = _fixture.Create<string>(),
+            Address = new StreetAddressDto(
+                 null!,
+                 "3000 Hillswood Business Park",
+                 null!,
+                 null!,
+                 null!,
+                 null!,
+                 null!,
+                 null!,
+                 "KT16 0RS",
+                 Nox.Types.CountryCode.GB),
+            Location = new LatLongDto(51.3728033, -0.5389749)
+        });
+        var ownerResponse = await PostAsync<StoreOwnerCreateDto, StoreOwnerDto>(Endpoints.StoreOwnersUrl, new StoreOwnerCreateDto
+        {
+            Id = "002",
+            Name = _fixture.Create<string>(),
+            TemporaryOwnerName = _fixture.Create<string>()
+        });
+        await PostAsync($"{Endpoints.StoresUrl}/{storeResponse!.Id}/{nameof(StoreDto.StoreOwner)}/{ownerResponse!.Id}/$ref");
+        var getOwnedResponse = await GetODataSimpleResponseAsync<StoreOwnerDto>($"{Endpoints.StoreOwnersUrl}/{ownerResponse!.Id}");
+
+        // Act
+        var headers = CreateEtagHeader(getOwnedResponse!.Etag);
+        var patchResponse = await PatchAsync<StoreOwnerPartialUpdateDto, StoreOwnerDto>(
+            $"{Endpoints.StoresUrl}/{storeResponse!.Id}/{nameof(StoreDto.StoreOwner)}/{ownerResponse!.Id}",
+            new StoreOwnerPartialUpdateDto
+            {
+                Name = ownerExpectedName
+            },
+            headers);
+
+
+        const string oDataRequest = $"$expand={nameof(StoreDto.StoreOwner)}";
+        var getStoreResponse = await GetODataSimpleResponseAsync<StoreDto>($"{Endpoints.StoresUrl}/{storeResponse!.Id}?{oDataRequest}");
+
+
+        //Assert
+        patchResponse.Should().NotBeNull();
+
+        getStoreResponse.Should().NotBeNull();
+        getStoreResponse!.StoreOwner.Should().NotBeNull();
+        getStoreResponse!.StoreOwner!.Name.Should().Be(ownerExpectedName);
+    }
 }
 
