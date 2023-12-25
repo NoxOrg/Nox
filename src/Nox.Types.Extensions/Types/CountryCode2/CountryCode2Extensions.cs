a9c794b62bf7cea6ebb5fabdb1008e857a60e80e
Dejan Bratic <dejan.bratic@igwplc.com>
2023-12-25 10:13:31 +0100

    added initial countrycode2 extensions method;

---
+++ src/Nox.Types.Extensions/Types/CountryCode2/CountryCode2Extensions.cs
@@ -0,0 +1,9 @@
+﻿using Nox.Reference;
+
+namespace Nox.Types.Extensions;
+
+public static class CountryCode2Extensions
+{
+    public static Country GetReferenceCountry(this CountryCode2 countryCode2)
+        => World.Countries.Get(countryCode2.Value)!; // TODO: should we throw an exception here?
+}
