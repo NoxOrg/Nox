608fc2794e0f1e6d0698ae19572a9b4db12958cf
m.sadik.kose <sadik.kose@authoritypartners.com>
2023-12-26 17:43:15 +0300

    Type converter cleaned up.

--- src/Nox.Yaml/TypeConverters/CultureTypeConverter.cs
+++ src/Nox.Yaml/TypeConverters/CultureTypeConverter.cs
@@ -27,87 +27,31 @@ public class CultureTypeConverter: IYamlTypeConverter
 
     public object? ReadYaml(IParser parser, Type type)
     {
-        if(parser.Current?.GetType() == typeof(Scalar))
-        {
-            var scalar = parser!.Consume<Scalar>();
-            if (CultureCodeToCulture.TryGetValue(scalar.Value, out var cultureValue))
-            {
-                return cultureValue;
-            }
-            else
-            {
-                throw new InvalidOperationException($"Unknown culture code: {scalar.Value}");
-            }
-        } 
+        if (parser.Current?.GetType() != typeof(Scalar))
+            throw new InvalidOperationException("Expected a YAML scalar");
         
-        if (parser.TryConsume<MappingStart>(out _))
+        var scalar = parser!.Consume<Scalar>();
+        if (CultureCodeToCulture.TryGetValue(scalar.Value, out var cultureValue))
         {
-            return ParseMapping(parser); // We're parsing a YAML object
+            return cultureValue;
         }
 
-        if (parser.TryConsume<SequenceStart>(out _))
-        {
-            return ParseSequence(parser); // We're parsing a YAML array
-        }
+        throw new InvalidOperationException($"Unknown culture code: {scalar.Value}");
 
-        throw new InvalidOperationException("Expected a YAML object or array");
     }
 
     public void WriteYaml(IEmitter emitter, object? value, Type type)
     {
-        throw new NotImplementedException();
-    }
-    
-    private static Culture ParseMapping(IParser parser)
-    {
-        Culture culture = default;
+        if(value is null)
+            return;
 
-        // Read all until we reached the end of the YAML object
-        while (!parser.Accept<MappingEnd>(out _))
-        {
-            var scalar = parser.Consume<Scalar>();
-            if (CultureCodeToCulture.TryGetValue(scalar.Value, out var cultureValue))
-            {
-                culture = cultureValue;
-            }
-            else
-            {
-                throw new InvalidOperationException($"Unknown culture code: {scalar.Value}");
-            }
-        }
+        Culture culture = (Culture)value;
+        var kvp = CultureCodeToCulture.FirstOrDefault(k => k.Value == culture);
         
-        // parser.Accept(out MappingEnd _);
-        // parser.Consume<Scalar>();
+        emitter.Emit(new MappingStart(null, null, false, MappingStyle.Block));
         
-        
+        emitter.Emit(new Scalar(null,kvp.Key));
 
-        // Consume the mapping end token
-        parser.MoveNext();
-        return culture;
+        emitter.Emit(new MappingEnd());
     }
-    
-    private static Culture ParseSequence(IParser parser)
-    {
-        Culture culture = default;
-
-        // Read all the array values until we reach the end of the YAML array
-        while (!parser.Accept<SequenceEnd>(out _))
-        {
-            var scalar = parser.Consume<Scalar>();
-
-            if (CultureCodeToCulture.TryGetValue(scalar.Value, out var cultureValue))
-            {
-                culture = cultureValue;
-            }
-            else
-            {
-                throw new InvalidOperationException("Invalid key value mapping: " + scalar.Value);
-            }
-        }
-
-        // Consume the mapping end token
-        parser.MoveNext();
-        return culture;
-    }
-
 }
