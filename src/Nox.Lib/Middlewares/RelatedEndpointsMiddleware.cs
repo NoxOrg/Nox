4bc2ded63171822052c03bc3bc07e464cd206c01
Anna Naumova <anna.naumova@dataart.com>
2023-12-27 10:59:23 -0500

    NOX-526 Add validation by ManageRelationshipDepth, CanManage* (#1292)
    * rename related endpoints yaml properties
    
    * add validation by ManageRelationshipDepth, CanManage*
    
    * switch from Dictionary to HasgSet for ApiGenerateRelatedEndpoint

--- src/Nox.Lib/Middlewares/RelatedEndpointsMiddleware.cs
+++ src/Nox.Lib/Middlewares/RelatedEndpointsMiddleware.cs
@@ -5,23 +5,28 @@ namespace Nox.Middlewares;
 
 internal class RelatedEndpointsMiddleware
 {
+    private readonly RequestDelegate _next;
+
     private readonly PathString _apiPrefix;
 
-    private readonly RequestDelegate _next;
+    private readonly int _endpointsMaxDepth;
 
     private readonly HashSet<string> _entitiesPluralNames;
 
     private readonly Dictionary<string, string> _navigationNameToEntityPluralName = 
         new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
+    private readonly HashSet<(string entityName, string navigationName)> _canRedirect = new(); 
+
     public RelatedEndpointsMiddleware(RequestDelegate next, NoxSolution solution)
     {
         _next = next;
 
         _apiPrefix = solution.Presentation.ApiConfiguration.ApiRoutePrefix;
-        
-        _entitiesPluralNames = solution.Domain!.Entities.Select(e => e.PluralName).ToHashSet();
 
+        _endpointsMaxDepth = solution.Presentation.ApiConfiguration.ApiGenerateRelatedEndpointsMaxDepth;
+
+        _entitiesPluralNames = solution.Domain!.Entities.Select(e => e.PluralName).ToHashSet();
 
         /*
         _navigationNameToEntityPluralName is used to map navigationName to EntityPluralName (existing controller name)
@@ -38,12 +43,17 @@ internal class RelatedEndpointsMiddleware
         */
         foreach (var entity in solution.Domain!.Entities)
         {
-            foreach(var relationship in entity.Relationships)
+            foreach (var relationship in entity.Relationships)
             {
                 var navigationName = entity.GetNavigationPropertyName(relationship);
                 if (!_navigationNameToEntityPluralName.ContainsKey(navigationName))
                 {
                     _navigationNameToEntityPluralName[navigationName] = relationship.EntityPlural;
+                }
+
+                if (relationship.ApiGenerateRelatedEndpoint)
+                {
+                    _canRedirect.Add((entity.PluralName.ToLower(), navigationName.ToLower()));
                 }
             }
         }
@@ -58,6 +68,7 @@ internal class RelatedEndpointsMiddleware
             await _next(context);
             return;
         }
+
 
         if (HttpMethods.IsPatch(context.Request.Method))
         {
@@ -77,6 +88,8 @@ internal class RelatedEndpointsMiddleware
 
     private bool TryParseAndValidatePath(string path, out List<string> segments)
     {
+        path = path.ToLower();
+
         segments = new List<string>();
 
         ReadOnlySpan<char> pathSpan = path.AsSpan();
@@ -89,7 +102,7 @@ internal class RelatedEndpointsMiddleware
             if (slashIndex == -1)
             {
                 var newSegment = pathSpan.Slice(startIndex).ToString();
-                if (!IsValidSegment(newSegment, count++))
+                if (!IsSegmentValid(newSegment, count++))
                     return false;
                 segments.Add(newSegment);
                 break;
@@ -97,17 +110,23 @@ internal class RelatedEndpointsMiddleware
             else
             {
                 var newSegment = pathSpan.Slice(startIndex, slashIndex).ToString();
-                if (!IsValidSegment(newSegment, count++))
+                if (!IsSegmentValid(newSegment, count++))
                     return false;
                 segments.Add(newSegment);
                 startIndex += slashIndex + 1;
             }
         }
 
-        return segments.Count > 3;
+        if (segments.Count <= 3)
+            return false;
+
+        if (!IsFirstPairValid(segments[0], segments[2]))
+            return false;
+
+        return IsDepthValid(segments);
     }
 
-    private bool IsValidSegment(string segment, int index)
+    private bool IsSegmentValid(string segment, int index)
     {
         if (index == 0)
             return _entitiesPluralNames.Contains(segment, StringComparer.OrdinalIgnoreCase);
@@ -115,6 +134,22 @@ internal class RelatedEndpointsMiddleware
             return _navigationNameToEntityPluralName.ContainsKey(segment);
         else
             return true; //even segments are valid by default
+    }
+
+    private bool IsDepthValid(List<string> segments)
+    {
+        var count = segments.Count;
+        //if(segments.Last() == "$ref")
+        //    count--;
+
+        var depth = (int)Math.Ceiling((double)count / 2) - 1;
+
+        return depth <= _endpointsMaxDepth;
+    }
+
+    private bool IsFirstPairValid(string entityName, string navigationName)
+    {
+        return _canRedirect.Contains((entityName, navigationName));
     }
 
     private PathString BuildNewPatchPath(List<string> segments)
