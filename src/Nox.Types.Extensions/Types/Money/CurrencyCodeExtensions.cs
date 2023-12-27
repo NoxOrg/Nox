f8b7c644ae664fb3e6e243d8026fc6d7bca95df9
Dejan Bratic <30667828+dejobratic@users.noreply.github.com>
2023-12-27 15:45:25 +0100

    added extensions (#1293)
    Co-authored-by: Dejan Bratic <dejan.bratic@igwplc.com>

---
+++ src/Nox.Types.Extensions/Types/Money/CurrencyCodeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Nox.Reference;
+
+namespace Nox.Types.Extensions;
+
+public static class CurrencyCodeExtensions
+{
+    public static Currency GetReferenceCurrency(this CurrencyCode currencyCode)
+        => World.Currencies.GetByIsoCode(currencyCode.ToString())!;
+}
