f51b9ee3a4073289eec02f35efb47bbdeff410f5
m.sadik.kose <sadik.kose@authoritypartners.com>
2023-12-28 22:20:55 +0300

    LanguageQueryParameterOperationFilter fix

--- src/Nox.Lib/Presentation/Api/OData/LanguageQueryParameterOperationFilter.cs
+++ src/Nox.Lib/Presentation/Api/OData/LanguageQueryParameterOperationFilter.cs
@@ -1,23 +1,25 @@
 using Microsoft.OpenApi.Models;
 using Nox.Solution;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using Nox.Solution.Extensions;
 
 namespace Nox.Presentation.Api.OData;
 
 public class LanguageQueryParameterOperationFilter: IOperationFilter
 {
-    private readonly HashSet<string> _auditEntityPaths;
+    private readonly HashSet<string> _localizedEntityPaths;
 
     public LanguageQueryParameterOperationFilter(NoxSolution noxSolution)
     {
-        _auditEntityPaths = 
-            noxSolution.Domain!.Entities.Where(e => e.IsLocalized)
-                .Select(e=> string.Concat(noxSolution.Presentation.ApiConfiguration.ApiRoutePrefix, "/", e.PluralName)).ToHashSet();
+        var entityPaths = GetEntityPathsForLocalizedOwnedEntities(noxSolution);
+        
+        entityPaths.AddRange( GetEntityPathsForLocalizedEntities(noxSolution));
+
+        _localizedEntityPaths = entityPaths.ToHashSet();
     }
-    
     public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
-        if (context.ApiDescription.HttpMethod == HttpMethod.Get.Method && _auditEntityPaths.Any(p => p.Contains(context.ApiDescription.RelativePath!)))
+        if (context.ApiDescription.HttpMethod == HttpMethod.Get.Method && _localizedEntityPaths.Any(p =>context.ApiDescription.RelativePath!.Contains(p)))
         {
             operation.Parameters.Add(new OpenApiParameter
             {
@@ -34,4 +36,39 @@ public class LanguageQueryParameterOperationFilter: IOperationFilter
             });
         }
     }
+    
+    private static IEnumerable<string> GetEntityPathsForLocalizedEntities(NoxSolution noxSolution)
+    {
+        return noxSolution.Domain!.Entities.Where(e => e is { IsLocalized: true, IsOwnedEntity: false })
+            .Select(e => string.Concat("/", e.PluralName));
+    }
+
+    private static List<string> GetEntityPathsForLocalizedOwnedEntities(NoxSolution noxSolution )
+    {
+        List<string> entityPaths = new();
+        var localizedOwnedEntities =
+            noxSolution.Domain!.Entities.Where(e => e is { IsLocalized: true, IsOwnedEntity: true });
+
+        foreach (var localizedOwnedEntity in localizedOwnedEntities)
+        {
+            var localizedRelationShips = localizedOwnedEntity.OwnerEntity!.OwnedRelationships.Where(e=>e.Entity == localizedOwnedEntity.Name).ToList();
+            foreach (var rel in localizedRelationShips)
+            {
+                string name;
+                if(localizedRelationShips.Count(r=>r.Entity == rel.Entity) == 1)
+                    name = rel.WithSingleEntity()
+                        ? rel.Entity
+                        : rel.EntityPlural;
+                else
+                {
+                    name = rel.Name;
+                }
+                
+                entityPaths.Add(string.Concat("/", name));
+            }
+        }
+
+        return entityPaths;
+    }
+
 }
