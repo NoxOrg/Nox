c3a41ac6dfd7634ff871b5b9bf554b9fc47f63c4
DanielHodgeGH <github@danielhodge.co.uk>
2023-12-29 15:23:50 +0000

    migrate Temp MacAddress Percentage Number

---
+++ src/Nox.Ui.Blazor.Lib/Components/NoxTypes/ViewNumber.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Nox.Types;
+using Nox.Ui.Blazor.Lib.Models;
+
+namespace Nox.Ui.Blazor.Lib.Components.NoxTypes;
+
+public partial class ViewNumber : ComponentBase
+{
+
+    #region Declarations
+
+    [Parameter]
+    public decimal? Number { get; set; }
+
+    [Parameter]
+    public string Format { get; set; } = "#,##0.##";
+
+    public string DisplayNumber
+    {
+        get
+        {
+            if (Number.HasValue)
+            {
+                return Number.Value.ToString(Format);
+            }
+
+            return String.Empty;
+        }
+    }
+
+    #endregion
+}
---
+++ src/Nox.Ui.Blazor.Lib/Components/NoxTypes/ViewPercentage.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Nox.Types;
+using Nox.Ui.Blazor.Lib.Models;
+
+namespace Nox.Ui.Blazor.Lib.Components.NoxTypes;
+
+public partial class ViewPercentage : ComponentBase
+{
+    #region Declarations
+
+    [Parameter]
+    public decimal? Percentage { get; set; }
+
+    [Parameter]
+    public string Format { get; set; } = "#.##";
+
+    public string DisplayPercentage
+    {
+        get
+        {
+            if (Percentage != null
+                && Percentage >= -100
+                && Percentage <= 100)
+            {
+                return Percentage?.ToString(Format) + "%";
+            }
+
+            return String.Empty;
+        }
+    }
+
+    #endregion
+}
