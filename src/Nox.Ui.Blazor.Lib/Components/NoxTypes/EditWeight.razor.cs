26996c473268e16973199ff18f2c688252b0dcf3
DanielHodgeGH <github@danielhodge.co.uk>
2024-01-03 09:51:36 +0000

    add Weight and Volume migration

---
+++ src/Nox.Ui.Blazor.Lib/Components/NoxTypes/EditWeight.razor.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Components;
+using Nox.Types;
+using Nox.Ui.Blazor.Lib.Models;
+
+namespace Nox.Ui.Blazor.Lib.Components.NoxTypes;
+
+public partial class EditWeight : ComponentBase
+{
+
+    #region Declarations
+
+    [Parameter]
+    public decimal? Weight { get; set; }
+
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter]
+    public WeightUnit WeightUnit { get; set; } = WeightUnit.Kilogram;
+
+    [Parameter]
+    public EventCallback<decimal?> WeightChanged { get; set; }
+
+    public string ErrorRequiredMessage
+    {
+        get
+        {
+            return Title + " is required";
+        }
+    }
+
+    [Parameter]
+    public string Format { get; set; } = "#,##0.##";
+
+    [Parameter]
+    public string? AdornmentIcon { get; set; }
+
+    #endregion
+
+    protected async Task OnWeightChanged(string newValue)
+    {
+        if (decimal.TryParse(newValue, out decimal parsedDecimal))
+        {
+            Weight = parsedDecimal;
+        }
+        else
+        {
+            Weight = null;
+        }
+
+        await WeightChanged.InvokeAsync(Weight);
+    }
+
+    public string GetAdornmentIcon()
+    {
+        if (!string.IsNullOrWhiteSpace(AdornmentIcon))
+        {
+            return AdornmentIcon;
+        }
+
+        if (WeightUnit == WeightUnit.Pound)
+        {
+            return Icon.WeightUnit_Pound;
+        }
+
+        if (WeightUnit == WeightUnit.Kilogram)
+        {
+            return Icon.WeightUnit_Kilogram;
+        }
+
+        return String.Empty;
+    }
+}
