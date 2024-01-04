d40d5c9cd5159ec609f59170b7a0211a765222ce
DanielHodgeGH <github@danielhodge.co.uk>
2024-01-04 08:48:51 +0000

    add Time Year Month migrations

---
+++ src/Nox.Ui.Blazor.Lib/Components/NoxTypes/EditMonth.razor.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Components;
+using Nox.Types;
+using Nox.Ui.Blazor.Lib.Models;
+using System.Globalization;
+
+namespace Nox.Ui.Blazor.Lib.Components.NoxTypes;
+
+public partial class EditMonth : ComponentBase
+{
+
+    #region Declarations
+
+    [Parameter]
+    public byte? Month { get; set; }
+
+    [Parameter]
+    public CultureInfo CultureInfo { get; set; } = CultureInfo.CurrentCulture;
+
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter]
+    public EventCallback<byte?> MonthChanged { get; set; }
+
+    public Dictionary<byte, string?>? MonthSelectionList { get; set; } = new Dictionary<byte, string?>();
+
+    public string? CurrentMonthStr { get; set; }
+
+    #endregion
+
+    /// <summary>
+    /// Handles initial loading
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        var months = Enumerable.Range(1, 12).Select(i => new { I = i, M = CultureInfo.GetCultureInfo(CultureInfo.LCID).DateTimeFormat?.GetMonthName(i) });
+
+        if (months != null)
+        {
+            foreach (var CurrentMonth in months)
+            {
+                MonthSelectionList?.Add((byte)CurrentMonth.I, CurrentMonth.M);
+            }
+        }
+    }
+
+    protected async Task OnMonthChanged(string newValue)
+    {
+        byte CurrentMonth = 0;
+
+        if (!string.IsNullOrWhiteSpace(newValue)
+            && byte.TryParse(newValue, out CurrentMonth))
+        {
+            if (CurrentMonth >= 1
+                && CurrentMonth <= 12)
+            {
+                Month = CurrentMonth;
+                CurrentMonthStr = Month.ToString();
+                await MonthChanged.InvokeAsync(Month);
+            }
+        }
+    }
+
+    protected async Task OnClear()
+    {
+        Month = null;
+        CurrentMonthStr = null;
+        await MonthChanged.InvokeAsync(Month);
+    }
+
+    protected string ErrorRequiredMessage(string? CurrentTitle)
+    {
+        return CurrentTitle + " is required";
+    }
+}
---
+++ src/Nox.Ui.Blazor.Lib/Components/NoxTypes/ViewMonth.razor.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Components;
+using Nox.Types;
+using Nox.Ui.Blazor.Lib.Models;
+using System.Globalization;
+
+namespace Nox.Ui.Blazor.Lib.Components.NoxTypes;
+
+public partial class ViewMonth : ComponentBase
+{
+
+    #region Declarations
+
+    [Parameter]
+    public byte? Month { get; set; }
+
+    [Parameter]
+    public CultureInfo CultureInfo { get; set; } = CultureInfo.CurrentCulture;
+
+    public Dictionary<byte, string?>? MonthSelectionList { get; set; } = new Dictionary<byte, string?>();
+
+    public string DisplayMonth
+    {
+        get
+        {
+            if (MonthSelectionList == null
+                || MonthSelectionList.Count < 1)
+            {
+                var months = Enumerable.Range(1, 12).Select(i => new { I = i, M = CultureInfo.GetCultureInfo(CultureInfo.LCID).DateTimeFormat?.GetMonthName(i) });
+
+                if (months != null)
+                {
+                    foreach (var CurrentMonth in months)
+                    {
+                        MonthSelectionList?.Add((byte)CurrentMonth.I, CurrentMonth.M);
+                    }
+                }
+            }
+
+            if (Month != null
+                && Month >= 1
+                && Month <= 12
+                && MonthSelectionList != null
+                && MonthSelectionList.Count > 0)
+            {
+                var SelectedMonth = MonthSelectionList[(byte)Month];
+                if (!String.IsNullOrWhiteSpace(SelectedMonth))
+                {
+                    return SelectedMonth;
+                }
+            }
+
+            return String.Empty;
+        }
+    }
+
+    #endregion
+}
