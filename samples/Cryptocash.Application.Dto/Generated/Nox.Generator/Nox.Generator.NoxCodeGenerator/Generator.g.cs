d3feb50a7a4bad27a17aaa4429a02f0ad0084f77
Ricardo Rocha <rochar@users.noreply.github.com>
2024-01-10 15:29:20 +0000

    add CryptoCash.Application.Dto (#1345)

---
+++ samples/Cryptocash.Application.Dto/Generated/Nox.Generator/Nox.Generator.NoxCodeGenerator/Generator.g.cs
@@ -0,0 +1,38 @@
+﻿// Generated
+
+#nullable enable
+
+// Found files ->
+//  - cryptocash.application.nox.yaml
+//  - cryptocash.solution.nox.yaml
+//  - cryptocash.domain.nox.yaml
+//  - BankNote.entity.nox.yaml
+//  - Booking.entity.nox.yaml
+//  - CashStockOrder.entity.nox.yaml
+//  - Commission.entity.nox.yaml
+//  - Country.entity.nox.yaml
+//  - CountryTimeZone.entity.nox.yaml
+//  - Currency.entity.nox.yaml
+//  - Customer.entity.nox.yaml
+//  - Employee.entity.nox.yaml
+//  - EmployeePhoneNumber.entity.nox.yaml
+//  - ExchangeRate.entity.nox.yaml
+//  - Holiday.entity.nox.yaml
+//  - Landlord.entity.nox.yaml
+//  - MinimumCashStock.entity.nox.yaml
+//  - PaymentDetail.entity.nox.yaml
+//  - PaymentProvider.entity.nox.yaml
+//  - Transaction.entity.nox.yaml
+//  - VendingMachine.entity.nox.yaml
+//  - cryptocash.infrastructure.nox.yaml
+//  - cryptocash.dependencies.nox.yaml
+//  - NoxReferenceData.dataConnection.nox.yaml
+//  - cryptocash.endpoints.nox.yaml
+//  - cryptocash.messaging.nox.yaml
+//  - cryptocash.persistence.nox.yaml
+//  - countriesByName.apiRouteMapping.nox.yaml
+//  - cryptocash.apiConfiguration.nox.yaml
+//  - cryptocash.presentation.nox.yaml
+//  - generator.nox.yaml
+// Logging Verbosity Minimal
+// SUCCESS.
