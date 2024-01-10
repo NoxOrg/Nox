7d15a63b60f052219968b972f005b4bd5ffb5fc1
m.sadik.kose <sadik.kose@authoritypartners.com>
2024-01-10 16:02:32 +0300

    Tests have been fixed - P1

--- src/Nox.Solution/Models/Domain/Entities/Events/DomainEvent.cs
+++ src/Nox.Solution/Models/Domain/Entities/Events/DomainEvent.cs
@@ -11,7 +11,6 @@ public class DomainEvent
     [Description("Descriptive name for the domain event. Should be a singular noun and be unique within a collection of events. Pascal Case recommended.")]
     public string Name { get; internal set; } = null!;
 
-    [Required]
     [Title("Event description")]
     [Description("A descriptive phrase that explains the nature and function of this event within a collection.")]
     public string? Description { get; internal set; }
