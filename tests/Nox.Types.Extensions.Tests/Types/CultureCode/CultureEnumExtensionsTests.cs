6b7bdeaa8fa85ac1a041ff8b9d10b1def73ad9e1
Muhammed Sadik Kose <136459287+msadikkoseaupa@users.noreply.github.com>
2024-01-16 17:29:37 +0300

    NOX-551 Add Nox Type Extensions for CultureCode (#1357)
    * Add Nox Type Extensions for CultureCode
    
    * Test classes has been inherited from WorldTestBase
    
    * Method name updated for naming convention

---
+++ tests/Nox.Types.Extensions.Tests/Types/CultureCode/CultureEnumExtensionsTests.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+using Nox.Types.Abstractions.Extensions;
+
+namespace Nox.Types.Extensions.Tests.Types.CultureCode;
+
+public class CultureEnumExtensionsTests : WorldTestBase
+{
+    [Theory]
+    [InlineData(Culture.tr_TR)]
+    [InlineData(Culture.en)]
+    [InlineData(Culture.agq_CM)]
+    [InlineData(Culture.bs_Latn_BA)]
+    public void WhenGettingReferenceCulture_WithValidCultureEnum_ThenReturnsCulture(Culture cultureEnum)
+    {
+        // Arrange Act
+        var culture = cultureEnum.GetReferenceCulture();
+        // Assert
+        culture.Should().NotBeNull();
+        culture.Name.Should().Be(cultureEnum.ToDisplayName());
+    }
+}
