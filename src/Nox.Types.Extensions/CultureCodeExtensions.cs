077cd46f203f65f77d7bbae38c1aa1e3b219d8c8
m.sadik.kose <sadik.kose@authoritypartners.com>
2024-01-16 22:01:35 +0300

    Code review comments have been resolved

---
+++ src/Nox.Types.Extensions/CultureCodeExtensions.cs
@@ -0,0 +1,34 @@
+using Nox.Reference;
+using Nox.Types.Abstractions.Extensions;
+
+namespace Nox.Types.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="CultureCode"/> class.
+/// </summary>
+public static class CultureCodeExtensions
+{
+    /// <summary>
+    /// Retrieves the reference culture associated with the given culture code.
+    /// </summary>
+    /// <param name="cultureCode">The culture code to retrieve the reference culture for.</param>
+    /// <returns>The reference culture associated with the given culture code.</returns>
+    public static Nox.Reference.Culture GetReferenceCulture(this CultureCode cultureCode)
+        => World.Cultures.Get(cultureCode.Value)!;
+    
+    /// <summary>
+    /// Gets the reference culture corresponding to the specified culture enum.
+    /// </summary>
+    /// <param name="culture">The culture.</param>
+    /// <returns>The reference culture.</returns>
+    public static Nox.Reference.Culture GetReferenceCulture(this Culture culture)
+        => World.Cultures.Get(culture.ToDisplayName())!;
+    
+    /// <summary>
+    /// Retrieves the reference culture corresponding to the given <see cref="Nox.Reference.Culture"/>.
+    /// </summary>
+    /// <param name="culture">The <see cref="Nox.Reference.Culture"/> to get the reference culture for.</param>
+    /// <returns>The reference culture as a <see cref="CultureCode"/>.</returns>
+    public static CultureCode GetCultureCode(this Nox.Reference.Culture culture)
+        => CultureCode.From(culture.Name);
+}
