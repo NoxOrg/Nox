68b08b96da420581ec8049072cb390aea1db6e27
emir.sehovic <emir.sehovic@regus.com>
2024-01-18 17:05:47 +0100

    Updated regexes for existing countries and consolidated data from nox reference for VAT numbers

--- tests/Nox.Types.Tests/Types/VatNumberTests/VatNumberTests.cs
+++ tests/Nox.Types.Tests/Types/VatNumberTests/VatNumberTests.cs
@@ -7,7 +7,7 @@ public class VatNumberTests
     [Fact]
     public void VatNumber_ParameterizedConstructor_Success()
     {
-        var vatNumberValue = "44403198682";
+        var vatNumberValue = "FR44403198682";
         var countryCode = CountryCode.FR;
 
         var vatNumber = VatNumber.From(vatNumberValue, countryCode);
@@ -19,31 +19,31 @@ public class VatNumberTests
     public void VatNumber_ParameterizedConstructor_WithUnsupportedCountryCode()
     {
         var vatNumberValue = "44403198682";
-        var countryCode = CountryCode.UA;
+        var countryCode = CountryCode.BA;
 
         var action = () => VatNumber.From(vatNumberValue, countryCode);
 
         action.Should().Throw<NoxTypeValidationException>()
-            .And.Errors.Should().BeEquivalentTo(new[] { new ValidationFailure("Value", "Could not create a Nox VatNumber type with unsupported CountryCode 'UA'.") });
+            .And.Errors.Should().BeEquivalentTo(new[] { new ValidationFailure("Value", "Could not create a Nox VatNumber type with unsupported CountryCode 'BA'.") });
     }
 
     [Fact]
     public void VatNumber_ParameterizedConstructor_WithUnsupportedFormat()
     {
-        var vatNumberValue = "44403198682123";
+        var vatNumberValue = "FR44403198682123";
         var countryCode = "FR";
 
         var action = () => VatNumber.From(vatNumberValue, countryCode);
 
         action.Should().Throw<NoxTypeValidationException>()
-            .And.Errors.Should().BeEquivalentTo(new[] { new ValidationFailure("Value", "Could not create a Nox VatNumber type with unsupported value 'FR44403198682123'.") });
+            .And.Errors.Should().BeEquivalentTo(new[] { new ValidationFailure("Value", "Could not create a Nox VatNumber type with unsupported value 'FR44403198682123' for CountryCode 'FR'.") });
     }
 
     [Fact]
     public void VatNumber_Should_Equal()
     {
-        var vatNumber1 = VatNumber.From("44403198682", "FR");
-        var vatNumber2 = VatNumber.From("44403198682", CountryCode.FR);
+        var vatNumber1 = VatNumber.From("FR44403198682", "FR");
+        var vatNumber2 = VatNumber.From("FR44403198682", CountryCode.FR);
 
         vatNumber1.Should().Be(vatNumber2);
     }
@@ -51,8 +51,8 @@ public class VatNumberTests
     [Fact]
     public void VatNumber_Should_Not_Equal()
     {
-        var vatNumber1 = VatNumber.From("44403198682", CountryCode.FR); 
-        var vatNumber2 = VatNumber.From("157050817", CountryCode.DE);
+        var vatNumber1 = VatNumber.From("FR44403198682", CountryCode.FR); 
+        var vatNumber2 = VatNumber.From("DE157050817", CountryCode.DE);
 
         vatNumber1.Should().NotBe(vatNumber2);
     }
@@ -60,7 +60,7 @@ public class VatNumberTests
     [Fact]
     public void VatNumber_From_With_Default_Options()
     {
-        var vatNumberValue = "123456789";
+        var vatNumberValue = "GB123456789";
 
         var vatNumber = VatNumber.From(vatNumberValue, new VatNumberTypeOptions());
 
@@ -70,7 +70,7 @@ public class VatNumberTests
     [Fact]
     public void VatNumber_From_With_Custom_Options()
     {
-        var vatNumberValue = "157050817";
+        var vatNumberValue = "DE157050817";
 
         var vatNumber = VatNumber.From(
             vatNumberValue,
@@ -83,7 +83,7 @@ public class VatNumberTests
     [Fact]
     public void VatNumber_From_String_CountryCode()
     {
-        var vatNumberValue = "44403198682";
+        var vatNumberValue = "FR44403198682";
         var countryCode = "FR";
 
         var vatNumber = VatNumber.From(
@@ -97,23 +97,24 @@ public class VatNumberTests
     [Fact]
     public void VatNumber_ToString_Success()
     {
-        var vatNumber = VatNumber.From("44403198682", System.Enum.Parse<CountryCode>("FR"));
+        var vatNumber = VatNumber.From("FR44403198682", System.Enum.Parse<CountryCode>("FR"));
 
         vatNumber.ToString().Should().Be("FR44403198682");
     }
 
     [Theory]
-    [InlineData("AT", "U12345678")]
-    [InlineData("BG", "123456789")]
-    [InlineData("BG", "1234567890")]
-    [InlineData("DK", "12345678")]
-    [InlineData("EE", "123456789")]
-    [InlineData("FI", "12345678")]
-    [InlineData("FR", "12345678901")]
-    [InlineData("FR", "XX123456789")]
-    [InlineData("DE", "123456789")]
-    [InlineData("HU", "12345678")]
-    [InlineData("IT", "12345678901")]
+    [InlineData("AT", "ATU12345678")]
+    [InlineData("BG", "BG123456789")]
+    [InlineData("BG", "BG1234567890")]
+    [InlineData("DK", "DK12345678")]
+    [InlineData("EE", "EE123456789")]
+    [InlineData("FI", "FI12345678")]
+    [InlineData("FR", "FR12345678901")]
+    [InlineData("DE", "DE123456789")]
+    [InlineData("HU", "HU12345678")]
+    [InlineData("IT", "IT12345678901")]
+    [InlineData("ES", "ES51234567A")]
+    [InlineData("ES", "ESA12345678")]
     public void VatNumber_Supported_Formats(string countryCode, string vatNumberValue)
     {
         var countryCode2 = System.Enum.Parse<CountryCode>(countryCode);
