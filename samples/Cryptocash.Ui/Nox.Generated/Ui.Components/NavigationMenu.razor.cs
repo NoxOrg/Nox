dbd26c1513809fb96460062e09aab1d68972529f
Anna Naumova <anna.naumova@dataart.com>
2024-01-19 14:18:15 -0500

    update NavigationMenu component

--- samples/Cryptocash.Ui/Nox.Generated/Ui.Components/NavigationMenu.razor.cs
+++ samples/Cryptocash.Ui/Nox.Generated/Ui.Components/NavigationMenu.razor.cs
@@ -1,50 +1,31 @@
 ﻿// Generated
 
-#nullable enable
-
 using Microsoft.AspNetCore.Components;
+using System.Collections.Immutable;
 
 namespace Cryptocash.Ui.Components;
 
+public record NavigationItem(string Name, string Link, string? Icon = null);
+
 public partial class NavigationMenu : ComponentBase
 {
-    
-    public string BookingsIcon { get; set; } = "";    
-    public string BookingsPageLink { get; set; } = "Bookings";
-    
-    public string CommissionsIcon { get; set; } = "";    
-    public string CommissionsPageLink { get; set; } = "Commissions";
-    
-    public string CountriesIcon { get; set; } = "";    
-    public string CountriesPageLink { get; set; } = "Countries";
-    
-    public string CurrenciesIcon { get; set; } = "";    
-    public string CurrenciesPageLink { get; set; } = "Currencies";
-    
-    public string CustomersIcon { get; set; } = "";    
-    public string CustomersPageLink { get; set; } = "Customers";
-    
-    public string PaymentDetailsIcon { get; set; } = "";    
-    public string PaymentDetailsPageLink { get; set; } = "PaymentDetails";
-    
-    public string TransactionsIcon { get; set; } = "";    
-    public string TransactionsPageLink { get; set; } = "Transactions";
-    
-    public string EmployeesIcon { get; set; } = "";    
-    public string EmployeesPageLink { get; set; } = "Employees";
-    
-    public string LandLordsIcon { get; set; } = "";    
-    public string LandLordsPageLink { get; set; } = "LandLords";
-    
-    public string MinimumCashStocksIcon { get; set; } = "";    
-    public string MinimumCashStocksPageLink { get; set; } = "MinimumCashStocks";
-    
-    public string PaymentProvidersIcon { get; set; } = "";    
-    public string PaymentProvidersPageLink { get; set; } = "PaymentProviders";
-    
-    public string VendingMachinesIcon { get; set; } = "";    
-    public string VendingMachinesPageLink { get; set; } = "VendingMachines";
-    
-    public string CashStockOrdersIcon { get; set; } = "";    
-    public string CashStockOrdersPageLink { get; set; } = "CashStockOrders";
+    [Parameter]
+    public List<NavigationItem> NavigationItems { get; set; } = DefaultNavigationItems.ToList();
+
+    public static readonly ImmutableList<NavigationItem> DefaultNavigationItems =
+        ImmutableList<NavigationItem>.Empty
+        .Add(new NavigationItem("Bookings", "Bookings", null))
+        .Add(new NavigationItem("Commissions", "Commissions", null))
+        .Add(new NavigationItem("Countries", "Countries", null))
+        .Add(new NavigationItem("Currencies", "Currencies", null))
+        .Add(new NavigationItem("Customers", "Customers", "<path d=\"M14,7h-4C8.9,7,8,7.9,8,9v6h2v7h4v-7h2V9C16,7.9,15.1,7,14,7z\"/><circle cx=\"12\" cy=\"4\" r=\"2\"/>"))
+        .Add(new NavigationItem("PaymentDetails", "PaymentDetails", null))
+        .Add(new NavigationItem("Transactions", "Transactions", null))
+        .Add(new NavigationItem("Employees", "Employees", "<path d=\"M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z\"/>"))
+        .Add(new NavigationItem("LandLords", "LandLords", null))
+        .Add(new NavigationItem("MinimumCashStocks", "MinimumCashStocks", null))
+        .Add(new NavigationItem("PaymentProviders", "PaymentProviders", null))
+        .Add(new NavigationItem("VendingMachines", "VendingMachines", "<path d=\"M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM12 17.5L6.5 12H10v-2h4v2h3.5L12 17.5zM5.12 5l.81-1h12l.94 1H5.12z\"/>"))
+        .Add(new NavigationItem("CashStockOrders", "CashStockOrders", null))
+        ;
 }
