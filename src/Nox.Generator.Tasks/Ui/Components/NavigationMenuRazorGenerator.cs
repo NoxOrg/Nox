4c61573287c32b6814a8771a3bcd2d58a07fc1e8
Anna Naumova <anna.naumova@dataart.com>
2024-01-22 10:24:18 -0500

    sort navigation items in alphabetical order

--- src/Nox.Generator.Tasks/Ui/Components/NavigationMenuRazorGenerator.cs
+++ src/Nox.Generator.Tasks/Ui/Components/NavigationMenuRazorGenerator.cs
@@ -22,13 +22,11 @@ internal class NavigationMenuRazor : INoxFileGenerator
         }
 
         var templateName = @"Tasks.Ui.Components.NavigationMenuRazor";
-        var entities = codeGeneratorState.Solution.Domain.Entities.Where(e => !e.IsOwnedEntity && e.Persistence.Read.IsEnabled);
 
         new TaskTemplateFileBuilder(codeGeneratorState, absoluteOutputPath)
             .WithFileExtension("razor")
             .WithClassName($"NavigationMenu")
             .WithFileNamePrefix($"Ui.Components")
-            .WithObject("entities", entities)
             .GenerateSourceCodeFromResource(templateName);
     }
 }
