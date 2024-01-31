f77286404a320672e83f2231f9f70e7cca9d8b5e
Dejan Bratic <dejan.bratic@igwplc.com>
2024-01-31 13:16:04 +0100

    reverted last change;

--- src/Nox.Types/Common/EnumExtensions.cs
+++ src/Nox.Types/Common/EnumExtensions.cs
@@ -7,5 +7,5 @@ namespace Nox.Common;
 public static class EnumExtensions
 {
     public static HashSet<string> ToHashSet<T>() where T : struct, Enum
-        => Enum.GetValues<T>().Select(e => nameof(e)).ToHashSet();
+        => Enum.GetValues<T>().Select(e => e.ToString()).ToHashSet();
 }
