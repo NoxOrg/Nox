f40bbcfc292f7e0634542cff2746a0da00cce43a
emir.sehovic <emir.sehovic@regus.com>
2024-01-31 17:23:45 +0100

    Test

--- src/Nox.Solution/Models/Infrastructure/Messaging/Server/AzureServiceBusConfig.cs
+++ src/Nox.Solution/Models/Infrastructure/Messaging/Server/AzureServiceBusConfig.cs
@@ -7,13 +7,13 @@ namespace Nox.Solution;
 [Description("Configuration to create a valid Azure Service Bus Connection, for example: sb://your-servicebus-name.servicebus.windows.net/;SharedAccessKeyName=YourKeyName;SharedAccessKey=YourKey;")]
 [AdditionalProperties(false)]
 public class AzureServiceBusConfig
-{
+{ 
     [Required]        
     [Title("The endpoint to be used for connecting to the Service Bus namespace.")]
     [Description("Example sb://your-servicebus-name.servicebus.windows.net/")]
     public string Endpoint { get; internal set; } = null!;
 
-    [Required]
+    [Required] 
     [Title("The actual security key associated with the shared access key name used to establish secure communication.")]        
     public string SharedAccessKey { get; internal set; } = null!;
 
