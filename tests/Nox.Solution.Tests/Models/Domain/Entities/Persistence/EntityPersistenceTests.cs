c1f92170d97649c51e191315d6b22edb71de3ebd
Ricardo Rocha <rochar@users.noreply.github.com>
2024-02-06 10:48:40 +0000

    add jobs to Nox.Solution at application level (#1419)

--- tests/Nox.Solution.Tests/Models/Domain/Entities/Persistence/EntityPersistenceTests.cs
+++ tests/Nox.Solution.Tests/Models/Domain/Entities/Persistence/EntityPersistenceTests.cs
@@ -1,19 +1,18 @@
 ﻿using FluentAssertions;
 
 
-namespace Nox.Solution.Tests.Models.Domain.Entities.Persistence
-{
-    public class EntityPersistenceTests
-    {
-        [Fact]
-        public void WhenTableNameIsSet_ShouldNotHaveDefault()
-        {
-            var solution = new NoxSolutionBuilder()
-                .WithFile("./files/entitypersistence.tablename.solution.nox.yaml")
-                .Build();
+namespace Nox.Solution.Tests.Models.Domain.Entities.Persistence;
 
-            solution.Domain!.Entities.Single(e=>e.Name == "Country").Persistence.TableName.Should().Be("SpecialCountry");
-            solution.Domain!.Entities.Single(e => e.Name == "Person").Persistence.TableName.Should().Be("People");
-        }
+public class EntityPersistenceTests
+{
+    [Fact]
+    public void WhenTableNameIsSet_ShouldNotHaveDefault()
+    {
+        var solution = new NoxSolutionBuilder()
+            .WithFile("./files/entitypersistence.tablename.solution.nox.yaml")
+            .Build();
+
+        solution.Domain!.Entities.Single(e=>e.Name == "Country").Persistence.TableName.Should().Be("SpecialCountry");
+        solution.Domain!.Entities.Single(e => e.Name == "Person").Persistence.TableName.Should().Be("People");
     }
 }
