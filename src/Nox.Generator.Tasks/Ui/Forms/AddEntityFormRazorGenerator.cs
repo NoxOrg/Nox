cbd1cb3aed0c6ef2c2b00bda0d884d67380e39a4
Anna Naumova <anna.naumova@dataart.com>
2024-02-07 11:59:39 -0500

    generate Title and Metadata on Add form

--- src/Nox.Generator.Tasks/Ui/Forms/AddEntityFormRazorGenerator.cs
+++ src/Nox.Generator.Tasks/Ui/Forms/AddEntityFormRazorGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using Nox.Generator.Common;
 using Nox.Solution;
+using Nox.Types;
 using Nox.Types.Extensions;
 using System.Linq;
 
@@ -32,13 +33,26 @@ internal class AddEntityFormRazorGenerator : INoxFileGenerator
                    ComponentType = key.Type.GetComponents(key).FirstOrDefault().Value
                });
 
+            var attributesWithTypeOptions = entity.Attributes.Where(a => HasTypeOptions(a)).Select(a => a.Name);
+
             new TaskTemplateFileBuilder(codeGeneratorState, absoluteOutputPath)
                 .WithFileExtension("razor")
                 .WithClassName($"Add{entity.Name}Form")
                 .WithFileNamePrefix($"Ui.Forms.Add")
                 .WithObject("entity", entity)
                 .WithObject("componentsInfo", componentsInfo)
+                .WithObject("attributesWithTypeOptions", attributesWithTypeOptions)
                 .GenerateSourceCodeFromResource(templateName);
         }
+    }
+
+    private bool HasTypeOptions(NoxSimpleTypeDefinition attribute)
+    {
+        var typeOptionsName = $"{attribute.Type}TypeOptions";
+        var propertyInfo = typeof(NoxSimpleTypeDefinition).GetProperty(typeOptionsName);
+        if (propertyInfo is null) return false;
+
+        object propertyValue = propertyInfo.GetValue(attribute);
+        return propertyValue is not null;
     }
 }
