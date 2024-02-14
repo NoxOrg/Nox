9bf4c579a0c2b1bf2ddd19f05907e846067abdc6
Anna Naumova <anna.naumova@dataart.com>
2024-02-14 10:07:15 -0500

    add ViewGuid Ui component

---
+++ src/Nox.Ui.Blazor.Lib/Components/NoxTypes/ViewGuid.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Nox.Ui.Blazor.Lib.Components.NoxTypes;
+
+public partial class ViewGuid : ComponentBase
+{
+    [Parameter]
+    public Guid? Guid { get; set; }
+}
